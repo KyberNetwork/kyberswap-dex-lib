798021a47ca4e412aade601ae93815fd3f72bfec
Dat Luong <105619094+datluongductuan@users.noreply.github.com>
2023-06-01 19:18:37 +0700

    Add some redis config (#198)

--- pkg/redis/config.go
+++ pkg/redis/config.go
@@ -3,11 +3,13 @@ package redis
 import "time"
 
 type Config struct {
-	MasterName   string        `mapstructure:"masterName" json:"masterName" default:""`
-	Addresses    []string      `mapstructure:"addresses" json:"addresses" default:""`
-	DBNumber     int           `mapstructure:"dbNumber" json:"dbNumber" default:"0"`
-	Prefix       string        `mapstructure:"prefix" json:"prefix" default:""`
-	Password     string        `mapstructure:"password" json:"-" default:""`
-	ReadTimeout  time.Duration `mapstructure:"readTimeout" json:"readTimeout" default:"0"`
-	WriteTimeout time.Duration `mapstructure:"writeTimeout" json:"writeTimeout" default:"0"`
+	MasterName    string        `mapstructure:"masterName" json:"masterName" default:""`
+	Addresses     []string      `mapstructure:"addresses" json:"addresses" default:""`
+	DBNumber      int           `mapstructure:"dbNumber" json:"dbNumber" default:"0"`
+	Prefix        string        `mapstructure:"prefix" json:"prefix" default:""`
+	Password      string        `mapstructure:"password" json:"-" default:""`
+	ReadOnly      bool          `mapstructure:"readOnly" json:"readOnly" default:""`
+	RouteRandomly bool          `mapstructure:"routeRandomly" json:"routeRandomly" default:""`
+	ReadTimeout   time.Duration `mapstructure:"readTimeout" json:"readTimeout" default:"0"`
+	WriteTimeout  time.Duration `mapstructure:"writeTimeout" json:"writeTimeout" default:"0"`
 }
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -7,6 +7,7 @@ import (
 	"github.com/redis/go-redis/v9"
 
 	"github.com/KyberNetwork/router-service/internal/pkg/utils"
+	"github.com/KyberNetwork/router-service/pkg/logger"
 )
 
 type Redis struct {
@@ -16,18 +17,21 @@ type Redis struct {
 
 func New(cfg *Config) (*Redis, error) {
 	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs:        cfg.Addresses,
-		DB:           cfg.DBNumber,
-		Password:     cfg.Password,
-		MasterName:   cfg.MasterName,
-		ReadTimeout:  cfg.ReadTimeout,
-		WriteTimeout: cfg.WriteTimeout,
+		Addrs:         cfg.Addresses,
+		DB:            cfg.DBNumber,
+		Password:      cfg.Password,
+		MasterName:    cfg.MasterName,
+		ReadTimeout:   cfg.ReadTimeout,
+		WriteTimeout:  cfg.WriteTimeout,
+		ReadOnly:      cfg.ReadOnly,
+		RouteRandomly: cfg.RouteRandomly,
 	})
 
 	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
 		return nil, err
 	}
 
+	logRedisOption(rdb)
 	return &Redis{config: cfg, Client: rdb}, nil
 }
 
@@ -41,4 +45,36 @@ func (s *Redis) Decode(data []byte, v interface{}) error {
 
 func (s *Redis) FormatKey(args ...interface{}) string {
 	return utils.Join(s.config.Prefix, utils.Join(args...))
+}
+
+func logRedisOption(universalClient redis.UniversalClient) {
+	clusterClient, ok := universalClient.(*redis.ClusterClient)
+
+	if ok {
+		logger.WithFields(logger.Fields{
+			"clusterClientOpts.PoolSize":        clusterClient.Options().PoolSize,
+			"clusterClientOpts.MinIdleConns":    clusterClient.Options().MinIdleConns,
+			"clusterClientOpts.PoolTimeout":     clusterClient.Options().PoolTimeout,
+			"clusterClientOpts.ConnMaxIdleTime": clusterClient.Options().ConnMaxIdleTime,
+			"clusterClientOpts.DialTimeout":     clusterClient.Options().DialTimeout,
+			"clusterClientOpts.ReadTimeout":     clusterClient.Options().ReadTimeout,
+			"clusterClientOpts.WriteTimeout":    clusterClient.Options().WriteTimeout,
+		}).Info("New Redis")
+		return
+	}
+
+	client, ok := universalClient.(*redis.Client)
+	if ok {
+		logger.WithFields(logger.Fields{
+			"client.PoolSize":        client.Options().PoolSize,
+			"client.MinIdleConns":    client.Options().MinIdleConns,
+			"client.PoolTimeout":     client.Options().PoolTimeout,
+			"client.ConnMaxIdleTime": client.Options().ConnMaxIdleTime,
+			"client.DialTimeout":     client.Options().DialTimeout,
+			"client.ReadTimeout":     client.Options().ReadTimeout,
+			"client.WriteTimeout":    client.Options().WriteTimeout,
+		}).Info("New Redis")
+		return
+	}
+
 }
