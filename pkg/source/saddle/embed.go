b84fc161e2718d13deb662a452c8704a1ac27808
Hoang Trinh <hoang.trinhj@gmail.com>
2023-06-21 22:12:34 +0700

    feat: support saddle-like dexes

--- pkg/source/saddle/embed.go
+++ pkg/source/saddle/embed.go
@@ -8,17 +8,57 @@ var swapFlashLoanData []byte
 //go:embed abi/ERC20.json
 var erc20Data []byte
 
-//go:embed pools/arbitrum.json
-var arbitrumPoolData []byte
+// Saddle pool data
 
-//go:embed pools/ethereum.json
-var ethereumPoolData []byte
+//go:embed pools/saddle/arbitrum.json
+var saddleArbitrumPoolData []byte
 
-//go:embed pools/fantom.json
-var fantomPoolData []byte
+//go:embed pools/saddle/ethereum.json
+var saddleEthereumPoolData []byte
+
+//go:embed pools/saddle/fantom.json
+var saddleFantomPoolData []byte
+
+// Synapse pool data
+
+//go:embed pools/synapse/arbitrum.json
+var synapseArbitrumPoolData []byte
+
+//go:embed pools/synapse/avalanche.json
+var synapseAvalanchePoolData []byte
+
+//go:embed pools/synapse/bsc.json
+var synapseBSCPoolData []byte
+
+//go:embed pools/synapse/ethereum.json
+var synapseEthereumPoolData []byte
+
+//go:embed pools/synapse/fantom.json
+var synapseFantomPoolData []byte
+
+//go:embed pools/synapse/optimism.json
+var synapseOptimismPoolData []byte
+
+//go:embed pools/synapse/polygon.json
+var synapsePolygonPoolData []byte
+
+// Axial pool data
+
+//go:embed pools/axial/avalanche.json
+var axialAvalanchePoolData []byte
 
 var bytesByPath = map[string][]byte{
-	"pools/arbitrum.json": arbitrumPoolData,
-	"pools/ethereum.json": ethereumPoolData,
-	"pools/fantom.json":   fantomPoolData,
+	"pools/saddle/arbitrum.json": saddleArbitrumPoolData,
+	"pools/saddle/ethereum.json": saddleEthereumPoolData,
+	"pools/saddle/fantom.json":   saddleFantomPoolData,
+
+	"pools/synapse/arbitrum.json":  synapseArbitrumPoolData,
+	"pools/synapse/avalanche.json": synapseAvalanchePoolData,
+	"pools/synapse/bsc.json":       synapseBSCPoolData,
+	"pools/synapse/ethereum.json":  synapseEthereumPoolData,
+	"pools/synapse/fantom.json":    synapseFantomPoolData,
+	"pools/synapse/optimism.json":  synapseOptimismPoolData,
+	"pools/synapse/polygon.json":   synapsePolygonPoolData,
+
+	"pools/axial/avalanche.json": axialAvalanchePoolData,
 }
