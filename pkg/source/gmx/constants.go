766e5ba6e36f383d718c21f9e9ebb4fa51a58cb8
le.cao <le.cao@kyber.network>
2023-06-30 16:46:51 +0700

    [AG-343] add gmx simulation logic

--- pkg/source/gmx/constants.go
+++ pkg/source/gmx/constants.go
@@ -1,5 +1,7 @@
 package gmx
 
+import "math/big"
+
 const DexTypeGmx = "gmx"
 
 const flagArbitrumSeqOffline = "0xa438451d6458044c3c8cd2f6f31c91ac882a6d91"
@@ -8,3 +10,12 @@ var secondaryPriceFeedVersionByChainID = map[ChainID]SecondaryPriceFeedVersion{
 	ARBITRUM:  secondaryPriceFeedVersion2,
 	AVALANCHE: secondaryPriceFeedVersion2,
 }
+
+var (
+	DefaultGas             = Gas{Swap: 165000}
+	BasisPointsDivisor     = big.NewInt(10000)
+	PricePrecision         = new(big.Int).Exp(big.NewInt(10), big.NewInt(30), nil)
+	USDGDecimals           = big.NewInt(18)
+	OneUSD                 = PricePrecision
+	FlagArbitrumSeqOffline = "0xa438451d6458044c3c8cd2f6f31c91ac882a6d91"
+)
