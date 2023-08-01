44450546d305b21ff9885dfeae6931d328d19839
t0rbik <90512605+t0rbik@users.noreply.github.com>
2023-08-01 15:43:42 +0700

    add velocimeter package (#68)
    * add velocimeter package
    
    * fix accident velodrome reused
    
    * bring go common
    
    * make test pass
    
    * remove redundant comment

--- pkg/source/handler.go
+++ pkg/source/handler.go
@@ -39,6 +39,7 @@ import (
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/uniswap"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/uniswapv3"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/velodrome"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/velocimeter"
 )
 
 func NewPoolsListUpdaterHandler(
@@ -109,6 +110,15 @@ func NewPoolsListUpdaterHandler(
 		cfg.DexID = scanDexCfg.Id
 
 		return velodrome.NewPoolListUpdater(&cfg, ethrpcClient), nil
+	case velocimeter.DexTypeVelocimeter:
+		var cfg velocimeter.Config
+		err := PropertiesToStruct(scanDexCfg.Properties, &cfg)
+		if err != nil {
+			return nil, err
+		}
+		cfg.DexID = scanDexCfg.Id
+
+		return velocimeter.NewPoolListUpdater(&cfg, ethrpcClient), nil
 	case muteswitch.DexTypeMuteSwitch:
 		var cfg muteswitch.Config
 		err := PropertiesToStruct(scanDexCfg.Properties, &cfg)
@@ -388,6 +398,15 @@ func NewPoolTrackerHandler(
 		cfg.DexID = scanDexCfg.Id
 
 		return velodrome.NewPoolTracker(&cfg, ethrpcClient)
+	case velocimeter.DexTypeVelocimeter:
+		var cfg velocimeter.Config
+		err := PropertiesToStruct(scanDexCfg.Properties, &cfg)
+		if err != nil {
+			return nil, err
+		}
+		cfg.DexID = scanDexCfg.Id
+
+		return velocimeter.NewPoolTracker(&cfg, ethrpcClient)
 	case muteswitch.DexTypeMuteSwitch:
 		var cfg muteswitch.Config
 		err := PropertiesToStruct(scanDexCfg.Properties, &cfg)
