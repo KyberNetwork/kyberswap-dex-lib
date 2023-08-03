3272eafb60d7429a696d85943d19971d9891eb82
le.cao <le.cao@kyber.network>
2023-08-03 09:16:58 +0700

    [AG-610] pool state tracker for algebrav1

---
+++ pkg/source/algebrav1/pool_tracker.go
@@ -0,0 +1,246 @@
+package algebrav1
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/KyberNetwork/ethrpc"
+	"github.com/KyberNetwork/logger"
+	v3Entities "github.com/daoleno/uniswapv3-sdk/entities"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/machinebox/graphql"
+	"github.com/sourcegraph/conc/pool"
+
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
+	graphqlPkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/graphql"
+)
+
+type PoolTracker struct {
+	config        *Config
+	ethrpcClient  *ethrpc.Client
+	graphqlClient *graphql.Client
+}
+
+func NewPoolTracker(
+	cfg *Config,
+	ethrpcClient *ethrpc.Client,
+) (*PoolTracker, error) {
+	graphqlClient := graphqlPkg.NewWithTimeout(cfg.SubgraphAPI, graphQLRequestTimeout)
+
+	return &PoolTracker{
+		config:        cfg,
+		ethrpcClient:  ethrpcClient,
+		graphqlClient: graphqlClient,
+	}, nil
+}
+
+func (d *PoolTracker) GetNewPoolState(ctx context.Context, p entity.Pool) (entity.Pool, error) {
+	logger.Infof("[%v] Start getting new state of pool: %v", d.config.DexID, p.Address)
+
+	var (
+		rpcData   FetchRPCResult
+		poolTicks []TickResp
+	)
+
+	g := pool.New().WithContext(ctx)
+	g.Go(func(context.Context) error {
+		var err error
+		rpcData, err = d.fetchRPCData(ctx, p)
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"poolAddress": p.Address,
+				"error":       err,
+			}).Errorf("failed to fetch data from RPC")
+
+		}
+
+		return err
+	})
+	g.Go(func(context.Context) error {
+		var err error
+		poolTicks, err = d.getPoolTicks(ctx, p.Address)
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"poolAddress": p.Address,
+				"error":       err,
+			}).Errorf("failed to query subgraph for pool ticks")
+		}
+
+		return err
+	})
+
+	if err := g.Wait(); err != nil {
+		logger.WithFields(logger.Fields{
+			"poolAddress": p.Address,
+			"error":       err,
+		}).Errorf("failed to fetch pool state, pool: %v, err: %v", p.Address, err)
+		return entity.Pool{}, err
+	}
+
+	ticks := make([]v3Entities.Tick, 0, len(poolTicks))
+	for _, tickResp := range poolTicks {
+		tick, err := transformTickRespToTick(tickResp)
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"poolAddress": p.Address,
+				"error":       err,
+			}).Errorf("failed to transform tickResp to tick")
+			continue
+		}
+
+		// LiquidityGross = 0 means that the tick is uninitialized
+		if tick.LiquidityGross.Cmp(bignumber.ZeroBI) == 0 {
+			continue
+		}
+
+		ticks = append(ticks, tick)
+	}
+
+	extraBytes, err := json.Marshal(Extra{
+		Liquidity:   rpcData.liquidity,
+		GlobalState: rpcData.state,
+		Ticks:       ticks,
+		TickSpacing: int24(rpcData.tickSpacing.Int64()),
+	})
+
+	if err != nil {
+		logger.WithFields(logger.Fields{
+			"poolAddress": p.Address,
+			"error":       err,
+		}).Errorf("failed to marshal extra data")
+		return entity.Pool{}, err
+	}
+
+	p.Extra = string(extraBytes)
+	p.Timestamp = time.Now().Unix()
+	p.Reserves = entity.PoolReserves{
+		rpcData.reserve0.String(),
+		rpcData.reserve1.String(),
+	}
+
+	logger.Infof("[%v] Finish updating state of pool: %v, approximate fee %v", d.config.DexID, p.Address, rpcData.state.Fee)
+
+	return p, nil
+}
+
+func (d *PoolTracker) fetchRPCData(ctx context.Context, p entity.Pool) (FetchRPCResult, error) {
+	var (
+		dataStorageOperator common.Address
+	)
+	res := FetchRPCResult{}
+
+	rpcRequest := d.ethrpcClient.NewRequest()
+	rpcRequest.SetContext(ctx)
+
+	rpcRequest.AddCall(&ethrpc.Call{
+		ABI:    algebraV1PoolABI,
+		Target: p.Address,
+		Method: methodGetLiquidity,
+		Params: nil,
+	}, []interface{}{&res.liquidity})
+
+	// TODO: check with a pool with uint8 community fee non-zero to see if it still work with uint16 ABI
+	rpcRequest.AddCall(&ethrpc.Call{
+		ABI:    algebraV1PoolABI,
+		Target: p.Address,
+		Method: methodGetGlobalState,
+		Params: nil,
+	}, []interface{}{&res.state})
+
+	rpcRequest.AddCall(&ethrpc.Call{
+		ABI:    algebraV1PoolABI,
+		Target: p.Address,
+		Method: methodGetDataStorageOperator,
+		Params: nil,
+	}, []interface{}{&dataStorageOperator})
+
+	rpcRequest.AddCall(&ethrpc.Call{
+		ABI:    algebraV1PoolABI,
+		Target: p.Address,
+		Method: methodGetTickSpacing,
+		Params: nil,
+	}, []interface{}{&res.tickSpacing})
+
+	if len(p.Tokens) == 2 {
+		rpcRequest.AddCall(&ethrpc.Call{
+			ABI:    erc20ABI,
+			Target: p.Tokens[0].Address,
+			Method: erc20MethodBalanceOf,
+			Params: []interface{}{common.HexToAddress(p.Address)},
+		}, []interface{}{&res.reserve0})
+
+		rpcRequest.AddCall(&ethrpc.Call{
+			ABI:    erc20ABI,
+			Target: p.Tokens[1].Address,
+			Method: erc20MethodBalanceOf,
+			Params: []interface{}{common.HexToAddress(p.Address)},
+		}, []interface{}{&res.reserve1})
+	}
+
+	_, err := rpcRequest.TryAggregate()
+	if err != nil {
+		logger.WithFields(logger.Fields{
+			"poolAddress": p.Address,
+			"error":       err,
+		}).Errorf("failed to process tryAggregate")
+		return res, err
+	}
+
+	return res, err
+}
+
+func (d *PoolTracker) getPoolTicks(ctx context.Context, poolAddress string) ([]TickResp, error) {
+	allowSubgraphError := d.config.AllowSubgraphError
+	skip := 0
+	var ticks []TickResp
+
+	for {
+		req := graphql.NewRequest(getPoolTicksQuery(allowSubgraphError, poolAddress, skip))
+
+		var resp struct {
+			Pool *SubgraphPoolTicks `json:"pool"`
+		}
+
+		if err := d.graphqlClient.Run(ctx, req, &resp); err != nil {
+			if allowSubgraphError {
+				if resp.Pool == nil {
+					logger.WithFields(logger.Fields{
+						"poolAddress":        poolAddress,
+						"error":              err,
+						"allowSubgraphError": allowSubgraphError,
+					}).Errorf("failed to query subgraph")
+
+					return nil, err
+				}
+			} else {
+				logger.WithFields(logger.Fields{
+					"poolAddress":        poolAddress,
+					"error":              err,
+					"allowSubgraphError": allowSubgraphError,
+				}).Errorf("failed to query subgraph")
+
+				return nil, err
+			}
+		}
+
+		if resp.Pool == nil || len(resp.Pool.Ticks) == 0 {
+			break
+		}
+
+		ticks = append(ticks, resp.Pool.Ticks...)
+
+		if len(resp.Pool.Ticks) < graphFirstLimit {
+			break
+		}
+
+		skip += len(resp.Pool.Ticks)
+		if skip > graphSkipLimit {
+			logger.Infoln("hit skip limit, continue in next cycle")
+			break
+		}
+	}
+
+	return ticks, nil
+}
