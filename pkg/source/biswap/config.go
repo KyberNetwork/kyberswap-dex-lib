84888f72ed335f28271c271e18362c829dbe2c01
Hải Nam <nam.le@kyber.network>
2023-09-14 11:20:56 +0700

    feat: configurable fee precision for biswap
    related: AG-871

--- pkg/source/biswap/config.go
+++ pkg/source/biswap/config.go
@@ -3,6 +3,7 @@ package biswap
 type Config struct {
 	DexID          string
 	SwapFee        float64 `json:"swapFee"`
+	FeePrecision   int     `json:"feePrecision"`
 	FactoryAddress string  `json:"factoryAddress"`
 	NewPoolLimit   int     `json:"newPoolLimit"`
 }
