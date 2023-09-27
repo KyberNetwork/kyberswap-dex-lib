857dc37927d859a7a49961dc084baa60a7c32dd6
Nguyễn Đức Minh <minh02091997@gmail.com>
2023-09-27 16:14:39 +0700

    Fix: isPaused field not found on old pools and classify crossChainPool (#164)

--- pkg/source/wombat/constant.go
+++ pkg/source/wombat/constant.go
@@ -5,19 +5,21 @@ import "time"
 const (
 	DexTypeWombat = "wombat"
 
-	poolTypeWombatLSD  = "wombat-lsd"
-	poolTypeWombatMain = "wombat-main"
+	poolTypeWombatLSD        = "wombat-lsd"
+	poolTypeWombatMain       = "wombat-main"
+	poolTypeWombatCrossChain = "wombat-cross-chain"
 
 	assetMethodGetRelativePrice = "getRelativePrice"
 	assetMethodCash             = "cash"
 	assetMethodLiability        = "liability"
 
-	poolMethodAddressOfAsset = "addressOfAsset"
-	poolMethodAmpFactor      = "ampFactor"
-	poolMethodEndCovRatio    = "endCovRatio"
-	poolMethodHaircutRate    = "haircutRate"
-	poolMethodStartCovRatio  = "startCovRatio"
-	poolMethodIsPaused       = "isPaused"
+	poolMethodAddressOfAsset         = "addressOfAsset"
+	poolMethodAmpFactor              = "ampFactor"
+	poolMethodEndCovRatio            = "endCovRatio"
+	poolMethodHaircutRate            = "haircutRate"
+	poolMethodStartCovRatio          = "startCovRatio"
+	poolMethodPaused                 = "paused"
+	poolMethodCreditForTokensHaircut = "creditForTokensHaircut"
 
 	graphQLRequestTimeout = 20 * time.Second
 )
