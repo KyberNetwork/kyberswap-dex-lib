e0aa4a8f7cd8a90b4ccbf2011fc5e6052d0a81f2
Hiep Nguyen <hiep.nguyen@kyber.network>
2023-10-03 15:09:58 +0700

    Update uniswap v2 reserves from event

--- pkg/source/uniswap/pool_tracker.go
+++ pkg/source/uniswap/pool_tracker.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/KyberNetwork/ethrpc"
 	"github.com/KyberNetwork/logger"
+	"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
@@ -26,25 +27,36 @@ func NewPoolTracker(
 func (d *PoolTracker) GetNewPoolState(
 	ctx context.Context,
 	p entity.Pool,
-	_ pool.GetNewPoolStateParams,
+	params pool.GetNewPoolStateParams,
 ) (entity.Pool, error) {
 	logger.Infof("[Uniswap V2] Start getting new state of pool: %v", p.Address)
 
-	rpcRequest := d.ethrpcClient.NewRequest()
-	rpcRequest.SetContext(ctx)
+	var (
+		err      error
+		reserves Reserves
+	)
 
-	var reserves Reserves
+	latestSyncEvent := d.findLatestSyncEvent(params.Logs)
+	if latestSyncEvent == nil {
+		reserves, err = d.fetchReservesFromNode(ctx, p.Address)
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"poolAddress": p.Address,
+				"error":       err,
+			}).Error("Fail to fetch reserves from node")
+		}
+	} else {
+		reserves, err = decodeSyncEvent(*latestSyncEvent)
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"poolAddress": p.Address,
+				"event":       latestSyncEvent,
+				"error":       err,
+			}).Error("Fail to decode sync event")
+		}
+	}
 
-	rpcRequest.AddCall(&ethrpc.Call{
-		ABI:    uniswapV2PairABI,
-		Target: p.Address,
-		Method: pairMethodGetReserves,
-		Params: nil,
-	}, []interface{}{&reserves})
-
-	_, err := rpcRequest.Call()
 	if err != nil {
-		logger.Errorf("failed to process tryAggregate for pool: %v, err: %v", p.Address, err)
 		return entity.Pool{}, err
 	}
 
@@ -57,4 +69,68 @@ func (d *PoolTracker) GetNewPoolState(
 	logger.Infof("[Uniswap V2] Finish getting new state of pool: %v", p.Address)
 
 	return p, nil
+}
+
+func (d *PoolTracker) fetchReservesFromNode(ctx context.Context, poolAddress string) (Reserves, error) {
+	var reserves Reserves
+
+	rpcRequest := d.ethrpcClient.NewRequest()
+	rpcRequest.SetContext(ctx)
+
+	rpcRequest.AddCall(&ethrpc.Call{
+		ABI:    uniswapV2PairABI,
+		Target: poolAddress,
+		Method: pairMethodGetReserves,
+		Params: nil,
+	}, []interface{}{&reserves})
+
+	_, err := rpcRequest.Call()
+	if err != nil {
+		logger.Errorf("failed to process tryAggregate for pool: %v, err: %v", poolAddress, err)
+		return Reserves{}, err
+	}
+
+	return reserves, nil
+}
+
+func (d *PoolTracker) findLatestSyncEvent(logs []types.Log) *types.Log {
+	var latestSyncEvent *types.Log
+	for _, log := range logs {
+		if log.Removed || !isSyncEvent(log) {
+			continue
+		}
+
+		if latestSyncEvent == nil ||
+			latestSyncEvent.BlockNumber < log.BlockNumber ||
+			(latestSyncEvent.BlockNumber == log.BlockNumber && latestSyncEvent.Index < log.Index) {
+			latestSyncEvent = &log
+		}
+	}
+
+	return latestSyncEvent
+}
+
+func isSyncEvent(log types.Log) bool {
+	if len(log.Topics) == 0 {
+		return false
+	}
+
+	return log.Topics[0] == uniswapV2PairABI.Events["Sync"].ID
+}
+
+func decodeSyncEvent(log types.Log) (Reserves, error) {
+	filterer, err := NewUniswapFilterer(log.Address, nil)
+	if err != nil {
+		return Reserves{}, err
+	}
+
+	syncEvent, err := filterer.ParseSync(log)
+	if err != nil {
+		return Reserves{}, err
+	}
+
+	return Reserves{
+		Reserve0: syncEvent.Reserve0,
+		Reserve1: syncEvent.Reserve1,
+	}, nil
 }
