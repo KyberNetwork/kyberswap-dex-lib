556924aebb7c6542c39394a8731168c2b20b6994
Le Vu Quang <54746883+vuquang23@users.noreply.github.com>
2023-10-11 17:05:15 +0700

    feat: support swapbased perps (#182)

---
+++ pkg/source/swapbased-perps/usdb.go
@@ -0,0 +1,14 @@
+package swapbasedperps
+
+import (
+	"math/big"
+)
+
+type USDB struct {
+	Address     string   `json:"address"`
+	TotalSupply *big.Int `json:"totalSupply"`
+}
+
+const (
+	usdbMethodTotalSupply = "totalSupply"
+)
