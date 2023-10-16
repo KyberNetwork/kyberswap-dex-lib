92f1e7f02d1c1b1211cd656d0c5cb4db7435b545
Quang Le <levuquang2k1@gmail.com>
2023-10-16 13:58:08 +0700

    wip

---
+++ pkg/source/velocorev2-cpmm/abis.go
@@ -0,0 +1,34 @@
+package velocorev2cpmm
+
+import (
+	"bytes"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+var (
+	factoryABI abi.ABI
+	poolABI    abi.ABI
+)
+
+func init() {
+	builder := []struct {
+		ABI  *abi.ABI
+		data []byte
+	}{
+		{
+			&factoryABI, factoryABIJson,
+		},
+		{
+			&poolABI, poolABIJson,
+		},
+	}
+
+	for _, b := range builder {
+		var err error
+		*b.ABI, err = abi.JSON(bytes.NewReader(b.data))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ pkg/source/velocorev2-cpmm/types.go
@@ -0,0 +1,23 @@
+package velocorev2cpmm
+
+import "encoding/hex"
+
+type Metadata struct {
+	Offset int `json:"offset"`
+}
+
+type bytes32 [32]byte
+
+func (b *bytes32) unwrapToken() string {
+	last20Bytes := b[12:]
+	return "0x" + hex.EncodeToString(last20Bytes)
+}
+
+type StaticExtra struct {
+	PoolTokenNumber uint `json:"poolTokenNumber"`
+}
+
+type Extra struct {
+	Fee1e9        uint32 `json:"fee1e9"`
+	FeeMultiplier string `json:"feeMultiplier"`
+}
