ada4b69a2066bf4ba0233979d9aeaf2d7a63a879
ChauNguyen-K <chau.nguyen@kyber.network>
2023-11-08 15:32:20 +0700

    L2 encoding for smardex (#602)

--- internal/pkg/usecase/encode/l2encode/executor/swapdata/smardex.go
+++ internal/pkg/usecase/encode/l2encode/executor/swapdata/smardex.go
@@ -35,30 +35,28 @@ func UnpackSmardex(data []byte, isFirstSwap bool) (Smardex, error) {
 	var swap Smardex
 	var startByte int
 
-	swap.recipientFlag, startByte = pack.ReadUInt8(data, startByte)
-	if swap.recipientFlag == 0 {
-		swap.Recipient, startByte = pack.ReadAddress(data, startByte)
-	} else {
-		swap.Recipient = common.BytesToAddress([]byte{swap.recipientFlag})
-	}
-
 	swap.PoolMappingID, startByte = pack.ReadUInt24(data, startByte)
 	if swap.PoolMappingID == 0 {
 		swap.Pool, startByte = pack.ReadAddress(data, startByte)
 	}
-
-	swap.TokenIn, startByte = pack.ReadAddress(data, startByte)
 	swap.TokenOut, startByte = pack.ReadAddress(data, startByte)
 
 	if isFirstSwap {
-		swap.Amount, _ = pack.ReadBigInt(data, startByte)
+		swap.Amount, startByte = pack.ReadBigInt(data, startByte)
 		swap.isFirstSwap = true
 	} else {
 		var collectAmountFlag bool
-		collectAmountFlag, _ = pack.ReadBoolean(data, startByte)
+		collectAmountFlag, startByte = pack.ReadBoolean(data, startByte)
 		if collectAmountFlag {
 			swap.Amount = abi.MaxUint256
 		}
+	}
+
+	swap.recipientFlag, startByte = pack.ReadUInt8(data, startByte)
+	if swap.recipientFlag == 0 {
+		swap.Recipient, _ = pack.ReadAddress(data, startByte)
+	} else {
+		swap.Recipient = common.BytesToAddress([]byte{swap.recipientFlag})
 	}
 
 	return swap, nil
@@ -81,16 +79,10 @@ func buildSmardex(swap types.L2EncodingSwap) (Smardex, error) {
 func packSmardex(swap Smardex) ([]byte, error) {
 	var args []interface{}
 
-	args = append(args, swap.recipientFlag)
-	if swap.recipientFlag == 0 {
-		args = append(args, swap.Recipient)
-	}
-
 	args = append(args, swap.PoolMappingID)
 	if swap.PoolMappingID == 0 {
 		args = append(args, swap.Pool)
 	}
-	args = append(args, swap.TokenIn)
 	args = append(args, swap.TokenOut)
 
 	if swap.isFirstSwap {
@@ -101,6 +93,11 @@ func packSmardex(swap Smardex) ([]byte, error) {
 			collectAmountFlag = true
 		}
 		args = append(args, collectAmountFlag)
+	}
+
+	args = append(args, swap.recipientFlag)
+	if swap.recipientFlag == 0 {
+		args = append(args, swap.Recipient)
 	}
 
 	return pack.Pack(args...)
--- internal/pkg/usecase/encode/l2encode/executor/swapdata/smardex_test.go
+++ internal/pkg/usecase/encode/l2encode/executor/swapdata/smardex_test.go
@@ -15,15 +15,16 @@ var packSmardexPairs = []struct {
 }{
 	{
 		data: Smardex{
-			Recipient: common.HexToAddress("0x8857d848e9094b473663F448134fd8a94e5C7C46"),
-			Pool:      common.HexToAddress("0x99f74674bdb885ec5915fac225d069255cc9ae07"),
-			TokenIn:   common.HexToAddress("0x82af49447d8a07e3bd95bd0d56f35241523fbab1"),
-			TokenOut:  common.HexToAddress("0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"),
-			Amount:    big.NewInt(96000000),
+			PoolMappingID: 0,
+			Recipient:     common.HexToAddress("0x8857d848e9094b473663F448134fd8a94e5C7C46"),
+			Pool:          common.HexToAddress("0x99f74674bdb885ec5915fac225d069255cc9ae07"),
+			TokenOut:      common.HexToAddress("0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"),
+			Amount:        big.NewInt(96000000),
 
-			isFirstSwap: true,
+			recipientFlag: 0,
+			isFirstSwap:   true,
 		},
-		packedData: "008857d848e9094b473663f448134fd8a94e5c7c4600000099f74674bdb885ec5915fac225d069255cc9ae0782af49447d8a07e3bd95bd0d56f35241523fbab10d500b1d8e8ef31e21c99d1db9a6444d3adf127000000000000000000000000005b8d800",
+		packedData: "00000099f74674bdb885ec5915fac225d069255cc9ae070d500b1d8e8ef31e21c99d1db9a6444d3adf127000000000000000000000000005b8d800008857d848e9094b473663f448134fd8a94e5c7c46",
 	},
 }
 
