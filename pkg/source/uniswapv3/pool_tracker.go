35bda58bff5fbd35ebbb5a6f79dcb8918ed14d65
Hải Nam <nam.le@kyber.network>
2023-11-17 16:07:22 +0700

    improvement: change json lib

--- pkg/source/uniswapv3/pool_tracker.go
+++ pkg/source/uniswapv3/pool_tracker.go
@@ -2,7 +2,6 @@ package uniswapv3
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"math/big"
 	"time"
@@ -10,6 +9,7 @@ import (
 	"github.com/KyberNetwork/ethrpc"
 	"github.com/KyberNetwork/logger"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/goccy/go-json"
 	"github.com/machinebox/graphql"
 	"github.com/samber/lo"
 	"github.com/sourcegraph/conc/pool"
