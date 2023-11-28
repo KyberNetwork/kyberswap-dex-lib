e118747f5549be82eea037923fc10c69341543cd
Quang Le <levuquang2k1@gmail.com>
2023-11-28 14:49:35 +0700

    wip

--- pkg/liquidity-source/balancer-v2/stable/pool_simulator.go
+++ pkg/liquidity-source/balancer-v2/stable/pool_simulator.go
@@ -1,29 +1,105 @@
 package stable
 
 import (
+	"encoding/json"
+	"errors"
 	"math/big"
 
 	"github.com/holiman/uint256"
 
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/balancer-v2/math"
 	poolpkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
+)
+
+var (
+	ErrInvalidSwapFeePercentage = errors.New("invalid swap fee percentage")
+	ErrPoolPaused               = errors.New("pool is paused")
+	ErrInvalidAmp               = errors.New("invalid amp")
 )
 
 type PoolSimulator struct {
 	poolpkg.Pool
+
+	paused bool
 
 	swapFeePercentage *uint256.Int
 	amp               *uint256.Int
 	scalingFactors    []*uint256.Int
 
 	poolType        string
-	poolTypeVersion uint
+	poolTypeVersion int
 }
 
-func (s *PoolSimulator) CalcAmountOut(
-	tokenAmountIn poolpkg.TokenAmount,
-	tokenOut string,
-) (*poolpkg.CalcAmountOutResult, error) {
+func NewPoolSimulator(entityPool entity.Pool) (*PoolSimulator, error) {
+	var (
+		extra       Extra
+		staticExtra StaticExtra
+
+		tokens         = make([]string, len(entityPool.Tokens))
+		reserves       = make([]*big.Int, len(entityPool.Tokens))
+		scalingFactors = make([]*uint256.Int, len(entityPool.Tokens))
+	)
+
+	if err := json.Unmarshal([]byte(entityPool.Extra), &extra); err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal([]byte(entityPool.StaticExtra), &staticExtra); err != nil {
+		return nil, err
+	}
+
+	swapFeePercentage, overflow := uint256.FromBig(extra.SwapFeePercentage)
+	if overflow {
+		return nil, ErrInvalidSwapFeePercentage
+	}
+
+	for idx := 0; idx < len(entityPool.Tokens); idx++ {
+		tokens[idx] = entityPool.Tokens[idx].Address
+		reserves[idx] = bignumber.NewBig10(entityPool.Reserves[idx])
+
+		scalingFactor, overflow := uint256.FromBig(staticExtra.ScalingFactors[idx])
+		if overflow {
+			return nil, ErrInvalidReserve
+		}
+		scalingFactors[idx] = scalingFactor
+	}
+
+	amp, overflow := uint256.FromBig(extra.Amp)
+	if overflow {
+		return nil, ErrInvalidAmp
+	}
+
+	poolInfo := poolpkg.PoolInfo{
+		Address:     entityPool.Address,
+		Exchange:    entityPool.Exchange,
+		Type:        entityPool.Type,
+		Tokens:      tokens,
+		Reserves:    reserves,
+		Checked:     true,
+		BlockNumber: uint64(entityPool.BlockNumber),
+	}
+
+	return &PoolSimulator{
+		Pool:              poolpkg.Pool{Info: poolInfo},
+		paused:            extra.Paused,
+		swapFeePercentage: swapFeePercentage,
+		amp:               amp,
+		scalingFactors:    scalingFactors,
+		poolType:          staticExtra.PoolType,
+		poolTypeVersion:   staticExtra.PoolTypeVersion,
+	}, nil
+}
+
+func (s *PoolSimulator) CalcAmountOut(params poolpkg.CalcAmountOutParams) (*poolpkg.CalcAmountOutResult, error) {
+	if s.paused {
+		return nil, ErrPoolPaused
+	}
+
+	tokenAmountIn := params.TokenAmountIn
+	tokenOut := params.TokenOut
+
 	indexIn, indexOut := s.GetTokenIndex(tokenAmountIn.Token), s.GetTokenIndex(tokenOut)
 	if indexIn == -1 || indexOut == -1 {
 		return nil, ErrTokenNotRegistered
@@ -77,14 +153,43 @@ func (s *PoolSimulator) CalcAmountOut(
 	}
 
 	return &poolpkg.CalcAmountOutResult{
-		TokenAmountOut: &poolpkg.TokenAmount{Token: tokenOut, Amount: amountOut.ToBig()},
-		Gas:            defaultGas.Swap,
+		TokenAmountOut: &poolpkg.TokenAmount{
+			Token:  tokenOut,
+			Amount: amountOut.ToBig(),
+		},
+		Fee: &poolpkg.TokenAmount{
+			Token:  tokenAmountIn.Token,
+			Amount: feeAmount.ToBig(),
+		},
+		Gas: defaultGas.Swap,
 	}, nil
+}
+
+func (s *PoolSimulator) GetMetaInfo(tokenIn string, tokenOut string) interface{} {
+	return nil
+}
+
+func (s *PoolSimulator) UpdateBalance(params poolpkg.UpdateBalanceParams) {
+	for idx, token := range s.Info.Tokens {
+		if token == params.TokenAmountIn.Token {
+			s.Info.Reserves[idx] = new(big.Int).Add(
+				s.Info.Reserves[idx],
+				params.TokenAmountIn.Amount,
+			)
+		}
+
+		if token == params.TokenAmountOut.Token {
+			s.Info.Reserves[idx] = new(big.Int).Sub(
+				s.Info.Reserves[idx],
+				params.TokenAmountOut.Amount,
+			)
+		}
+	}
 }
 
 func calculateInvariant(
 	poolType string,
-	poolTypeVersion uint,
+	poolTypeVersion int,
 	amp *uint256.Int,
 	balances []*uint256.Int,
 ) (*uint256.Int, error) {
