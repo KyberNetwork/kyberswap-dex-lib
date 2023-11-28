687dee573191dbdef9a2c212a014474cc0c87cf6
Quang Le <levuquang2k1@gmail.com>
2023-11-28 18:13:04 +0700

    wip

--- pkg/liquidity-source/balancer-v2/weighted/pools_list_updater.go
+++ pkg/liquidity-source/balancer-v2/weighted/pools_list_updater.go
@@ -3,17 +3,22 @@ package weighted
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"math/big"
-	"strconv"
+	"strings"
 	"time"
 
+	"github.com/KyberNetwork/blockchain-toolkit/number"
 	"github.com/KyberNetwork/ethrpc"
 	"github.com/KyberNetwork/logger"
+	"github.com/holiman/uint256"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/balancer-v2/shared"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
 )
+
+var ErrInvalidWeight = errors.New("invalid weight")
 
 type PoolsListUpdater struct {
 	config        Config
@@ -81,39 +86,44 @@ func (u *PoolsListUpdater) initPools(ctx context.Context, subgraphPools []*share
 
 func (u *PoolsListUpdater) initPool(ctx context.Context, subgraphPool *shared.SubgraphPool) (entity.Pool, error) {
 	var (
-		poolTokens     = make([]*entity.PoolToken, len(subgraphPool.Tokens))
-		reserves       = make([]string, len(subgraphPool.Tokens))
-		scalingFactors = make([]*big.Int, len(subgraphPool.Tokens))
+		poolTokens        = make([]*entity.PoolToken, len(subgraphPool.Tokens))
+		reserves          = make([]string, len(subgraphPool.Tokens))
+		scalingFactors    = make([]*uint256.Int, len(subgraphPool.Tokens))
+		normalizedWeights = make([]*uint256.Int, len(subgraphPool.Tokens))
+
+		err error
 	)
 
 	for j, token := range subgraphPool.Tokens {
-		w, err := strconv.ParseFloat(token.Weight, 64)
+		w, ok := new(big.Float).SetString(token.Weight)
+		if !ok {
+			return entity.Pool{}, ErrInvalidWeight
+		}
+		weightStr := new(big.Float).Mul(w, bignumber.BoneFloat).String()
+		normalizedWeights[j], err = uint256.FromDecimal(weightStr)
 		if err != nil {
 			return entity.Pool{}, err
 		}
-		weight := uint(w * 1e18)
-		if weight == 0 {
-			weight = uint(1e18 / len(subgraphPool.Tokens))
-		}
+
 		poolTokens[j] = &entity.PoolToken{
-			Address:   token.Address,
-			Weight:    weight,
+			Address:   strings.ToLower(token.Address),
 			Swappable: true,
 		}
 
 		reserves[j] = "0"
 
-		scalingFactors[j] = bignumber.TenPowInt(18 - uint8(token.Decimals))
+		scalingFactors[j] = number.TenPow(18 - uint8(token.Decimals))
 		if subgraphPool.PoolTypeVersion.Int64() > poolTypeVer1 {
-			scalingFactors[j] = new(big.Int).Mul(scalingFactors[j], bignumber.BONE)
+			scalingFactors[j] = new(uint256.Int).Mul(scalingFactors[j], number.Number_1e18)
 		}
 	}
 
 	staticExtra := StaticExtra{
-		PoolID:          subgraphPool.ID,
-		PoolType:        subgraphPool.PoolType,
-		PoolTypeVersion: int(subgraphPool.PoolTypeVersion.Int64()),
-		ScalingFactors:  scalingFactors,
+		PoolID:            subgraphPool.ID,
+		PoolType:          subgraphPool.PoolType,
+		PoolTypeVersion:   int(subgraphPool.PoolTypeVersion.Int64()),
+		ScalingFactors:    scalingFactors,
+		NormalizedWeights: normalizedWeights,
 	}
 	staticExtraBytes, err := json.Marshal(staticExtra)
 	if err != nil {
@@ -121,7 +131,7 @@ func (u *PoolsListUpdater) initPool(ctx context.Context, subgraphPool *shared.Su
 	}
 
 	return entity.Pool{
-		Address:     subgraphPool.Address,
+		Address:     strings.ToLower(subgraphPool.Address),
 		Exchange:    u.config.DexID,
 		Type:        DexType,
 		Timestamp:   time.Now().Unix(),
