8e6180d0b703364fee67e04fc0f4be39e51977a1
Nguyen Duc Minh <minh02091997@gmail.com>
2023-11-28 11:04:46 +0700

    Fix quickperps testcase

--- pkg/source/quickperps/errors.go
+++ pkg/source/quickperps/errors.go
@@ -9,11 +9,9 @@ var (
 	ErrVaultReserveExceedsPool             = errors.New("vault: reserve exceeds pool") // code: 50
 	ErrVaultPoolAmountLessThanBufferAmount = errors.New("vault: poolAmount < buffer")
 
-	ErrVaultPriceFeedInvalidPriceFeed         = errors.New("vaultPriceFeed: invalid price feed")
-	ErrVaultPriceFeedInvalidPrice             = errors.New("vaultPriceFeed: invalid price")
-	ErrVaultPriceFeedExpired                  = errors.New("vaultPriceFeed: expired")
-	ErrVaultPriceFeedCouldNotFetchPrice       = errors.New("vaultPriceFeed: could not fetch price")
-	ErrVaultPriceFeedChainlinkFeedsNotUpdated = errors.New("chainlink feeds are not being updated")
+	ErrVaultPriceFeedInvalidPriceFeed = errors.New("vaultPriceFeed: invalid price feed")
+	ErrVaultPriceFeedInvalidPrice     = errors.New("vaultPriceFeed: invalid price")
+	ErrVaultPriceFeedExpired          = errors.New("vaultPriceFeed: expired")
 
 	ErrInvalidSecondaryPriceFeedVersion = errors.New("invalid secondary price feed version")
 )
