3f4c27e7fa03975460521949c3454c29d9048ac7
Hải Nam <nam.le@kyber.network>
2023-12-18 13:15:33 +0700

    feat: integrate ramses v2 (#266)
    * feat: integrate ramses-v2
    
    * feat: migrate with count tick sdk
    
    * chore: remove unused config
    
    * chore: clean up queries test
    
    * feat: estimate gas base on cross tick loops
    
    * chore: cont clean up unused code
    
    * revert: estimate gas using cross tick count
    
    * chore: add explaination for zero gas config

---
+++ pkg/source/ramsesv2/config.go
@@ -0,0 +1,11 @@
+package ramsesv2
+
+type Config struct {
+	DexID              string
+	SubgraphAPI        string `json:"subgraphAPI"`
+	AllowSubgraphError bool   `json:"allowSubgraphError"`
+}
+
+func (c *Config) IsAllowSubgraphError() bool {
+	return c.AllowSubgraphError
+}
