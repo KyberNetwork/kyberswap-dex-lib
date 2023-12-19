b41aed82ffdacb3bda5d652100e781f8373500a6
Hoang Trinh <hoang.trinhj@gmail.com>
2023-12-19 11:01:03 +0700

    refactor: store block number for ticks based dexes

--- pkg/source/pancakev3/pool_tracker.go
+++ pkg/source/pancakev3/pool_tracker.go
@@ -54,6 +54,14 @@ func (d *PoolTracker) GetNewPoolState(
 		poolTicks []TickResp
 	)
 
+	blockNumber, err := d.ethrpcClient.GetBlockNumber(ctx)
+	if err != nil {
+		l.WithFields(logger.Fields{
+			"error": err,
+		}).Error("failed to get block number")
+		return entity.Pool{}, err
+	}
+
 	g := pool.New().WithContext(ctx)
 	g.Go(func(context.Context) error {
 		var err error
@@ -118,6 +126,7 @@ func (d *PoolTracker) GetNewPoolState(
 		rpcData.Reserve0.String(),
 		rpcData.Reserve1.String(),
 	}
+	p.BlockNumber = blockNumber
 
 	l.Infof("Finish updating state of pool")
 
