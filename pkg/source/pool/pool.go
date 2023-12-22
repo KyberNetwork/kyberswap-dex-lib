f36097d309b63ed1010d6cd52ab6a5db5a87812a
Hoang Trinh <hoang.trinhj@gmail.com>
2023-12-22 11:38:15 +0700

    feat: approximate amount in for generic dexes

--- pkg/source/pool/pool.go
+++ pkg/source/pool/pool.go
@@ -2,6 +2,7 @@ package pool
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 
 	"github.com/KyberNetwork/logger"
@@ -9,6 +10,8 @@ import (
 
 var (
 	ErrCalcAmountOutPanic = errors.New("calcAmountOut was panic")
+	ErrInsufficientAmount = errors.New("not enough amount")
+	ErrNotConverge        = errors.New("secant loop cannot converged")
 )
 
 type Pool struct {
@@ -133,6 +136,19 @@ type CalcAmountOutParams struct {
 	Limit         SwapLimit
 }
 
+type CalcAmountInParams struct {
+	TokenAmountOut TokenAmount
+	TokenIn        string
+	Limit          SwapLimit
+}
+
+type CalcAmountInResult struct {
+	TokenAmountIn *TokenAmount
+	Fee           *TokenAmount
+	Gas           int64
+	SwapInfo      interface{}
+}
+
 // wrap around pool.CalcAmountOut and catch panic
 func CalcAmountOut(pool IPoolSimulator, tokenAmountIn TokenAmount, tokenOut string, limit SwapLimit) (res *CalcAmountOutResult, err error) {
 	defer func() {
@@ -152,4 +168,142 @@ func CalcAmountOut(pool IPoolSimulator, tokenAmountIn TokenAmount, tokenOut stri
 			TokenOut:      tokenOut,
 			Limit:         limit,
 		})
+}
+
+type ApproxAmountInParams struct {
+	ExpectedTokenOut TokenAmount
+	TokenIn          string
+	Limit            SwapLimit
+	MaxLoop          int
+	Threshold        *big.Int
+}
+
+type ApproxAmountInResult struct {
+	TokenAmountIn  *TokenAmount
+	TokenAmountOut *TokenAmount
+	Fee            *TokenAmount
+	Gas            int64
+	SwapInfo       interface{}
+}
+
+func ApproxAmountIn(
+	pool IPoolSimulator,
+	param ApproxAmountInParams,
+) (*ApproxAmountInResult, error) {
+
+	expectedTokenOut := param.ExpectedTokenOut
+	expectedAmountOut := param.ExpectedTokenOut.Amount
+	thresholdBI := param.Threshold
+
+	if expectedAmountOut.Cmp(big.NewInt(0)) == 0 {
+		return nil, fmt.Errorf("expectedAmountOut is zero")
+	}
+
+	// if the pool support calculate directly then use it
+	revSim, ok := pool.(IPoolExactOutSimulator)
+	if ok {
+		resIn, err := revSim.CalcAmountIn(CalcAmountInParams{
+			TokenAmountOut: expectedTokenOut,
+			TokenIn:        param.TokenIn,
+			Limit:          param.Limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		// still need to check again to see if the calculated amountIn is good enough
+		resOut, err := CalcAmountOut(pool, *resIn.TokenAmountIn, expectedTokenOut.Token, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		diff := new(big.Int).Abs(new(big.Int).Sub(resOut.TokenAmountOut.Amount, expectedAmountOut))
+		if diff.Cmp(thresholdBI) < 0 {
+			return &ApproxAmountInResult{
+				TokenAmountIn:  resIn.TokenAmountIn,
+				TokenAmountOut: resOut.TokenAmountOut,
+				Fee:            resOut.Fee,
+				Gas:            resOut.Gas,
+				SwapInfo:       resOut.SwapInfo,
+			}, nil
+		}
+		return nil, ErrInsufficientAmount
+	}
+
+	// otherwise try to approximate:
+	// consider the pool as a function `fpool`
+	// we need to find amountIn such that fpool(amountIn) = amountOut
+	// in another word, we need to find root of function fpool_1 where fpool_1(x) = fpool(x) - amountOut
+	// here we'll use https://en.wikipedia.org/wiki/Secant_method for that
+
+	// get 1st initial point by converting back expectedAmountOut to tokenIn
+	// this might yield error if the pool doesn't support that
+	x0res, err := CalcAmountOut(pool, expectedTokenOut, param.TokenIn, param.Limit)
+	if err != nil {
+		logger.Debugf("error getting 1st initial point %v", err)
+		return nil, err
+	}
+	x0 := x0res.TokenAmountOut.Amount
+
+	// get the 2nd initial point:
+	// 	- convert x0 tokenIn to tokenOut
+	fx0Res, err := CalcAmountOut(pool, *x0res.TokenAmountOut, expectedTokenOut.Token, param.Limit)
+	if err != nil {
+		logger.Debugf("error getting 2nd initial point %v", err)
+		return nil, err
+	}
+	if fx0Res == nil || fx0Res.TokenAmountOut.Amount.Cmp(big.NewInt(0)) == 0 {
+		logger.Debugf("error getting 2nd initial point %v", fx0Res)
+		return nil, ErrInsufficientAmount
+	}
+	// - then convert back
+	x1 := new(big.Int).Div(
+		new(big.Int).Mul(expectedAmountOut, x0),
+		fx0Res.TokenAmountOut.Amount,
+	)
+
+	fx0 := new(big.Int).Sub(fx0Res.TokenAmountOut.Amount, expectedAmountOut)
+
+	// Secant
+	loopCount := 0
+	for loopCount < param.MaxLoop {
+		// fpool_1(x1) = fpool(x1) - amountOut
+		fx1Res, err := CalcAmountOut(pool, TokenAmount{Token: param.TokenIn, Amount: x1}, expectedTokenOut.Token, param.Limit)
+		if err != nil {
+			logger.Debugf("error calculating fx1 %v", err)
+			return nil, err
+		}
+		fx1 := new(big.Int).Sub(fx1Res.TokenAmountOut.Amount, expectedAmountOut)
+
+		// check if we're close enough
+		if new(big.Int).Abs(fx1).Cmp(thresholdBI) < 0 {
+			return &ApproxAmountInResult{
+				TokenAmountIn:  &TokenAmount{Token: param.TokenIn, Amount: x1},
+				TokenAmountOut: fx1Res.TokenAmountOut,
+				Fee:            fx1Res.Fee,
+				Gas:            fx1Res.Gas,
+				SwapInfo:       fx1Res.SwapInfo,
+			}, nil
+		}
+
+		// if fx0 is close enough to fx1 we should stop to avoid error
+		// (and because we likely won't be able to get any closer to the result)
+		if fx0.Cmp(fx1) == 0 {
+			logger.Debugf("breaking early, fx0=fx1=%v", fx0)
+			return nil, ErrNotConverge
+		}
+
+		// get new value for x0 x1
+		nom := new(big.Int).Mul(fx1, new(big.Int).Sub(x1, x0))
+		denom := new(big.Int).Sub(fx1, fx0)
+		frac, _ := new(big.Float).Quo(new(big.Float).SetInt(nom), new(big.Float).SetInt(denom)).Int(nil)
+
+		x2 := new(big.Int).Sub(x1, frac)
+		x0, x1 = x1, x2
+		fx0 = fx1
+
+		loopCount += 1
+	}
+
+	return nil, ErrNotConverge
 }
