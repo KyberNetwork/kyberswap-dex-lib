9e9b5cd81a99a50c41b0e352b2f8c3b1bc0f9d18
le.cao <le.cao@kyber.network>
2024-01-08 10:24:22 +0700

    fix lint

--- pkg/source/dodo/embed.go
+++ pkg/source/dodo/embed.go
@@ -7,10 +7,3 @@ var v1PoolData []byte
 
 //go:embed abi/DodoV2Pool.json
 var v2PoolData []byte
-
-//go:embed blacklist/bsc.txt
-var bscBlacklistFilePath []byte
-
-var bytesByPath = map[string][]byte{
-	"blacklist/bsc.txt": bscBlacklistFilePath,
-}
