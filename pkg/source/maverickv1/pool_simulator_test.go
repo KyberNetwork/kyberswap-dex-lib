fdfb8e0f0541b5b0aeb16116be04adb3b075ae95
le.cao <le.cao@kyber.network>
2024-01-11 15:24:20 +0700

    fix bin update for maverick

--- pkg/source/maverickv1/pool_simulator_test.go
+++ pkg/source/maverickv1/pool_simulator_test.go
@@ -1,19 +1,20 @@
-package maverickv1_test
+package maverickv1
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
-	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/maverickv1"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
 )
 
-var maverickPool, err = maverickv1.NewPoolSimulator(entity.Pool{
+var maverickPool, err = NewPoolSimulator(entity.Pool{
 	Tokens: []*entity.PoolToken{
 		{
 			Address:  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
@@ -335,7 +336,7 @@ func BenchmarkNextActive(b *testing.B) {
 	err := json.Unmarshal([]byte(poolRedis), &poolEnt)
 	require.Nil(b, err)
 
-	maverickPool, err := maverickv1.NewPoolSimulator(poolEnt)
+	maverickPool, err := NewPoolSimulator(poolEnt)
 	require.Nil(b, err)
 
 	for i := 0; i < b.N; i++ {
@@ -346,6 +347,123 @@ func BenchmarkNextActive(b *testing.B) {
 			},
 			TokenOut: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
 			Limit:    nil,
+		})
+	}
+}
+
+func TestEmptyPool(t *testing.T) {
+	poolRedis := `{
+    "address": "0xccd9eb9480f7beaa2bcac7d0cf5d4143f328ac06",
+    "swapFee": 0.001,
+    "exchange": "maverick-v1",
+    "type": "maverick-v1",
+    "timestamp": 1704940286,
+    "reserves": ["0", "0"],
+    "tokens": [
+      { "address": "0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "name": "", "symbol": "", "decimals": 18, "weight": 50, "swappable": true },
+      { "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "name": "", "symbol": "", "decimals": 6, "weight": 50, "swappable": true }
+    ],
+    "extra": "{\"fee\":1000000000000000,\"protocolFeeRatio\":0,\"activeTick\":-1470,\"binCounter\":1,\"bins\":{},\"binPositions\":{},\"binMap\":{},\"binMapHex\":{},\"liquidity\":0,\"sqrtPriceX96\":479523079949611800}",
+    "staticExtra": "{\"tickSpacing\":10}"
+  }`
+	var poolEnt entity.Pool
+	err := json.Unmarshal([]byte(poolRedis), &poolEnt)
+	require.Nil(t, err)
+
+	_, err = NewPoolSimulator(poolEnt)
+	assert.True(t, errors.Is(err, ErrEmptyBins))
+}
+
+func TestUpdateBalance(t *testing.T) {
+	poolRedis := `{"address":"0x5fdf78aef906cbad032fbaea032aaae3accf9dc3","reserveUsd":47625.963767453606,"amplifiedTvl":2.0145226157464416e+41,"swapFee":0.0005,"exchange":"maverick-v1","type":"maverick-v1","timestamp":1704957203,"reserves":["108363845032166910770488","2097024497432052549"],"tokens":[{"address":"0x04506dddbf689714487f91ae1397047169afcf34","decimals":18,"weight":50,"swappable":true},{"address":"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd","decimals":18,"weight":50,"swappable":true}],"extra":"{\"fee\":500000000000000,\"protocolFeeRatio\":0,\"activeTick\":10,\"binCounter\":18,\"bins\":{\"1\":{\"reserveA\":1880866557485545835609,\"reserveB\":0,\"lowerTick\":-5,\"kind\":0,\"mergeId\":0},\"10\":{\"reserveA\":2013495774191474777406,\"reserveB\":0,\"lowerTick\":4,\"kind\":0,\"mergeId\":0},\"11\":{\"reserveA\":411993441413380258157,\"reserveB\":0,\"lowerTick\":5,\"kind\":0,\"mergeId\":0},\"12\":{\"reserveA\":491298562692665969507,\"reserveB\":0,\"lowerTick\":6,\"kind\":0,\"mergeId\":0},\"13\":{\"reserveA\":620606767055018215315,\"reserveB\":0,\"lowerTick\":7,\"kind\":0,\"mergeId\":0},\"14\":{\"reserveA\":725257522405584599699,\"reserveB\":0,\"lowerTick\":8,\"kind\":0,\"mergeId\":0},\"15\":{\"reserveA\":897478209865575805530,\"reserveB\":0,\"lowerTick\":9,\"kind\":0,\"mergeId\":0},\"16\":{\"reserveA\":2142944919078882824342,\"reserveB\":0,\"lowerTick\":-6,\"kind\":0,\"mergeId\":0},\"17\":{\"reserveA\":1022668409565365293976,\"reserveB\":2097024497432052514,\"lowerTick\":10,\"kind\":0,\"mergeId\":0},\"2\":{\"reserveA\":1634106566195962389560,\"reserveB\":0,\"lowerTick\":-4,\"kind\":0,\"mergeId\":0},\"3\":{\"reserveA\":1405424035812355050009,\"reserveB\":0,\"lowerTick\":-3,\"kind\":0,\"mergeId\":0},\"4\":{\"reserveA\":1233705168748319240144,\"reserveB\":0,\"lowerTick\":-2,\"kind\":0,\"mergeId\":0},\"5\":{\"reserveA\":47686688533077328269486,\"reserveB\":0,\"lowerTick\":-1,\"kind\":0,\"mergeId\":0},\"6\":{\"reserveA\":30071745509492793533770,\"reserveB\":0,\"lowerTick\":0,\"kind\":0,\"mergeId\":0},\"7\":{\"reserveA\":6925596663250336094803,\"reserveB\":0,\"lowerTick\":1,\"kind\":0,\"mergeId\":0},\"8\":{\"reserveA\":5442282585416271863178,\"reserveB\":0,\"lowerTick\":2,\"kind\":0,\"mergeId\":0},\"9\":{\"reserveA\":3757685806420050749903,\"reserveB\":0,\"lowerTick\":3,\"kind\":0,\"mergeId\":0}},\"binPositions\":{\"-1\":{\"0\":5},\"-2\":{\"0\":4},\"-3\":{\"0\":3},\"-4\":{\"0\":2},\"-5\":{\"0\":1},\"-6\":{\"0\":16},\"0\":{\"0\":6},\"1\":{\"0\":7},\"10\":{\"0\":17},\"2\":{\"0\":8},\"3\":{\"0\":9},\"4\":{\"0\":10},\"5\":{\"0\":11},\"6\":{\"0\":12},\"7\":{\"0\":13},\"8\":{\"0\":14},\"9\":{\"0\":15}},\"binMap\":{\"-1\":7719472155704656575533813171595469705082968814302106124604735256359260389376,\"0\":1172812402961},\"binMapHex\":{\"-1\":7719472155704656575533813171595469705082968814302106124604735256359260389376,\"0\":1172812402961},\"liquidity\":399352711841249002240879,\"sqrtPriceX96\":1027874653953720738}","staticExtra":"{\"tickSpacing\":50}"}`
+	var poolEnt entity.Pool
+	err := json.Unmarshal([]byte(poolRedis), &poolEnt)
+	require.Nil(t, err)
+
+	sim, err := NewPoolSimulator(poolEnt)
+	require.Nil(t, err)
+
+	testCases := []struct {
+		tokenIn      string
+		tokenOut     string
+		amountIn     string
+		expAmountOut string
+	}{
+		{"0x04506dddbf689714487f91ae1397047169afcf34", "0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "1000000000000000000", "946022415423519310"},
+		{"0x04506dddbf689714487f91ae1397047169afcf34", "0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "500000000000000000", "473009480095110835"},
+		{"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "0x04506dddbf689714487f91ae1397047169afcf34", "200000000000000000", "211201042322131096"},
+		{"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "0x04506dddbf689714487f91ae1397047169afcf34", "900000000000000000", "950402000485391540"},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test %d", i), func(t *testing.T) {
+			in := pool.TokenAmount{
+				Token:  tc.tokenIn,
+				Amount: bignumber.NewBig10(tc.amountIn),
+			}
+			result, err := sim.CalcAmountOut(pool.CalcAmountOutParams{
+				TokenAmountIn: in,
+				TokenOut:      tc.tokenOut,
+				Limit:         nil,
+			})
+			require.Nil(t, err)
+			require.Equal(t, tc.expAmountOut, result.TokenAmountOut.Amount.String())
+
+			updateBalanceParams := pool.UpdateBalanceParams{
+				TokenAmountIn:  in,
+				TokenAmountOut: *result.TokenAmountOut,
+				Fee:            *result.Fee,
+				SwapInfo:       result.SwapInfo,
+			}
+			sim.UpdateBalance(updateBalanceParams)
+		})
+	}
+}
+
+func TestUpdateBalanceNextTick(t *testing.T) {
+	poolRedis := `{"address":"0xd50c68c7fbaee4f469e04cebdcfbf1113b4cdadf","reserveUsd":52056.74739685542,"amplifiedTvl":3.641901122084877e+44,"swapFee":0.01,"exchange":"maverick-v1","type":"maverick-v1","timestamp":1704959580,"reserves":["13095016099313357610018","26336470622025877177"],"tokens":[{"address":"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd","decimals":18,"weight":50,"swappable":true},{"address":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","decimals":18,"weight":50,"swappable":true}],"extra":"{\"fee\":10000000000000000,\"protocolFeeRatio\":0,\"activeTick\":433,\"binCounter\":55,\"bins\":{\"1\":{\"reserveA\":0,\"reserveB\":91157437341918885,\"lowerTick\":434,\"kind\":3,\"mergeId\":0},\"10\":{\"reserveA\":0,\"reserveB\":1239975611309030976,\"lowerTick\":447,\"kind\":0,\"mergeId\":0},\"11\":{\"reserveA\":0,\"reserveB\":1141328409485710165,\"lowerTick\":448,\"kind\":0,\"mergeId\":0},\"12\":{\"reserveA\":0,\"reserveB\":1090262378803153846,\"lowerTick\":449,\"kind\":0,\"mergeId\":0},\"13\":{\"reserveA\":0,\"reserveB\":1058956124010387881,\"lowerTick\":450,\"kind\":0,\"mergeId\":0},\"14\":{\"reserveA\":0,\"reserveB\":373789640683233838,\"lowerTick\":451,\"kind\":0,\"mergeId\":0},\"15\":{\"reserveA\":0,\"reserveB\":357135930104930106,\"lowerTick\":452,\"kind\":0,\"mergeId\":0},\"16\":{\"reserveA\":0,\"reserveB\":338216301716701110,\"lowerTick\":453,\"kind\":0,\"mergeId\":0},\"17\":{\"reserveA\":0,\"reserveB\":320760828172399247,\"lowerTick\":454,\"kind\":0,\"mergeId\":0},\"18\":{\"reserveA\":0,\"reserveB\":313627396754223600,\"lowerTick\":455,\"kind\":0,\"mergeId\":0},\"19\":{\"reserveA\":0,\"reserveB\":307794867765749325,\"lowerTick\":456,\"kind\":0,\"mergeId\":0},\"20\":{\"reserveA\":0,\"reserveB\":298219676795168630,\"lowerTick\":457,\"kind\":0,\"mergeId\":0},\"21\":{\"reserveA\":0,\"reserveB\":294481079018034973,\"lowerTick\":458,\"kind\":0,\"mergeId\":0},\"22\":{\"reserveA\":0,\"reserveB\":2773410496175111720,\"lowerTick\":455,\"kind\":1,\"mergeId\":0},\"29\":{\"reserveA\":1609295705362818753486,\"reserveB\":0,\"lowerTick\":426,\"kind\":0,\"mergeId\":0},\"3\":{\"reserveA\":0,\"reserveB\":1598645710773758142,\"lowerTick\":440,\"kind\":0,\"mergeId\":0},\"30\":{\"reserveA\":1713963852223063753018,\"reserveB\":0,\"lowerTick\":427,\"kind\":0,\"mergeId\":0},\"31\":{\"reserveA\":1796069142786354277710,\"reserveB\":0,\"lowerTick\":428,\"kind\":0,\"mergeId\":0},\"32\":{\"reserveA\":1739178743674569940344,\"reserveB\":0,\"lowerTick\":429,\"kind\":0,\"mergeId\":0},\"33\":{\"reserveA\":1677169405367640113180,\"reserveB\":0,\"lowerTick\":430,\"kind\":0,\"mergeId\":0},\"34\":{\"reserveA\":1771627492707184684589,\"reserveB\":0,\"lowerTick\":431,\"kind\":0,\"mergeId\":0},\"35\":{\"reserveA\":1872397651819441245354,\"reserveB\":0,\"lowerTick\":432,\"kind\":0,\"mergeId\":0},\"36\":{\"reserveA\":915314105372284841505,\"reserveB\":217573456205038785,\"lowerTick\":433,\"kind\":0,\"mergeId\":0},\"37\":{\"reserveA\":0,\"reserveB\":423191801919726618,\"lowerTick\":434,\"kind\":0,\"mergeId\":0},\"38\":{\"reserveA\":0,\"reserveB\":425853602425037489,\"lowerTick\":435,\"kind\":0,\"mergeId\":0},\"39\":{\"reserveA\":0,\"reserveB\":434870246675316320,\"lowerTick\":436,\"kind\":0,\"mergeId\":0},\"4\":{\"reserveA\":0,\"reserveB\":1747275673731363525,\"lowerTick\":441,\"kind\":0,\"mergeId\":0},\"40\":{\"reserveA\":0,\"reserveB\":428431372113458941,\"lowerTick\":437,\"kind\":0,\"mergeId\":0},\"41\":{\"reserveA\":0,\"reserveB\":1032183470388298339,\"lowerTick\":438,\"kind\":0,\"mergeId\":0},\"42\":{\"reserveA\":0,\"reserveB\":985419259209570776,\"lowerTick\":439,\"kind\":0,\"mergeId\":0},\"43\":{\"reserveA\":0,\"reserveB\":242182693866501568,\"lowerTick\":459,\"kind\":0,\"mergeId\":0},\"44\":{\"reserveA\":0,\"reserveB\":239797032983525254,\"lowerTick\":460,\"kind\":0,\"mergeId\":0},\"45\":{\"reserveA\":0,\"reserveB\":237434872449643063,\"lowerTick\":461,\"kind\":0,\"mergeId\":0},\"46\":{\"reserveA\":0,\"reserveB\":235095980770748030,\"lowerTick\":462,\"kind\":0,\"mergeId\":0},\"47\":{\"reserveA\":0,\"reserveB\":232780128733125118,\"lowerTick\":463,\"kind\":0,\"mergeId\":0},\"48\":{\"reserveA\":0,\"reserveB\":230487089380947529,\"lowerTick\":464,\"kind\":0,\"mergeId\":0},\"49\":{\"reserveA\":0,\"reserveB\":225934471614129577,\"lowerTick\":465,\"kind\":0,\"mergeId\":0},\"5\":{\"reserveA\":0,\"reserveB\":1461855769991903582,\"lowerTick\":442,\"kind\":0,\"mergeId\":0},\"50\":{\"reserveA\":0,\"reserveB\":221901118128804058,\"lowerTick\":466,\"kind\":0,\"mergeId\":0},\"6\":{\"reserveA\":0,\"reserveB\":1480772137646768965,\"lowerTick\":443,\"kind\":0,\"mergeId\":0},\"7\":{\"reserveA\":0,\"reserveB\":1324401440806630210,\"lowerTick\":444,\"kind\":0,\"mergeId\":0},\"8\":{\"reserveA\":0,\"reserveB\":1411870610798131358,\"lowerTick\":445,\"kind\":0,\"mergeId\":0},\"9\":{\"reserveA\":0,\"reserveB\":1499396503277694755,\"lowerTick\":446,\"kind\":0,\"mergeId\":0}},\"binPositions\":{\"426\":{\"0\":29},\"427\":{\"0\":30},\"428\":{\"0\":31},\"429\":{\"0\":32},\"430\":{\"0\":33},\"431\":{\"0\":34},\"432\":{\"0\":35},\"433\":{\"0\":36},\"434\":{\"0\":37,\"3\":1},\"435\":{\"0\":38},\"436\":{\"0\":39},\"437\":{\"0\":40},\"438\":{\"0\":41},\"439\":{\"0\":42},\"440\":{\"0\":3},\"441\":{\"0\":4},\"442\":{\"0\":5},\"443\":{\"0\":6},\"444\":{\"0\":7},\"445\":{\"0\":8},\"446\":{\"0\":9},\"447\":{\"0\":10},\"448\":{\"0\":11},\"449\":{\"0\":12},\"450\":{\"0\":13},\"451\":{\"0\":14},\"452\":{\"0\":15},\"453\":{\"0\":16},\"454\":{\"0\":17},\"455\":{\"0\":18,\"1\":22},\"456\":{\"0\":19},\"457\":{\"0\":20},\"458\":{\"0\":21},\"459\":{\"0\":43},\"460\":{\"0\":44},\"461\":{\"0\":45},\"462\":{\"0\":46},\"463\":{\"0\":47},\"464\":{\"0\":48},\"465\":{\"0\":49},\"466\":{\"0\":50}},\"binMap\":{\"6\":7719472615821092550409086380891770248800661236333969968563225660742308986880,\"7\":5037190915061491765521},\"binMapHex\":{\"6\":7719472615821092550409086380891770248800661236333969968563225660742308986880,\"7\":5037190915061491765521},\"liquidity\":2878336312141942146424,\"sqrtPriceX96\":73028492082257348963}","staticExtra":"{\"tickSpacing\":198}"}`
+	var poolEnt entity.Pool
+	err := json.Unmarshal([]byte(poolRedis), &poolEnt)
+	require.Nil(t, err)
+
+	sim, err := NewPoolSimulator(poolEnt)
+	require.Nil(t, err)
+
+	testCases := []struct {
+		tokenIn      string
+		tokenOut     string
+		amountIn     string
+		expAmountOut string
+		expNextTick  string
+	}{
+		{"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "10000000000000000000000", "1784415750858931428", "437"},
+		{"0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "50000000000000000000000", "7995459204101958875", "443"},
+		{"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "5000000000000000000", "31297333580169335152628", "440"},
+		{"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0x7448c7456a97769f6cd04f1e83a4a23ccdc46abd", "900000000000000000", "5437425224138046326996", "439"},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test %d", i), func(t *testing.T) {
+			in := pool.TokenAmount{
+				Token:  tc.tokenIn,
+				Amount: bignumber.NewBig10(tc.amountIn),
+			}
+			result, err := sim.CalcAmountOut(pool.CalcAmountOutParams{
+				TokenAmountIn: in,
+				TokenOut:      tc.tokenOut,
+				Limit:         nil,
+			})
+			require.Nil(t, err)
+			require.Equal(t, tc.expAmountOut, result.TokenAmountOut.Amount.String())
+			require.Equal(t, tc.expNextTick, result.SwapInfo.(maverickSwapInfo).activeTick.String())
+
+			updateBalanceParams := pool.UpdateBalanceParams{
+				TokenAmountIn:  in,
+				TokenAmountOut: *result.TokenAmountOut,
+				Fee:            *result.Fee,
+				SwapInfo:       result.SwapInfo,
+			}
+			sim.UpdateBalance(updateBalanceParams)
 		})
 	}
 }
