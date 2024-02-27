c5fc1adfffcbc9f39f608bd021d0e3e8a181ef38
le.cao <le.cao@kyber.network>
2024-02-27 17:59:20 +0700

    add shared common Curve code: pool list & classify

---
+++ pkg/liquidity-source/curve/shared/errors.go
@@ -0,0 +1,7 @@
+package shared
+
+import "errors"
+
+var (
+	ErrGetPoolsFailed = errors.New("getPools failed")
+)
---
+++ pkg/liquidity-source/curve/shared/type.go
@@ -0,0 +1,76 @@
+package shared
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/KyberNetwork/logger"
+)
+
+type (
+	CurveCoin struct {
+		Address           string
+		Decimals          interface{}
+		IsBasePoolLpToken bool
+		Symbol            string
+	}
+
+	CurvePool struct {
+		Id             string
+		Address        string
+		Coins          []CurveCoin
+		Name           string
+		Implementation string
+		LpTokenAddress string
+		IsMetaPool     bool
+	}
+
+	GetPoolsResult struct {
+		Success bool
+		Data    struct {
+			PoolData []CurvePool
+		}
+	}
+
+	GetRegistryAddressResult struct {
+		Success bool
+		Data    struct {
+			RegistryAddress string
+		}
+	}
+
+	CurvePoolWithType struct {
+		CurvePool
+		PoolType CurvePoolType
+	}
+
+	PoolListUpdaterMetadata struct {
+		LastRun time.Time
+	}
+
+	CurvePoolType   string
+	CurveDataSource string
+)
+
+func (c *CurveCoin) GetDecimals() uint8 {
+	switch v := c.Decimals.(type) {
+	case float64:
+		return uint8(v)
+	case string:
+		dec, err := strconv.ParseInt(v, 10, 0)
+		if err != nil {
+			logger.Errorf("curve coin with invalid decimal %v %v", c.Address, c.Decimals)
+			return 0
+		}
+		return uint8(dec)
+	default:
+		logger.Errorf("curve coin with invalid decimal %v %v", c.Address, c.Decimals)
+		return 0
+	}
+}
+
+func (m PoolListUpdaterMetadata) ToBytes() []byte {
+	b, _ := json.Marshal(m)
+	return b
+}
