edbb939ad3e7dba2e5c29da232d80e9932607476
Ha Nguyen <ha.nguyenk07@gmail.com>
2024-02-28 21:23:57 +0700

    feat: integrate reth

---
+++ pkg/liquidity-source/rocketpool/reth/constant.go
@@ -0,0 +1,10 @@
+package reth
+
+const (
+	RocketDepositPool                = "0xdd3f50f8a6cafbe9b31a427582963f465e745af8"
+	RocketDAOProtocolSettingsDeposit = "0xac2245be4c2c1e9752499bcd34861b761d62fc27"
+	RocketVault                      = "0x3bdc69c4e5e13e52a65f5583c23efb9636b469d6"
+	RocketMinipoolQueue              = "0x9e966733e3e9bfa56af95f762921859417cf6faaa"
+	RocketNetworkBalances            = "0x07fcabcbe4ff0d80c2b1eb42855c0131b6cba2f4"
+	RocketTokenRETH                  = "0xae78736cd615f374d3085123a210448e74fc6393"
+)
