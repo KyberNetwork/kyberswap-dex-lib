bac73010769bfe32ac83df2432c1b69a62e59494
Dat Luong <dat.luong@kyber.network>
2024-02-29 17:24:41 +0700

    feat: unit test for poolSim

--- pkg/liquidity-source/bancor-v21/pool_simulator_test.go
+++ pkg/liquidity-source/bancor-v21/pool_simulator_test.go
@@ -40,5 +40,14 @@ func TestPoolSimulator_CalcAmountOut(t *testing.T) {
 	samplePath := []string{eth, anchor1, bnt}
 	result, err := poolSim.rateByPath(samplePath, new(big.Int).SetUint64(1000))
 	assert.Nil(t, err)
-	t.Log(result.String())
+	assert.Equal(t, result.Uint64(), 3898108)
+
+	samplePath2 := []string{
+		strings.ToLower("0x4Fabb145d64652a948d72533023f6E7A623C7C53"),
+		strings.ToLower("0x7b86306D72103Ccd5405DF9dBFf4B794C46EBbC9"),
+		strings.ToLower("0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C"),
+		strings.ToLower("0xE6b31fB3f29fbde1b92794B0867A315Ff605A324"),
+		strings.ToLower("0xB8c77482e45F1F44dE1745F52C74426C631bDD52")}
+	result, err = poolSim.rateByPath(samplePath2, new(big.Int).SetUint64(1000))
+	assert.Equal(t, err, ErrInvalidReserve)
 }
