b139c28add0e4d234bb58be8a125ac124bf50c6c
Quang Le <levuquang2k1@gmail.com>
2024-03-06 17:40:46 +0700

    fix: swaap-v2 (#374)

--- pkg/liquidity-source/swaap-v2/pool_simulator.go
+++ pkg/liquidity-source/swaap-v2/pool_simulator.go
@@ -1,14 +1,13 @@
 package swaapv2
 
 import (
-	"encoding/json"
 	"errors"
+	"math"
 	"math/big"
 	"strings"
 
-	"github.com/KyberNetwork/blockchain-toolkit/float"
 	"github.com/KyberNetwork/blockchain-toolkit/integer"
-	"github.com/KyberNetwork/blockchain-toolkit/unit"
+	"github.com/goccy/go-json"
 	"github.com/samber/lo"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
@@ -19,22 +18,22 @@ import (
 var (
 	ErrEmptyPriceLevels      = errors.New("empty price levels")
 	ErrInsufficientLiquidity = errors.New("insufficient liquidity")
+	ErrPoolSwapped           = errors.New("pool swapped")
+	ErrOutOfLiquidity        = errors.New("out of liquidity")
 )
 
 type (
 	PoolSimulator struct {
 		pool.Pool
-
+		isBaseSwapped          bool
+		isQuoteSwapped         bool
 		baseToken              *entity.PoolToken
 		quoteToken             *entity.PoolToken
 		baseToQuotePriceLevels []PriceLevel
 		quoteToBasePriceLevels []PriceLevel
-
-		timestamp int64
-
-		priceTolerance *big.Int
-
-		gas Gas
+		timestamp              int64
+		priceTolerance         float64
+		gas                    Gas
 	}
 
 	MetaInfo struct {
@@ -42,8 +41,8 @@ type (
 	}
 
 	PriceLevel struct {
-		Price  float64 `json:"price"`
-		Amount float64 `json:"amount"`
+		Price float64 `json:"price"`
+		Level float64 `json:"level"`
 	}
 
 	Gas struct {
@@ -80,37 +79,32 @@ func NewPoolSimulator(entityPool entity.Pool) (*PoolSimulator, error) {
 				Reserves:   lo.Map(entityPool.Reserves, func(item string, index int) *big.Int { return bignumber.NewBig(item) }),
 			},
 		},
+		isBaseSwapped:          false,
+		isQuoteSwapped:         false,
 		baseToken:              entityPool.Tokens[0],
 		quoteToken:             entityPool.Tokens[1],
 		baseToQuotePriceLevels: extra.BaseToQuotePriceLevels,
 		quoteToBasePriceLevels: extra.QuoteToBasePriceLevels,
-
-		timestamp: entityPool.Timestamp,
-
-		priceTolerance: big.NewInt(int64(extra.PriceTolerance)),
-
-		gas: DefaultGas,
+		timestamp:              entityPool.Timestamp,
+		priceTolerance:         float64(extra.PriceTolerance),
+		gas:                    DefaultGas,
 	}, nil
 }
 
 func (p *PoolSimulator) CalcAmountOut(params pool.CalcAmountOutParams) (*pool.CalcAmountOutResult, error) {
 	if params.TokenAmountIn.Token == p.baseToken.Address {
 		return p.swapBaseToQuote(params.TokenAmountIn.Amount)
-	} else {
-		return p.swapQuoteToBase(params.TokenAmountIn.Amount)
 	}
+
+	return p.swapQuoteToBase(params.TokenAmountIn.Amount)
 }
 
 func (p *PoolSimulator) UpdateBalance(params pool.UpdateBalanceParams) {
 	if params.TokenAmountIn.Token == p.baseToken.Address {
-		amountInAfterDecimals := unit.ToDecimal(params.TokenAmountIn.Amount, p.baseToken.Decimals)
-
-		p.baseToQuotePriceLevels = getNewPriceLevelsState(amountInAfterDecimals, p.baseToQuotePriceLevels)
-	} else {
-		amountInAfterDecimals := unit.ToDecimal(params.TokenAmountIn.Amount, p.quoteToken.Decimals)
-
-		p.quoteToBasePriceLevels = getNewPriceLevelsState(amountInAfterDecimals, p.quoteToBasePriceLevels)
+		p.isBaseSwapped = true
+		return
 	}
+	p.isQuoteSwapped = true
 }
 
 func (p *PoolSimulator) GetMetaInfo(_ string, _ string) interface{} {
@@ -120,25 +114,22 @@ func (p *PoolSimulator) GetMetaInfo(_ string, _ string) interface{} {
 }
 
 func (p *PoolSimulator) swapBaseToQuote(amountIn *big.Int) (*pool.CalcAmountOutResult, error) {
-	amountInAfterDecimals := unit.ToDecimal(amountIn, p.baseToken.Decimals)
+	if p.isBaseSwapped {
+		return nil, ErrPoolSwapped
+	}
+
+	amountInFl, _ := amountIn.Float64()
+	amountInAfterDecimals := amountInFl / math.Pow10(int(p.baseToken.Decimals))
 
 	amountOutAfterDecimals, err := getAmountOut(amountInAfterDecimals, p.baseToQuotePriceLevels)
 	if err != nil {
 		return nil, err
 	}
 
-	amountOut, _ := new(big.Float).Mul(
-		amountOutAfterDecimals,
-		bignumber.TenPowDecimals(p.quoteToken.Decimals),
-	).Int(nil)
+	amountOutFl := amountOutAfterDecimals * math.Pow10(int(p.quoteToken.Decimals))
+	amountOutFl = amountOutFl * (priceToleranceBps - p.priceTolerance) / priceToleranceBps
 
-	amountOut = new(big.Int).Quo(
-		new(big.Int).Mul(
-			amountOut,
-			new(big.Int).Sub(priceToleranceBps, p.priceTolerance),
-		),
-		priceToleranceBps,
-	)
+	amountOut, _ := new(big.Float).SetFloat64(amountOutFl).Int(nil)
 
 	return &pool.CalcAmountOutResult{
 		TokenAmountOut: &pool.TokenAmount{Token: p.quoteToken.Address, Amount: amountOut},
@@ -153,25 +144,22 @@ func (p *PoolSimulator) swapBaseToQuote(amountIn *big.Int) (*pool.CalcAmountOutR
 }
 
 func (p *PoolSimulator) swapQuoteToBase(amountIn *big.Int) (*pool.CalcAmountOutResult, error) {
-	amountInAfterDecimals := unit.ToDecimal(amountIn, p.quoteToken.Decimals)
+	if p.isQuoteSwapped {
+		return nil, ErrPoolSwapped
+	}
+
+	amountInFl, _ := amountIn.Float64()
+	amountInAfterDecimals := amountInFl / math.Pow10(int(p.quoteToken.Decimals))
 
 	amountOutAfterDecimals, err := getAmountOut(amountInAfterDecimals, p.quoteToBasePriceLevels)
 	if err != nil {
 		return nil, err
 	}
 
-	amountOut, _ := new(big.Float).Mul(
-		amountOutAfterDecimals,
-		bignumber.TenPowDecimals(p.baseToken.Decimals),
-	).Int(nil)
+	amountOutFl := amountOutAfterDecimals * math.Pow10(int(p.baseToken.Decimals))
+	amountOutFl = amountOutFl * (priceToleranceBps - p.priceTolerance) / priceToleranceBps
 
-	amountOut = new(big.Int).Quo(
-		new(big.Int).Mul(
-			amountOut,
-			new(big.Int).Sub(priceToleranceBps, p.priceTolerance),
-		),
-		priceToleranceBps,
-	)
+	amountOut, _ := new(big.Float).SetFloat64(amountOutFl).Int(nil)
 
 	return &pool.CalcAmountOutResult{
 		TokenAmountOut: &pool.TokenAmount{Token: p.baseToken.Address, Amount: amountOut},
@@ -185,87 +173,34 @@ func (p *PoolSimulator) swapQuoteToBase(amountIn *big.Int) (*pool.CalcAmountOutR
 	}, nil
 }
 
-func getAmountOut(amountIn *big.Float, priceLevels []PriceLevel) (*big.Float, error) {
+func getAmountOut(amountIn float64, priceLevels []PriceLevel) (float64, error) {
 	if len(priceLevels) == 0 {
-		return nil, ErrEmptyPriceLevels
+		return 0, ErrEmptyPriceLevels
 	}
 
-	// Calculate the total available amount in the price levels
-	availableAmount := lo.Reduce(priceLevels, func(acc float64, priceLevel PriceLevel, _ int) float64 {
-		return acc + priceLevel.Amount
-	}, 0.0)
-
-	availableAmountBF := new(big.Float).SetFloat64(availableAmount)
-
-	// If the amount in is greater than the available amount that price levels can provide, return error insufficient liquidity
-	if amountIn.Cmp(availableAmountBF) > 0 {
-		return nil, ErrInsufficientLiquidity
+	if amountIn > priceLevels[len(priceLevels)-1].Level {
+		return 0, ErrOutOfLiquidity
 	}
 
-	amountOut := float.Zero()
-	amountInLeft := amountIn
-	currentLevelIdx := 0
+	var (
+		amountOut    = float64(0)
+		amountInLeft = amountIn
+		levelIdx     = 0
+	)
 
 	for {
-		currentLevelAvailableAmount := new(big.Float).SetFloat64(priceLevels[currentLevelIdx].Amount)
-		swappableAmount := currentLevelAvailableAmount
-
-		if currentLevelAvailableAmount.Cmp(amountInLeft) > 0 {
-			swappableAmount = amountInLeft
+		availableAmount := priceLevels[levelIdx].Level
+		if levelIdx > 0 {
+			availableAmount -= priceLevels[levelIdx-1].Level
 		}
-
-		amountOut = new(big.Float).Add(
-			amountOut,
-			new(big.Float).Mul(
-				swappableAmount, new(big.Float).SetFloat64(priceLevels[currentLevelIdx].Price),
-			),
-		)
-
-		amountInLeft = new(big.Float).Sub(amountInLeft, swappableAmount)
-		currentLevelIdx += 1
-
-		if amountInLeft.Cmp(float.Zero()) == 0 || currentLevelIdx > len(priceLevels)-1 {
+		swappableAmount := math.Min(availableAmount, amountInLeft)
+		amountOut += swappableAmount * priceLevels[levelIdx].Price
+		amountInLeft -= swappableAmount
+		levelIdx += 1
+		if amountInLeft == 0 || levelIdx >= len(priceLevels) {
 			break
 		}
 	}
 
 	return amountOut, nil
-}
-
-func getNewPriceLevelsState(
-	amountIn *big.Float,
-	priceLevels []PriceLevel,
-) []PriceLevel {
-	if len(priceLevels) == 0 {
-		return priceLevels
-	}
-
-	amountInLeft := amountIn
-	currentLevelIdx := 0
-
-	for {
-		currentLevelAvailableAmount := new(big.Float).SetFloat64(priceLevels[currentLevelIdx].Amount)
-		swappableAmount := currentLevelAvailableAmount
-
-		if currentLevelAvailableAmount.Cmp(amountInLeft) > 0 {
-			// Update the price level at the current step because it's partially filled
-			priceLevels[currentLevelIdx].Amount, _ = new(big.Float).Sub(currentLevelAvailableAmount, amountInLeft).Float64()
-
-			swappableAmount = amountInLeft
-		} else {
-			// Only increase the step if the current level is fully filled
-			currentLevelIdx += 1
-		}
-
-		amountInLeft = new(big.Float).Sub(amountInLeft, swappableAmount)
-
-		if amountInLeft.Cmp(float.Zero()) == 0 || currentLevelIdx > len(priceLevels)-1 {
-			// Get the remaining price levels
-			priceLevels = priceLevels[currentLevelIdx:]
-
-			break
-		}
-	}
-
-	return priceLevels
 }
--- pkg/liquidity-source/swaap-v2/pool_simulator_test.go
+++ pkg/liquidity-source/swaap-v2/pool_simulator_test.go
@@ -4,187 +4,65 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
+	poolpkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/testutil"
 )
 
-func TestPoolSimulator_getAmountOut(t *testing.T) {
-	type args struct {
-		amountIn    *big.Float
-		priceLevels []PriceLevel
-	}
-	tests := []struct {
-		name              string
-		args              args
-		expectedAmountOut *big.Float
-		expectedErr       error
-	}{
-		{
-			name: "it should return error when price levels is empty",
-			args: args{
-				amountIn:    new(big.Float).SetFloat64(1),
-				priceLevels: []PriceLevel{},
-			},
-			expectedAmountOut: nil,
-			expectedErr:       ErrEmptyPriceLevels,
-		},
-		{
-			name: "it should return insufficient liquidity error when the requested amount is greater than available amount in price levels",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(4),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-					{
-						Price:  99,
-						Amount: 2,
-					},
-				},
-			},
-			expectedAmountOut: nil,
-			expectedErr:       ErrInsufficientLiquidity,
-		},
-		{
-			name: "it should return correct amount out when fully filled",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(1),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-				},
-			},
-			expectedAmountOut: new(big.Float).SetFloat64(100),
-			expectedErr:       nil,
-		},
-		{
-			name: "it should return correct amount out when partially filled",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(2),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-					{
-						Price:  99,
-						Amount: 2,
-					},
-				},
-			},
-			expectedAmountOut: new(big.Float).SetFloat64(199),
-			expectedErr:       nil,
-		},
-	}
+func TestCalcAmountOut(t *testing.T) {
+	t.Run("1. should return OK", func(t *testing.T) {
+		p := `{
+			"address": "swaap_v2_0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2_0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+			"exchange": "swaap-v2",
+			"type": "swaap-v2",
+			"timestamp": 1709711042,
+			"reserves": [
+			  "952034231656045615",
+			  "1259118739"
+			],
+			"tokens": [
+			  {
+				"address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+				"symbol": "WETH",
+				"decimals": 18,
+				"swappable": true
+			  },
+			  {
+				"address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+				"symbol": "USDC",
+				"decimals": 6,
+				"swappable": true
+			  }
+			],
+			"extra": "{\"baseToQuotePriceLevels\":[{\"price\":3766.8762085558155,\"level\":0},{\"price\":3766.8762085558155,\"level\":0.0022288821657478614},{\"price\":3766.8490507666365,\"level\":0.01114440978035138},{\"price\":3766.8012247130564,\"level\":0.02228881956070276},{\"price\":3766.6965307059054,\"level\":0.0557220489017569},{\"price\":3766.4841226647645,\"level\":0.1114440978035138},{\"price\":3766.214521902419,\"level\":0.16716615928592582},{\"price\":3765.938538280598,\"level\":0.2228881956070276},{\"price\":3765.6575174450813,\"level\":0.2786102487023362},{\"price\":3765.3730881244423,\"level\":0.33433231857185164}],\"quoteToBasePriceLevels\":[{\"price\":0.00026532281648942546,\"level\":0},{\"price\":0.00026532281648942546,\"level\":17.941056366984068},{\"price\":0.0002653199249494233,\"level\":89.70605550508886},{\"price\":0.00026531464759751224,\"level\":179.41409087714086},{\"price\":0.0002653025212490239,\"level\":448.55049801735805},{\"price\":0.0002652764691449185,\"level\":897.1552285484877},{\"price\":0.00026524099477687863,\"level\":1345.8200585281966},{\"price\":0.00026520285689812795,\"level\":1794.54920677605},{\"price\":0.0002651613332028819,\"level\":2243.348760148422},{\"price\":0.0002651189910337773,\"level\":2692.2201264482296},{\"price\":0.0002650756079489333,\"level\":3141.1646861140907},{\"price\":0.0002650316307629569,\"level\":3590.1837399765}],\"priceTolerance\":10}"
+		  }`
+		var pool entity.Pool
+		err := json.Unmarshal([]byte(p), &pool)
+		assert.Nil(t, err)
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			amountOut, err := testutil.MustConcurrentSafe[*big.Float](t, func() (any, error) {
-				return getAmountOut(tt.args.amountIn, tt.args.priceLevels)
+		// expected amount
+		expectedAmountOut := "1257859620"
+
+		// calculation
+		simulator, err := NewPoolSimulator(pool)
+		assert.Nil(t, err)
+		amountIn, _ := new(big.Int).SetString("334332318571851640", 10)
+		result, err := testutil.MustConcurrentSafe[*poolpkg.CalcAmountOutResult](t, func() (any, error) {
+			return simulator.CalcAmountOut(poolpkg.CalcAmountOutParams{
+				TokenAmountIn: poolpkg.TokenAmount{
+					Token:  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+					Amount: amountIn,
+				},
+				TokenOut: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
 			})
-			assert.Equal(t, tt.expectedErr, err)
-
-			if amountOut != nil {
-				assert.Equal(t, tt.expectedAmountOut.Cmp(amountOut), 0)
-			}
 		})
-	}
-}
 
-func TestPoolSimulator_getNewPriceLevelsState(t *testing.T) {
-	type args struct {
-		amountIn    *big.Float
-		priceLevels []PriceLevel
-	}
-	tests := []struct {
-		name                string
-		args                args
-		expectedPriceLevels []PriceLevel
-	}{
-		{
-			name: "it should do nothing when price levels is empty",
-			args: args{
-				amountIn:    new(big.Float).SetFloat64(1),
-				priceLevels: []PriceLevel{},
-			},
-			expectedPriceLevels: []PriceLevel{},
-		},
-		{
-			name: "it should return correct new price levels when fully filled",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(1),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-				},
-			},
-			expectedPriceLevels: []PriceLevel{},
-		},
-		{
-			name: "it should return correct new price levels when the amountIn is greater than the amount available in the single price level",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(2),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-				},
-			},
-			expectedPriceLevels: []PriceLevel{},
-		},
-		{
-			name: "it should return correct new price levels when the amountIn is greater than the amount available in the all price levels",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(5),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-					{
-						Price:  99,
-						Amount: 2,
-					},
-				},
-			},
-			expectedPriceLevels: []PriceLevel{},
-		},
-		{
-			name: "it should return correct new price levels when partially filled",
-			args: args{
-				amountIn: new(big.Float).SetFloat64(2),
-				priceLevels: []PriceLevel{
-					{
-						Price:  100,
-						Amount: 1,
-					},
-					{
-						Price:  99,
-						Amount: 2,
-					},
-				},
-			},
-			expectedPriceLevels: []PriceLevel{
-				{
-					Price:  99,
-					Amount: 1,
-				},
-			},
-		},
-	}
+		// assert
+		assert.Nil(t, err)
+		assert.Equal(t, expectedAmountOut, result.TokenAmountOut.Amount.String())
+	})
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			newPriceLevels := getNewPriceLevelsState(tt.args.amountIn, tt.args.priceLevels)
-
-			assert.ElementsMatch(t, tt.expectedPriceLevels, newPriceLevels)
-		})
-	}
 }
