fbccda730bf47af448573538be42357f255378fe
Hoang Trinh <hoang.trinhj@gmail.com>
2024-04-16 18:40:15 +0700

    feat: implement CalcAmountIn for bancor-v3

--- pkg/liquidity-source/bancor-v3/pool_collection.go
+++ pkg/liquidity-source/bancor-v3/pool_collection.go
@@ -11,10 +11,11 @@ import (
 )
 
 var (
-	ErrInsufficientLiquidity = errors.New("insufficient liquidity")
-	ErrByTargetAmount        = errors.New("trade by target amount")
-	ErrDoesNotExit           = errors.New("does not exit")
-	ErrTradeDisabled         = errors.New("trade disabled")
+	ErrInsufficientLiquidity    = errors.New("insufficient liquidity")
+	ErrInsufficientTargetAmount = errors.New("insufficient target amount")
+	ErrInsufficientSourceAmount = errors.New("insufficient source amount")
+	ErrDoesNotExit              = errors.New("does not exit")
+	ErrTradeDisabled            = errors.New("trade disabled")
 )
 
 type (
@@ -69,6 +70,7 @@ type (
 	}
 )
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L816
 func (p *poolCollection) tradeBySourceAmount(
 	sourceToken,
 	targetToken string,
@@ -110,6 +112,52 @@ func (p *poolCollection) tradeBySourceAmount(
 	return &tradeAmountAndFee, &poolCollectionTradeInfo, nil
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L855
+func (p *poolCollection) tradeByTargetAmount(
+	sourceToken,
+	targetToken string,
+	targetAmount,
+	maxSourceAmount *uint256.Int,
+	ignoreFees bool,
+) (*tradeAmountAndFee, *poolCollectionTradeInfo, error) {
+	result, err := p._initTrade(
+		sourceToken,
+		targetToken,
+		targetAmount,
+		maxSourceAmount,
+		false,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if ignoreFees {
+		result.TradingFeePPM = number.Zero
+	}
+
+	newLiquidity, err := p._performTrade(result)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	tradeAmountAndFee := tradeAmountAndFee{
+		Amount:           result.SourceAmount,
+		TradingFeeAmount: result.TradingFeeAmount,
+		NetworkFeeAmount: result.NetworkFeeAmount,
+	}
+
+	poolCollectionTradeInfo := poolCollectionTradeInfo{
+		Pool:             result.Pool,
+		NewPoolLiquidity: newLiquidity,
+	}
+
+	return &tradeAmountAndFee, &poolCollectionTradeInfo, nil
+}
+
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1466
+/**
+ * @dev returns initial trading params
+ */
 func (p *poolCollection) _initTrade(
 	sourceToken,
 	targetToken string,
@@ -175,6 +223,10 @@ func (p *poolCollection) _initTrade(
 	return &result, nil
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1183
+/**
+ * @dev returns a storage reference to pool data
+ */
 func (p *poolCollection) _poolStorage(pool string) (*pool, error) {
 	data, ok := p.PoolData[pool]
 	if !ok {
@@ -183,6 +235,10 @@ func (p *poolCollection) _poolStorage(pool string) (*pool, error) {
 	return data, nil
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1648
+/**
+ * @dev performs a trade
+ */
 func (p *poolCollection) _performTrade(result *tradeIntermediateResult) (*poolLiquidity, error) {
 	if err := p._processTrade(result); err != nil {
 		return nil, err
@@ -202,25 +258,56 @@ func (p *poolCollection) _performTrade(result *tradeIntermediateResult) (*poolLi
 	return &newLiquidity, nil
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1564
+/**
+ * @dev processes a trade by providing either the source or the target amount and updates the in-memory intermediate
+ * result
+ */
 func (p *poolCollection) _processTrade(result *tradeIntermediateResult) error {
-	if !result.BySourceAmount {
-		return ErrByTargetAmount
-	}
-
-	tradeAmountAndFee, err := p._tradeAmountAndFeeBySourceAmount(
-		result.SourceBalance,
-		result.TargetBalance,
-		result.TradingFeePPM,
-		result.SourceAmount,
+	var (
+		tradeAmountAndFee *tradeAmountAndTradingFee
+		err               error
 	)
-	if err != nil {
-		return err
-	}
 
-	result.TargetAmount = tradeAmountAndFee.Amount
+	if result.BySourceAmount {
+		tradeAmountAndFee, err = p._tradeAmountAndFeeBySourceAmount(
+			result.SourceBalance,
+			result.TargetBalance,
+			result.TradingFeePPM,
+			result.SourceAmount,
+		)
+		if err != nil {
+			return err
+		}
+
+		result.TargetAmount = tradeAmountAndFee.Amount
+
+		// ensure that the target amount is above the requested minimum return amount
+		if result.TargetAmount.Cmp(result.Limit) < 0 {
+			return ErrInsufficientTargetAmount
+		}
+	} else {
+		tradeAmountAndFee, err = p._tradeAmountAndFeeByTargetAmount(
+			result.SourceBalance,
+			result.TargetBalance,
+			result.TradingFeePPM,
+			result.TargetAmount,
+		)
+		if err != nil {
+			return err
+		}
+
+		result.SourceAmount = tradeAmountAndFee.Amount
+
+		// ensure that the user has provided enough tokens to make the trade
+		if result.SourceAmount.Cmp(number.Zero) == 0 || result.SourceAmount.Cmp(result.Limit) > 0 {
+			return ErrInsufficientSourceAmount
+		}
+	}
 
 	result.TradingFeeAmount = tradeAmountAndFee.TradingFeeAmount
 
+	// sync the trading and staked balance
 	result.SourceBalance = new(uint256.Int).Add(result.SourceBalance, result.SourceAmount)
 	result.TargetBalance = new(uint256.Int).Sub(result.TargetBalance, result.TargetAmount)
 
@@ -231,6 +318,10 @@ func (p *poolCollection) _processTrade(result *tradeIntermediateResult) error {
 	return p._processNetworkFee(result)
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1613
+/**
+ * @dev processes the network fee and updates the in-memory intermediate result
+ */
 func (p *poolCollection) _processNetworkFee(result *tradeIntermediateResult) error {
 	if p.NetworkFeePMM.IsZero() {
 		return nil
@@ -270,6 +361,10 @@ func (p *poolCollection) _processNetworkFee(result *tradeIntermediateResult) err
 	return nil
 }
 
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1523
+/**
+ * @dev returns trade amount and fee by providing the source amount
+ */
 func (p *poolCollection) _tradeAmountAndFeeBySourceAmount(
 	sourceBalance,
 	targetBalance *uint256.Int,
@@ -294,6 +389,40 @@ func (p *poolCollection) _tradeAmountAndFeeBySourceAmount(
 
 	return &tradeAmountAndTradingFee{
 		Amount:           new(uint256.Int).Sub(targetAmount, tradingFeeAmount),
+		TradingFeeAmount: tradingFeeAmount,
+	}, nil
+}
+
+// https://github.com/bancorprotocol/contracts-v3/blob/4d3070bf3a759106ec8e6c9ab5ad5d71ff15f3ee/contracts/pools/PoolCollection.sol#L1543
+/**
+ * @dev returns trade amount and fee by providing either the target amount
+ */
+func (p *poolCollection) _tradeAmountAndFeeByTargetAmount(
+	sourceBalance,
+	targetBalance *uint256.Int,
+	feePPM *uint256.Int,
+	targetAmount *uint256.Int,
+) (*tradeAmountAndTradingFee, error) {
+	if sourceBalance.IsZero() {
+		return nil, ErrInsufficientLiquidity
+	}
+
+	tradingFeeAmount, err := math.MathEx.MulDivF(
+		targetAmount, feePPM, new(uint256.Int).Sub(pmmResolution, feePPM),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	fullTargetAmount := new(uint256.Int).Add(targetAmount, tradingFeeAmount)
+
+	sourceAmount, err := math.MathEx.MulDivF(sourceBalance, fullTargetAmount, new(uint256.Int).Sub(targetBalance, fullTargetAmount))
+	if err != nil {
+		return nil, err
+	}
+
+	return &tradeAmountAndTradingFee{
+		Amount:           sourceAmount,
 		TradingFeeAmount: tradingFeeAmount,
 	}, nil
 }
