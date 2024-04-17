18ce5eb2fb2cf0bf9c4c23969ded0ddeba37425b
Hải Nam <nam.le@kyber.network>
2024-04-17 17:26:12 +0700

    chore: add necessary swell encoding data (#425)

--- pkg/liquidity-source/bedrock/unieth/constant.go
+++ pkg/liquidity-source/bedrock/unieth/constant.go
@@ -7,7 +7,7 @@ const (
 var (
 	WETH    = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 	UNIETH  = "0xf1376bcef0f78459c0ed0ba5ddce976f1ddf51f4"
-	Staking = "0xd968495636c4cf7435b36a6a8135c1b528ff31b1"
+	Staking = "0x4befa2aa9c305238aa3e0b5d17eb20c045269e9d"
 )
 
 var (
