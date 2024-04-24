1ddf435db2c81723ac50482be226560d5569da49
phqb <bao.phan@kyber.network>
2024-04-25 03:14:01 +0700

    native-v1

---
+++ pkg/liquidity-source/native-v1/pool_simulator_msgp_test.go
@@ -0,0 +1,45 @@
+package nativev1
+
+import (
+	"testing"
+
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/testutil"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgpackMarshalUnmarshal(t *testing.T) {
+	poolEntities := []entity.Pool{
+		{
+			Address:  "native_v1_0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270_0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+			Exchange: "native-v1",
+			Type:     "native-v1",
+			Reserves: []string{"9320038994403940352", "166143156993"},
+			Tokens: []*entity.PoolToken{
+				{Address: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", Decimals: 18, Swappable: true},
+				{Address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", Decimals: 6, Swappable: true},
+			},
+			Extra: "{\"0to1\":[{\"q\":0.0001,\"p\":0.91245042136692},{\"q\":4.659919497201971,\"p\":0.91245042136692}," +
+				"{\"q\":4.66001949720197,\"p\":0.91245042136692}]," +
+				"\"1to0\":[{\"q\":0.0001,\"p\":1.0942398729806944},{\"q\":18277.528075741084,\"p\":1.0942398729806944}," +
+				"{\"q\":25244.263002363805,\"p\":1.0939119116852096},{\"q\":32092.9359692824,\"p\":1.0937921053280593}," +
+				"{\"q\":33219.273417201824,\"p\":1.0936723252106664},{\"q\":29917.17166407224,\"p\":1.0935525713244107}," +
+				"{\"q\":27391.98476499627,\"p\":1.093432843660677}],\"tlrnce\":0}",
+		},
+	}
+	var err error
+	pools := make([]*PoolSimulator, len(poolEntities))
+	for i, poolEntity := range poolEntities {
+		pools[i], err = NewPoolSimulator(poolEntity)
+		require.NoError(t, err)
+	}
+	for _, pool := range pools {
+		b, err := pool.MarshalMsg(nil)
+		require.NoError(t, err)
+		actual := new(PoolSimulator)
+		_, err = actual.UnmarshalMsg(b)
+		require.NoError(t, err)
+		require.Empty(t, cmp.Diff(pool, actual, testutil.CmpOpts(PoolSimulator{})...))
+	}
+}
