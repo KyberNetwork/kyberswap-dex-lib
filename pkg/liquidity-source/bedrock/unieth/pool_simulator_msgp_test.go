20c8656c5c729388c9ce1bbbfcdf25aa8c38feb3
phqb <bao.phan@kyber.network>
2024-04-25 03:42:56 +0700

    bedrock/unieth

---
+++ pkg/liquidity-source/bedrock/unieth/pool_simulator_msgp_test.go
@@ -0,0 +1,34 @@
+package unieth
+
+import (
+	"testing"
+
+	poolpkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/testutil"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgpackMarshalUnmarshal(t *testing.T) {
+	pools := []*PoolSimulator{
+		{
+			Pool: poolpkg.Pool{
+				Info: poolpkg.PoolInfo{
+					Tokens: []string{WETH, UNIETH},
+				},
+			},
+			paused:         false,
+			totalSupply:    bignumber.NewBig("40654517980271452478787"),
+			currentReserve: bignumber.NewBig("43102498463014375406128"),
+		},
+	}
+	for _, pool := range pools {
+		b, err := pool.MarshalMsg(nil)
+		require.NoError(t, err)
+		actual := new(PoolSimulator)
+		_, err = actual.UnmarshalMsg(b)
+		require.NoError(t, err)
+		require.Empty(t, cmp.Diff(pool, actual, testutil.CmpOpts(PoolSimulator{})...))
+	}
+}
