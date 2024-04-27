18e49f674e28d76070b52c96f991de64fde05102
phqb <bao.phan@kyber.network>
2024-04-28 04:39:05 +0700

    fixed typo `msgp:"-"` && moved SwapLimit back to pool_simulator.go

--- pkg/source/synthetix/pool_simulator.go
+++ pkg/source/synthetix/pool_simulator.go
@@ -1,7 +1,8 @@
 //go:generate go run github.com/tinylib/msgp -unexported -tests=false -v
-//msgp:tuple PoolSimulator Gas
+//msgp:tuple PoolSimulator Gas AtomicLimits
 //msgp:ignore Meta
 //msgp:shim PoolStateVersion as:uint using:uint/PoolStateVersion
+//msgp:shim *big.Int as:[]byte using:msgpencode.EncodeInt/msgpencode.DecodeInt
 
 package synthetix
 
@@ -263,4 +264,48 @@ func (p *PoolSimulator) CalculateLimit() map[string]*big.Int {
 	return map[string]*big.Int{
 		fmt.Sprint(s.BlockTimestamp): maxVol,
 	}
+}
+
+// AtomicLimits implement pool.SwapLimit for synthetic
+// key is blockTimestamp, and the limit is its balance
+// The balance is stored WITHOUT decimals
+// DONOT directly modify it, use UpdateLimit if needed
+type AtomicLimits struct {
+	lock   *sync.RWMutex `msg:"-"`
+	Limits map[string]*big.Int
+}
+
+func NewLimits(atomicMaxVolumePerBlocks map[string]*big.Int) pool.SwapLimit {
+	return &AtomicLimits{
+		lock:   &sync.RWMutex{},
+		Limits: atomicMaxVolumePerBlocks,
+	}
+}
+
+// GetLimit returns a copy of balance for the token in Inventory
+func (i *AtomicLimits) GetLimit(blockTimeStamp string) *big.Int {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
+	balance, avail := i.Limits[blockTimeStamp]
+	if !avail {
+		return big.NewInt(0)
+	}
+	return big.NewInt(0).Set(balance)
+}
+
+// UpdateLimit will reduce the limit to reflect the change in inventory
+// note this delta is amount without Decimal
+func (i *AtomicLimits) UpdateLimit(blockTimeStamp, _ string, decreaseDelta, _ *big.Int) (*big.Int, *big.Int, error) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	volLimit, avail := i.Limits[blockTimeStamp]
+	if !avail {
+		return big.NewInt(0), big.NewInt(0), pool.ErrTokenNotAvailable
+	}
+	if volLimit.Cmp(decreaseDelta) < 0 {
+		return big.NewInt(0), big.NewInt(0), pool.ErrNotEnoughInventory
+	}
+	i.Limits[blockTimeStamp] = volLimit.Sub(volLimit, decreaseDelta)
+
+	return big.NewInt(0).Set(i.Limits[blockTimeStamp]), big.NewInt(0), nil
 }
