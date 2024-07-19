9e60c72fe9dbffaff694d30516efeb8dc0de40cd
phqb <bao.phan@kyber.network>
2024-07-19 13:58:50 +0700

    fixed ambient.PoolListUpdater.GetNewPools() returns error when metadataBytes = nil

--- pkg/liquidity-source/ambient/pool_list_updater_test.go
+++ pkg/liquidity-source/ambient/pool_list_updater_test.go
@@ -14,20 +14,31 @@ import (
 func TestPoolListUpdater(t *testing.T) {
 	t.Skip()
 
-	var (
-		metadataBytes, _ = json.Marshal(ambient.PoolListUpdaterMetadata{LastCreateTime: 0})
-	)
-
 	pu := ambient.NewPoolsListUpdater(ambient.Config{
 		DexID:                  "ambient",
 		SubgraphURL:            "https://api.studio.thegraph.com/query/47610/croc-mainnet/version/latest",
 		SubgraphRequestTimeout: durationjson.Duration{Duration: time.Second * 10},
 	})
-	pools, meta, err := pu.GetNewPools(context.Background(), metadataBytes)
-	require.NoError(t, err)
 
-	t.Logf("%s\n", string(meta))
-	for i := range pools {
-		t.Logf("%+v\n", pools[i])
-	}
+	t.Run("nil metadataBytes, default config", func(t *testing.T) {
+		pools, meta, err := pu.GetNewPools(context.Background(), nil)
+		require.NoError(t, err)
+
+		t.Logf("%s\n", string(meta))
+		for i := range pools {
+			t.Logf("%+v\n", pools[i])
+		}
+	})
+
+	t.Run("with metadataBytes, specified lastCreateTime", func(t *testing.T) {
+		metadataBytes, _ := json.Marshal(ambient.PoolListUpdaterMetadata{LastCreateTime: 1697392211})
+
+		pools, meta, err := pu.GetNewPools(context.Background(), metadataBytes)
+		require.NoError(t, err)
+
+		t.Logf("%s\n", string(meta))
+		for i := range pools {
+			t.Logf("%+v\n", pools[i])
+		}
+	})
 }
