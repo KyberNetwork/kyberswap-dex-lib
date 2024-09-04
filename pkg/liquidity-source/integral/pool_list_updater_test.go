0ce702f5717560a34a7a1ab684e6e62e1c709584
sunspirit9999 <phamnhatlinh19999@gmail.com>
2024-09-04 15:16:45 +0700

    update

--- pkg/liquidity-source/integral/pool_list_updater_test.go
+++ pkg/liquidity-source/integral/pool_list_updater_test.go
@@ -79,6 +79,6 @@ func (ts *PoolListUpdaterTestSuite) TestGetNewPools() {
 }
 
 func TestPoolListUpdaterTestSuite(t *testing.T) {
-	// t.Skip("Skipping testing in CI environment")
+	t.Skip("Skipping testing in CI environment")
 	suite.Run(t, new(PoolListUpdaterTestSuite))
 }
