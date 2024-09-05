cee65ee2da53053a8d14a8a2ca4af68af7e9dafd
Hoang Trinh <hoang.trinhj@gmail.com>
2024-09-05 10:52:05 +0700

    feat: integrate maverick-v2 for tracking pools (#504)

---
+++ pkg/liquidity-source/maverick-v2/constant.go
@@ -0,0 +1,13 @@
+package maverickv2
+
+const (
+	DexType = "maverick-v2"
+)
+
+const (
+	factoryMethodLookup = "lookup"
+
+	poolMethodTokenA   = "tokenA"
+	poolMethodTokenB   = "tokenB"
+	poolMethodGetState = "getState"
+)
