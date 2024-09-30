2edd72551e81527b7f1cae740d808829030b8852
Dat Luong <dat.luong@kyber.network>
2024-09-30 17:59:47 +0700

    feat: savingdai stable  overrides state

--- pkg/liquidity-source/maker/savingsdai/pool_tracker.go
+++ pkg/liquidity-source/maker/savingsdai/pool_tracker.go
@@ -12,6 +12,8 @@ import (
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	poolpkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 )
 
 type PoolTracker struct {
@@ -25,7 +27,24 @@ func NewPoolTracker(ethrpcClient *ethrpc.Client) *PoolTracker {
 func (t *PoolTracker) GetNewPoolState(
 	ctx context.Context,
 	p entity.Pool,
+	params poolpkg.GetNewPoolStateParams,
+) (entity.Pool, error) {
+	return t.getNewPoolState(ctx, p, params, nil)
+}
+
+func (t *PoolTracker) GetNewPoolStateWithOverrides(
+	ctx context.Context,
+	p entity.Pool,
+	params poolpkg.GetNewPoolStateWithOverridesParams,
+) (entity.Pool, error) {
+	return t.getNewPoolState(ctx, p, poolpkg.GetNewPoolStateParams{Logs: params.Logs}, params.Overrides)
+}
+
+func (t *PoolTracker) getNewPoolState(
+	ctx context.Context,
+	p entity.Pool,
 	_ poolpkg.GetNewPoolStateParams,
+	overrides map[common.Address]gethclient.OverrideAccount,
 ) (entity.Pool, error) {
 	logger.WithFields(logger.Fields{
 		"dexType":     DexType,
@@ -45,6 +64,9 @@ func (t *PoolTracker) GetNewPoolState(
 	)
 
 	req := t.ethrpcClient.R().SetContext(ctx)
+	if overrides != nil {
+		req.SetOverrides(overrides)
+	}
 	req.AddCall(&ethrpc.Call{
 		ABI:    potABI,
 		Target: pot,
@@ -81,6 +103,10 @@ func (t *PoolTracker) GetNewPoolState(
 	}
 
 	req = t.ethrpcClient.R().SetContext(ctx)
+	if overrides != nil {
+		req.SetOverrides(overrides)
+	}
+
 	blockTimestamp, err := req.GetCurrentBlockTimestamp()
 	if err != nil {
 		return p, err
