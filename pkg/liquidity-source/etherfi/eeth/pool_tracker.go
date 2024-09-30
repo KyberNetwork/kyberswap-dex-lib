eb803420dbb772036173e20a81f48d8f615882fa
Dat Luong <dat.luong@kyber.network>
2024-09-30 17:52:06 +0700

    feat: etherfi stable  overrides state

--- pkg/liquidity-source/etherfi/eeth/pool_tracker.go
+++ pkg/liquidity-source/etherfi/eeth/pool_tracker.go
@@ -11,6 +11,8 @@ import (
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/etherfi/common"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	gethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 )
 
 type PoolTracker struct {
@@ -26,9 +28,26 @@ func NewPoolTracker(ethrpcClient *ethrpc.Client) *PoolTracker {
 func (t *PoolTracker) GetNewPoolState(
 	ctx context.Context,
 	p entity.Pool,
-	_ pool.GetNewPoolStateParams,
+	params pool.GetNewPoolStateParams,
 ) (entity.Pool, error) {
-	extra, blockNumber, err := t.getExtra(ctx)
+	return t.getNewPoolState(ctx, p, params, nil)
+}
+
+func (t *PoolTracker) GetNewPoolStateWithOverrides(
+	ctx context.Context,
+	p entity.Pool,
+	params pool.GetNewPoolStateWithOverridesParams,
+) (entity.Pool, error) {
+	return t.getNewPoolState(ctx, p, pool.GetNewPoolStateParams{Logs: params.Logs}, params.Overrides)
+}
+
+func (t *PoolTracker) getNewPoolState(
+	ctx context.Context,
+	p entity.Pool,
+	_ pool.GetNewPoolStateParams,
+	overrides map[gethcommon.Address]gethclient.OverrideAccount,
+) (entity.Pool, error) {
+	extra, blockNumber, err := t.getExtra(ctx, overrides)
 	if err != nil {
 		return p, err
 	}
@@ -45,14 +64,17 @@ func (t *PoolTracker) GetNewPoolState(
 	return p, nil
 }
 
-func (t *PoolTracker) getExtra(ctx context.Context) (PoolExtra, uint64, error) {
+func (t *PoolTracker) getExtra(ctx context.Context, overrides map[gethcommon.Address]gethclient.OverrideAccount,
+) (PoolExtra, uint64, error) {
 	var (
 		totalShares      *big.Int
 		totalPooledEther *big.Int
 	)
 
 	getPoolStateRequest := t.ethrpcClient.NewRequest().SetContext(ctx)
-
+	if overrides != nil {
+		getPoolStateRequest.SetOverrides(overrides)
+	}
 	getPoolStateRequest.AddCall(&ethrpc.Call{
 		ABI:    common.LiquidityPoolABI,
 		Target: common.LiquidityPool,
