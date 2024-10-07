5dbf6b7f0145aebf1be8c2dc5f4f332c0f7c793e
Hải Nam <nam.le@kyber.network>
2024-10-07 16:17:04 +0700

    chore: dis-allow swapping through underlaying tokens in curve-meta

--- pkg/source/curve/meta/pool_simulator.go
+++ pkg/source/curve/meta/pool_simulator.go
@@ -220,12 +220,9 @@ func (t *Pool) CanSwapTo(address string) []string {
 			for i := 0; i < len(t.Info.Tokens)-1; i += 1 {
 				ret = append(ret, t.Info.Tokens[i])
 			}
-			underlyingTokens := t.BasePool.GetInfo().Tokens
-			for i := 0; i < len(underlyingTokens); i += 1 {
-				if i != tokenIndex {
-					ret = append(ret, underlyingTokens[i])
-				}
-			}
+
+			// We don't allow swapping between underlying tokens here.
+			// Swap between underlying tokens must go directly through the base pool.
 		}
 		return ret
 	}
