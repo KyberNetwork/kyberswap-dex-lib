00e0176c6ee608937df46a276e5450014cb9d4fe
nhathm <nhat.hm@outlook.com>
2024-10-09 14:10:55 +0700

    chore: rebase and remove unrelated files

--- pkg/liquidity-source/mkr-sky/constant.go
+++ pkg/liquidity-source/mkr-sky/constant.go
@@ -5,6 +5,6 @@ const (
 
 	defaultTokenWeight       = 1
 	reserveZero              = "0"
-	defaultReserves          = "100000000000000000000000000"
+	defaultReserves          = "10000000000000000000"
 	DefaultGas         int64 = 60000
 )
