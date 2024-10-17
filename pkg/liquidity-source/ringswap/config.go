74cbd31550609dc4e14e6dae11c18d87c5dafa49
sunspirit99 <phamnhatlinh19999@gmail.com>
2024-10-17 16:01:30 +0700

    remove FeeTracker

--- pkg/liquidity-source/ringswap/config.go
+++ pkg/liquidity-source/ringswap/config.go
@@ -4,7 +4,6 @@ type Config struct {
 	DexID          string `json:"dexID"`
 	Fee            uint64 `json:"fee"`
 	FeePrecision   uint64 `json:"feePrecision"`
-	FeeTracker     string `json:"feeTracker"`
 	FactoryAddress string `json:"factoryAddress"`
 	NewPoolLimit   int    `json:"newPoolLimit"`
 	FewFactory     string `json:"fewFactory"`
