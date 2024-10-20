4c0e8fa6b84d5aed3f2ed4c411a13318522b1dee
bergben <bergben@tutanota.com>
2024-10-20 17:36:09 +0200

    docs(fluid): fix comment  WETH

--- pkg/liquidity-source/fluid/dex-t1/pool_simulator.go
+++ pkg/liquidity-source/fluid/dex-t1/pool_simulator.go
@@ -30,7 +30,7 @@ type PoolSimulator struct {
 var (
 	// Uniswap takes total gas of 125k = 21k base gas & 104k swap (this is when user has token balance)
 	// Fluid takes total gas of 175k = 21k base gas & 154k swap (this is when user has token balance),
-	// with ETH swaps costing less (because no weETH conversion)
+	// with ETH swaps costing less (because no WETH conversion)
 	defaultGas = Gas{Swap: 154000}
 )
 
