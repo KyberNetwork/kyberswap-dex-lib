fe0acbb446c6e2ae5fe882acd6e695423af90b6b
ChauNguyen-K <chau.nguyen@kyber.network>
2024-10-24 09:58:16 +0700

    Add span for calculate amount out metrics (#1127)

--- internal/pkg/core/pool/calc_amount_out.go
+++ internal/pkg/core/pool/calc_amount_out.go
@@ -17,12 +17,15 @@ func NewCalcAmountOut(dexUseAEVM map[string]bool) *calcAmountOut {
 }
 
 func (c *calcAmountOut) CalcAmountOut(ctx context.Context, pool poolpkg.IPoolSimulator, tokenAmountIn poolpkg.TokenAmount, tokenOut string, limit poolpkg.SwapLimit) (*poolpkg.CalcAmountOutResult, error) {
+	span, _ := tracer.StartSpanFromContext(ctx, "CalcAmountOut")
+	defer span.End()
+
 	if c.dexUseAEVM[pool.GetType()] {
-		span, _ := tracer.StartSpanFromContext(ctx, "CalcAmountOut")
-		span.SetTag("calcAmountOutAEVM", "aevm")
-		span.SetTag("dex", pool.GetType())
-		defer span.End()
+		span.SetTag("dexUseAEVM", "true")
+	} else {
+		span.SetTag("dexUseAEVM", "false")
 	}
+	span.SetTag("dex", pool.GetType())
 
 	if c := ctx.Value(metrics.CalcAmountOutCounterContextKey); c != nil {
 		c.(*metrics.CalcAmountOutCounter).Inc(pool.GetType(), 1)
@@ -33,12 +36,16 @@ func (c *calcAmountOut) CalcAmountOut(ctx context.Context, pool poolpkg.IPoolSim
 
 // [Deprecated] CalcAmountOut wrapper of (github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool).CalcAmountOut
 func CalcAmountOut(ctx context.Context, pool poolpkg.IPoolSimulator, tokenAmountIn poolpkg.TokenAmount, tokenOut string, limit poolpkg.SwapLimit, dexUseAEVM map[string]bool) (res *poolpkg.CalcAmountOutResult, err error) {
+	span, _ := tracer.StartSpanFromContext(ctx, "CalcAmountOut")
+	defer span.End()
+
 	if dexUseAEVM[pool.GetType()] {
-		span, _ := tracer.StartSpanFromContext(ctx, "CalcAmountOut")
-		span.SetTag("calcAmountOutAEVM", "aevm")
-		span.SetTag("dex", pool.GetType())
-		defer span.End()
+		span.SetTag("dexUseAEVM", "true")
+	} else {
+		span.SetTag("dexUseAEVM", "false")
 	}
+	span.SetTag("dex", pool.GetType())
+
 	if c := ctx.Value(metrics.CalcAmountOutCounterContextKey); c != nil {
 		c.(*metrics.CalcAmountOutCounter).Inc(pool.GetType(), 1)
 	}
