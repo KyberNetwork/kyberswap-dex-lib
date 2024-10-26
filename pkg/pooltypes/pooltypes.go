4f03f18fdb0623a026a18cf9eaa6f60e677547d2
bergben <bergben@tutanota.com>
2024-10-26 22:25:00 +0300

    refactor(poolTypes): fix formatting

--- pkg/pooltypes/pooltypes.go
+++ pkg/pooltypes/pooltypes.go
@@ -241,7 +241,7 @@ type Types struct {
 	RingSwap                       string
 	PrimeETH                       string
 	StaderETHx                     string
-  FluidVaultT1                   string
+	FluidVaultT1                   string
 	FluidDexT1                     string
 	MantleETH                      string
 	OndoUSDY                       string
