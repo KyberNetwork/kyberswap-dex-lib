c5574ed9816ce38ddcccbbfac20f8150aca3a87b
Vu Tien <tien7668@gmail.com>
2024-10-30 09:02:34 +0700

    update test

--- pkg/liquidity-source/dexalot/client/http_test.go
+++ pkg/liquidity-source/dexalot/client/http_test.go
@@ -31,6 +31,6 @@ func TestHTTPClient(t *testing.T) {
 		TakerAmount: "200000000",
 		UserAddress: "0x05A1AAC00662ADda4Aa25E1FA658f4256ed881eD",
 		Executor:    "0xdef171fe48cf0115b1d80b88dc8eab59176fee57",
-	})
+	}, 20)
 	assert.NoError(t, err)
 }
