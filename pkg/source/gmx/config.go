cbb8a8ade037628f56c7b512e8cd28db20a9cb71
sunspirit99 <phamnhatlinh19999@gmail.com>
2024-11-13 20:54:10 +0700

    correct config

--- pkg/source/gmx/config.go
+++ pkg/source/gmx/config.go
@@ -3,7 +3,7 @@ package gmx
 import "github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 
 type Config struct {
-	ChainID                 valueobject.ChainID `json:"-"`
+	ChainID                 valueobject.ChainID `json:"chainID"`
 	DexID                   string              `json:"-"`
 	VaultAddress            string              `json:"vaultAddress"`
 	UseSecondaryPriceFeedV1 bool                `json:"useSecondaryPriceFeedV1"`
