a4bbdaddff18d63061592b6abcf73f0411d01cf2
thanhpp <thanhphanphu18@gmail.com>
2024-11-22 17:58:34 +0700

    chore: 🚧 kyber-pmm: calculate limits
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- pkg/source/kyber-pmm/pool_simulator_test.go
+++ pkg/source/kyber-pmm/pool_simulator_test.go
@@ -321,6 +321,52 @@ func TestPoolSimulator_swapLimit(t *testing.T) {
 	}
 }
 
+func TestPoolSimulator_calculateLimit(t *testing.T) {
+	data := `
+      {
+        "address": "kyber_pmm_0x4d224452801aced8b2f0aebe155379bb5d594381_0xdac17f958d2ee523a2206206994597c13d831ec7",
+        "reserveUsd": 1360970.7667567069,
+        "amplifiedTvl": 1360970.7667567069,
+        "exchange": "kyber-pmm",
+        "type": "kyber-pmm",
+        "timestamp": 1732272598,
+        "reserves": [
+          "210003216790492160720",
+          "1359367795230"
+        ],
+        "tokens": [
+          {
+            "address": "0x4d224452801aced8b2f0aebe155379bb5d594381",
+            "name": "ApeCoin",
+            "symbol": "APE",
+            "decimals": 18,
+            "weight": 0,
+            "swappable": true
+          },
+          {
+            "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+            "name": "Tether USD",
+            "symbol": "USDT",
+            "decimals": 6,
+            "weight": 0,
+            "swappable": true
+          }
+        ],
+        "extra": "{\"baseToQuotePriceLevels\":[{\"price\":1.1426086945029605,\"amount\":1448.3925369383157},{\"price\":1.14428955845081,\"amount\":4345.177610814947},{\"price\":1.1441893875816131,\"amount\":4779.695371896442},{\"price\":1.1440687560594114,\"amount\":5257.664909086088},{\"price\":1.143679713857277,\"amount\":5784.6637098782685},{\"price\":1.142752749157162,\"amount\":6367.316457895198},{\"price\":1.142540120616818,\"amount\":7004.0584093461075},{\"price\":1.1422856716849081,\"amount\":7704.464250280718},{\"price\":1.1419786762349307,\"amount\":8474.910675308798},{\"price\":1.1416082956442937,\"amount\":9322.401742839684},{\"price\":1.1411612434242049,\"amount\":10254.641917123634},{\"price\":1.1406214835799104,\"amount\":11280.106108836},{\"price\":1.1395740595704091,\"amount\":12412.425629594145},{\"price\":1.1381886889713548,\"amount\":13660.848853169038},{\"price\":1.1372387982757062,\"amount\":15026.933738485925},{\"price\":1.136091185210735,\"amount\":16529.627112334536},{\"price\":1.1347044942686983,\"amount\":18182.589823567978},{\"price\":1.132319788574757,\"amount\":20013.375000310654},{\"price\":1.130016117550905,\"amount\":22020.16527190755},{\"price\":1.1275704951197274,\"amount\":24222.181799098296},{\"price\":1.1246649343962203,\"amount\":21379.00293962797}],\"quoteToBasePriceLevels\":[{\"price\":0.8567231469341043,\"amount\":243.89816179743246}]}",
+        "staticExtra": "{\"pairID\":\"APE/USDT\",\"baseTokenAddress\":\"0x4d224452801ACEd8B2F0aebE155379bb5D594381\",\"quoteTokenAddress\":\"0xdAC17F958D2ee523a2206206994597C13D831ec7\"}"
+      }`
+	var entPool entity.Pool
+	require.NoError(t, json.Unmarshal([]byte(data), &entPool))
+
+	pSim, err := NewPoolSimulator(entPool)
+	require.NoError(t, err)
+
+	limits := pSim.CalculateLimit()
+
+	t.Log(string(jsonify(limits)))
+	// "0x4d224452801aced8b2f0aebe155379bb5d594381":210003216790492160720,"0xdac17f958d2ee523a2206206994597c13d831ec7":1359367795230}
+}
+
 func jsonify(data any) []byte {
 	v, _ := json.Marshal(data)
 
