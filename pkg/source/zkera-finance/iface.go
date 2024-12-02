7366447b64326e6aeca05fa09729f6451a875282
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2024-12-02 17:09:50 +0700

    ft/algebra v1 uint256 (#620)
    * feat: use uint256 for algebra-v1
    
    * feat: implement CalcAmountIn for algebrav1
    * fix: clone liquidity during calc for algebrav1
    * fix: CalcAmountIn still need remainingAmountOut and gas
    
    ---------
    
    Co-authored-by: le.cao <le.cao@kyber.network>

--- pkg/source/zkera-finance/iface.go
+++ pkg/source/zkera-finance/iface.go
@@ -1,14 +1,5 @@
 package zkerafinance
 
-// NOTE: we generate mock files in the same package to avoid cycle dependencies
-//go:generate mockgen -destination ./mock_vault_reader.go -package gmx pool-service/internal/dex/gmx IVaultReader
-//go:generate mockgen -destination ./mock_vault_price_feed_reader.go -package gmx pool-service/internal/dex/gmx IVaultPriceFeedReader
-//go:generate mockgen -destination ./mock_fast_price_feed_reader_v1.go -package gmx pool-service/internal/dex/gmx IFastPriceFeedV1Reader
-//go:generate mockgen -destination ./mock_price_feed_reader.go -package gmx pool-service/internal/dex/gmx IPriceFeedReader
-//go:generate mockgen -destination ./mock_usdg_reader.go -package gmx pool-service/internal/dex/gmx IUSDGReader
-//go:generate mockgen -destination ./mock_chainlink_flags_reader.go -package gmx pool-service/internal/dex/gmx IChainlinkFlagsReader
-//go:generate mockgen -destination ./mock_pancake_pair_reader.go -package gmx pool-service/internal/dex/gmx IPancakePairReader
-
 import (
 	"context"
 	"math/big"
