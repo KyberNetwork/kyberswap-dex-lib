ddeff0dff67dade304072f2009e9f9618526bbc2
tien7668 <33758175+tien7668@users.noreply.github.com>
2024-12-12 14:43:54 +0700

    Vt/syncswapv2 stable bug fix (#647)
    * stable helper test script
    
    * clean
    
    * fix wrong usage of param A for syncswapv2 stable pool

--- pkg/liquidity-source/syncswapv2/stable/pool_simulator_test.go
+++ pkg/liquidity-source/syncswapv2/stable/pool_simulator_test.go
@@ -21,47 +21,14 @@ func TestGetAmountOut(t *testing.T) {
 		expectedErr       error
 	}{
 		{
-			name: "test token0 as tokenIn",
-			entityPool: entity.Pool{
-				Address:  "0x92eae0b3a75f3ef6c50369ce8ca96b285d2139b8",
-				Exchange: "syncswap",
-				Type:     "syncswap-stable",
-				Reserves: []string{
-					"276926762767",
-					"284081796016",
-				},
-				Tokens: []*entity.PoolToken{
-					{
-						Address:   "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-						Swappable: true,
-					},
-					{
-						Address:   "0xfc7e56298657b002b3e656400e746b7212912757",
-						Swappable: true,
-					},
-				},
-				Extra: "{\"swapFee0To1\":40,\"swapFee1To0\":40,\"token0PrecisionMultiplier\":1000000000000,\"token1PrecisionMultiplier\":1000000000000,\"A\":2000}",
-			},
-			tokenAmountIn: poolPkg.TokenAmount{
-				Token:  "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-				Amount: bignumber.NewBig("100000000000"),
-			},
-			tokenOut: "0xfc7e56298657b002b3e656400e746b7212912757",
-			expectedAmountOut: &poolPkg.TokenAmount{
-				Token:  "0xfc7e56298657b002b3e656400e746b7212912757",
-				Amount: bignumber.NewBig("99922559468"),
-			},
-			expectedErr: nil,
-		},
-		{
 			name: "test token1 as tokenIn",
 			entityPool: entity.Pool{
-				Address:  "0x92eae0b3a75f3ef6c50369ce8ca96b285d2139b8",
-				Exchange: "syncswap",
-				Type:     "syncswap-stable",
+				Address:  "0xd5a1a9680f083237c10c6357e72b37cafe1fb5de",
+				Exchange: "syncswapv2-stable",
+				Type:     "syncswapv2-stable",
 				Reserves: []string{
-					"276838614939",
-					"284170002373",
+					"1771167531",
+					"8079308863505801735196",
 				},
 				Tokens: []*entity.PoolToken{
 					{
@@ -69,20 +36,20 @@ func TestGetAmountOut(t *testing.T) {
 						Swappable: true,
 					},
 					{
-						Address:   "0xfc7e56298657b002b3e656400e746b7212912757",
+						Address:   "0x5fc44e95eaa48f9eb84be17bd3ac66b6a82af709",
 						Swappable: true,
 					},
 				},
-				Extra: "{\"swapFee0To1\":40,\"swapFee1To0\":40,\"token0PrecisionMultiplier\":1000000000000,\"token1PrecisionMultiplier\":1000000000000,\"A\":2000}",
+				Extra: "{\"swapFee0To1\":100,\"swapFee1To0\":100,\"token0PrecisionMultiplier\":1000000000000,\"token1PrecisionMultiplier\":1,\"A\":80}",
 			},
 			tokenAmountIn: poolPkg.TokenAmount{
-				Token:  "0xfc7e56298657b002b3e656400e746b7212912757",
-				Amount: bignumber.NewBig("100000000000"),
+				Token:  "0x5fc44e95eaa48f9eb84be17bd3ac66b6a82af709",
+				Amount: bignumber.NewBig("100000000000000000000"),
 			},
 			tokenOut: "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
 			expectedAmountOut: &poolPkg.TokenAmount{
 				Token:  "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-				Amount: bignumber.NewBig("99915796719"),
+				Amount: bignumber.NewBig("95366156"),
 			},
 			expectedErr: nil,
 		},
@@ -102,101 +69,6 @@ func TestGetAmountOut(t *testing.T) {
 
 			assert.Equal(t, tc.expectedErr, err)
 			assert.Equal(t, tc.expectedAmountOut, calcAmountOutResult.TokenAmountOut)
-		})
-	}
-}
-
-func TestGetAmountIn(t *testing.T) {
-	testCases := []struct {
-		name             string
-		entityPool       entity.Pool
-		tokenAmountOut   poolPkg.TokenAmount
-		tokenIn          string
-		expectedAmountIn *poolPkg.TokenAmount
-		expectedErr      error
-	}{
-		{
-			name: "test token0 as tokenIn",
-			entityPool: entity.Pool{
-				Address:  "0x92eae0b3a75f3ef6c50369ce8ca96b285d2139b8",
-				Exchange: "syncswap",
-				Type:     "syncswap-stable",
-				Reserves: []string{
-					"276926762767",
-					"284081796016",
-				},
-				Tokens: []*entity.PoolToken{
-					{
-						Address:   "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-						Swappable: true,
-					},
-					{
-						Address:   "0xfc7e56298657b002b3e656400e746b7212912757",
-						Swappable: true,
-					},
-				},
-				Extra: "{\"swapFee0To1\":40,\"swapFee1To0\":40,\"token0PrecisionMultiplier\":1000000000000,\"token1PrecisionMultiplier\":1000000000000,\"A\":2000}",
-			},
-			tokenAmountOut: poolPkg.TokenAmount{
-				Token:  "0xfc7e56298657b002b3e656400e746b7212912757",
-				Amount: bignumber.NewBig("100000000000"),
-			},
-			tokenIn: "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-			expectedAmountIn: &poolPkg.TokenAmount{
-				Token:  "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-				Amount: bignumber.NewBig("100077539910"),
-			},
-			expectedErr: nil,
-		},
-		{
-			name: "test token1 as tokenIn",
-			entityPool: entity.Pool{
-				Address:  "0x92eae0b3a75f3ef6c50369ce8ca96b285d2139b8",
-				Exchange: "syncswap",
-				Type:     "syncswap-stable",
-				Reserves: []string{
-					"276838614939",
-					"284170002373",
-				},
-				Tokens: []*entity.PoolToken{
-					{
-						Address:   "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-						Swappable: true,
-					},
-					{
-						Address:   "0xfc7e56298657b002b3e656400e746b7212912757",
-						Swappable: true,
-					},
-				},
-				Extra: "{\"swapFee0To1\":40,\"swapFee1To0\":40,\"token0PrecisionMultiplier\":1000000000000,\"token1PrecisionMultiplier\":1000000000000,\"A\":2000}",
-			},
-			tokenAmountOut: poolPkg.TokenAmount{
-				Token:  "0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4",
-				Amount: bignumber.NewBig("100000000000"),
-			},
-			tokenIn: "0xfc7e56298657b002b3e656400e746b7212912757",
-			expectedAmountIn: &poolPkg.TokenAmount{
-				Token:  "0xfc7e56298657b002b3e656400e746b7212912757",
-				Amount: bignumber.NewBig("100084320367"),
-			},
-			expectedErr: nil,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			pool, err := NewPoolSimulator(tc.entityPool)
-			assert.Nil(t, err)
-			calcAmountInResult, err := testutil.MustConcurrentSafe[*poolPkg.CalcAmountInResult](t, func() (any, error) {
-				return pool.CalcAmountIn(poolPkg.CalcAmountInParams{
-					TokenAmountOut: tc.tokenAmountOut,
-					TokenIn:        tc.tokenIn,
-					Limit:          nil,
-				})
-			})
-
-			assert.Equal(t, tc.expectedErr, err)
-			assert.Equalf(t, tc.expectedAmountIn.Amount, calcAmountInResult.TokenAmountIn.Amount, "expected amount in %s, got %s", tc.expectedAmountIn.Amount.String(), calcAmountInResult.TokenAmountIn.Amount.String())
 		})
 	}
 }
