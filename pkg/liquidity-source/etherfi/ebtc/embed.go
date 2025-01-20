173de91a641695b9dcbc3b4ae3f0f7c36e3c34d3
minhnhathoang <nhat.hm@outlook.com>
2025-01-20 10:49:35 +0700

    feat: add arbitrum and base pools

--- pkg/liquidity-source/etherfi/ebtc/embed.go
+++ pkg/liquidity-source/etherfi/ebtc/embed.go
@@ -11,6 +11,14 @@ var accountantABIData []byte
 //go:embed pools/ethereum.json
 var ethereumPoolData []byte
 
+//go:embed pools/arbitrum.json
+var arbitrumPoolData []byte
+
+//go:embed pools/base.json
+var basePoolData []byte
+
 var bytesByPath = map[string][]byte{
 	"pools/ethereum.json": ethereumPoolData,
+	"pools/arbitrum.json": arbitrumPoolData,
+	"pools/base.json":     basePoolData,
 }
