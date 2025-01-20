7e106d5287171408e3196fc460d1d235e06e08e0
Hải Nam <nam.le@kyber.network>
2025-01-20 11:02:14 +0700

    chore: update liquiditybook v20 benchmark

--- pkg/source/liquiditybookv20/benchmark_test.go
+++ pkg/source/liquiditybookv20/benchmark_test.go
@@ -23,6 +23,7 @@ func initPoolSimulator() *PoolSimulator {
 }
 
 // Before optimized
+// BenchmarkOptimizePoolSimulator-10    	   12337	    100675 ns/op	  109411 B/op	    2828 allocs/op
 // After optimized
 // BenchmarkOptimizePoolSimulator-10    	  328146	      3494 ns/op	    2902 B/op	      73 allocs/op
 func BenchmarkOptimizePoolSimulator(b *testing.B) {
