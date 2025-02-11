5d074bc839b818b69017cd6d60f5ae7e3e1e143a
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-02-12 03:51:06 +0700

    ft: check useApproveMax by function instead of exchange (#1416)

--- internal/pkg/valueobject/exchange.go
+++ internal/pkg/valueobject/exchange.go
@@ -4,13 +4,15 @@ import (
 	"hash/fnv"
 	"sort"
 
-	dexValueObject "github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
+	l1executor "github.com/KyberNetwork/aggregator-encoding/pkg/encode/l1encode/executor"
+	l2executor "github.com/KyberNetwork/aggregator-encoding/pkg/encode/l2encode/executor"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 )
 
-type Exchange = dexValueObject.Exchange
+type Exchange = valueobject.Exchange
 
 func IsAnExchange(exchange Exchange) bool {
-	return dexValueObject.IsAMMSource(exchange) || dexValueObject.IsRFQSource(exchange)
+	return valueobject.IsAMMSource(exchange) || valueobject.IsRFQSource(exchange)
 }
 
 // HashSources unique, then sort and has the slice string
@@ -38,201 +40,97 @@ func HashSources(sources []string) uint64 {
 	return h.Sum64()
 }
 
-// `useApproveMaxExchangeSet` defines set of exchanges that we should track if executor `approveMax` for the pools,
+// `useApproveMaxFunctionSet` defines set of functions that we should track if executor `approveMax` for the pools,
 // to support optimizing gas cost when encode (not all pools need to call `approveMax` when swap with executor).
-// This data can be add by check the SC code, if the function of each pool type reads the SHOULD_APPROVE_MAX flag.
-var useApproveMaxExchangeSet = map[Exchange]struct{}{
-	// executeStableSwap
-	dexValueObject.ExchangeOneSwap:             {},
-	dexValueObject.ExchangeNerve:               {},
-	dexValueObject.ExchangeIronStable:          {},
-	dexValueObject.ExchangeSynapse:             {},
-	dexValueObject.ExchangeSaddle:              {},
-	dexValueObject.ExchangeAxial:               {},
-	dexValueObject.ExchangeAlienBaseStableSwap: {},
+// This data can be added by check the SC code, if the function of each pool type reads the SHOULD_APPROVE_MAX flag.
+var useApproveMaxFunctionSet = map[string]struct{}{
+	l1executor.FunctionSelectorStableSwap.RawName:        {},
+	l1executor.FunctionSelectorCurveSwap.RawName:         {},
+	l1executor.FunctionSelectorKokonutCrypto.RawName:     {},
+	l1executor.FunctionSelectorPancakeStableSwap.RawName: {},
+	l1executor.FunctionSelectorBalancerV1.RawName:        {},
+	l1executor.FunctionSelectorBalancerV2.RawName:        {},
+	l1executor.FunctionSelectorBalancerV3.RawName:        {},
+	l1executor.FunctionSelectorDODO.RawName:              {},
+	l1executor.FunctionSelectorPSM.RawName:               {},
+	l1executor.FunctionSelectorWSTETH.RawName:            {},
+	l1executor.FunctionSelectorPlatypus.RawName:          {},
+	l1executor.FunctionSelectorWombat.RawName:            {},
+	l1executor.FunctionSelectorMantisSwap.RawName:        {},
+	l1executor.FunctionSelectorLimitOrderDS.RawName:      {},
+	l1executor.FunctionSelectorKyberPMM.RawName:          {},
+	l1executor.FunctionSelectorVooi.RawName:              {},
+	l1executor.FunctionSelectorMaticMigrate.RawName:      {},
+	l1executor.FunctionSelectorSmardex.RawName:           {},
+	l1executor.FunctionSelectorIntegral.RawName:          {},
+	l1executor.FunctionSelectorVelocoreV2.RawName:        {},
+	l1executor.FunctionSelectorSwaapV2.RawName:           {},
+	l1executor.FunctionSelectorBancorV3.RawName:          {},
+	l1executor.FunctionSelectorEtherFiWeETH.RawName:      {},
+	l1executor.FunctionSelectorKelp.RawName:              {},
+	l1executor.FunctionSelectorRocketPool.RawName:        {},
+	l1executor.FunctionSelectorEthenaSusde.RawName:       {},
+	l1executor.FunctionSelectorMakerSavingsDai.RawName:   {},
+	l1executor.FunctionSelectorHashflow.RawName:          {},
+	l1executor.FunctionSelectorNative.RawName:            {},
+	l1executor.FunctionSelectorRenzo.RawName:             {},
+	l1executor.FunctionSelectorPufferFinance.RawName:     {},
+	l1executor.FunctionSelectorAmbient.RawName:           {},
+	l1executor.FunctionSelectorEtherVista.RawName:        {},
+	l1executor.FunctionSelectorLitePSM.RawName:           {},
+	l1executor.FunctionSelectorMkrSky.RawName:            {},
+	l1executor.FunctionSelectorDaiUsds.RawName:           {},
+	l1executor.FunctionSelectorFluidVaultT1.RawName:      {},
+	l1executor.FunctionSelectorFluidDex.RawName:          {},
+	l1executor.FunctionSelectorUsd0PP.RawName:            {},
+	l1executor.FunctionSelectorBebop.RawName:             {},
+	l1executor.FunctionSelectorWBETH.RawName:             {},
+	l1executor.FunctionSelectorUniswapV1.RawName:         {},
+	l1executor.FunctionSelectorOndoUSDY.RawName:          {},
+	l1executor.FunctionSelectorDexalot.RawName:           {},
+	l1executor.FunctionSelectorRingSwap.RawName:          {},
+	l1executor.FunctionSelectorSfrxETHConvertor.RawName:  {},
+	l1executor.FunctionSelectorEtherfiVampire.RawName:    {},
+	l1executor.FunctionSelectorLO1inch.RawName:           {},
+	l1executor.FunctionSelectorBeetsSS.RawName:           {},
+	l1executor.FunctionSelectorEtherFieBTC.RawName:       {},
+	l1executor.FunctionSelectorUniswapV4.RawName:         {},
 
-	// executeCurve
-	dexValueObject.ExchangeCurve:         {},
-	dexValueObject.ExchangeEllipsis:      {},
-	dexValueObject.ExchangeKokonutCrypto: {},
-
-	dexValueObject.ExchangeCurveStablePlain:  {},
-	dexValueObject.ExchangeCurveStableNg:     {},
-	dexValueObject.ExchangeCurveStableMetaNg: {},
-	dexValueObject.ExchangeCurveTriCryptoNg:  {},
-	dexValueObject.ExchangeCurveTwoCryptoNg:  {},
-
-	// executePancakeStableSwap
-	dexValueObject.ExchangePancakeStable: {},
-	dexValueObject.ExchangeZkSwapStable:  {},
-
-	// executeBalV2
-	dexValueObject.ExchangeBalancerV2Weighted:         {},
-	dexValueObject.ExchangeBalancerV2Stable:           {},
-	dexValueObject.ExchangeBalancerV2ComposableStable: {},
-	dexValueObject.ExchangeBeethovenXWeighted:         {},
-	dexValueObject.ExchangeBeethovenXStable:           {},
-	dexValueObject.ExchangeBeethovenXComposableStable: {},
-	dexValueObject.ExchangeGyroscope2CLP:              {},
-	dexValueObject.ExchangeGyroscope3CLP:              {},
-	dexValueObject.ExchangeGyroscopeECLP:              {},
-
-	// executeBalancerV3
-	dexValueObject.ExchangeBalancerV3Weighted:   {},
-	dexValueObject.ExchangeBalancerV3Stable:     {},
-	dexValueObject.ExchangeBeethovenXV3Weighted: {},
-	dexValueObject.ExchangeBeethovenXV3Stable:   {},
-
-	// executeBalancerV1
-	dexValueObject.ExchangeBalancerV1: {},
-
-	// executeDODO
-	dexValueObject.ExchangeDodo:               {},
-	dexValueObject.ExchangeDodoClassical:      {},
-	dexValueObject.ExchangeDodoPrivatePool:    {},
-	dexValueObject.ExchangeDodoStablePool:     {},
-	dexValueObject.ExchangeDodoVendingMachine: {},
-
-	// executeWrappedstETH
-	dexValueObject.ExchangeMakerLido: {},
-
-	// executePlatypus
-	dexValueObject.ExchangePlatypus: {},
-
-	// executePSM
-	dexValueObject.ExchangeMakerPSM: {},
-
-	// executeBalancerBatch
-
-	// executeMantis
-	dexValueObject.ExchangeMantisSwap: {},
-
-	// executeWombat
-	dexValueObject.ExchangeWombat: {},
-
-	// executeRfq
-	dexValueObject.ExchangeKyberPMM: {},
-
-	// executeKyberDSLO
-	dexValueObject.ExchangeKyberSwapLimitOrderDS: {},
-
-	// executeVooi
-	dexValueObject.ExchangeVooi: {},
-
-	// executeMaticMigrate
-	dexValueObject.ExchangePolMatic: {},
-
-	// executeSmardex
-	dexValueObject.ExchangeSmardex: {},
-
-	// executeIntegral
-	dexValueObject.ExchangeIntegral: {},
-
-	// executeVelocoreV2
-	dexValueObject.ExchangeVelocoreV2CPMM:         {},
-	dexValueObject.ExchangeVelocoreV2WombatStable: {},
-
-	// executeSwaapV2
-	dexValueObject.ExchangeSwaapV2: {},
-
-	// executeBancorV3
-	dexValueObject.ExchangeBancorV3: {},
-
-	// executeEtherFiWeETH
-	dexValueObject.ExchangeEtherfiWEETH: {},
-
-	// executeKelp
-	dexValueObject.ExchangeKelpRSETH: {},
-
-	// executeRocketPool
-	dexValueObject.ExchangeRocketPoolRETH: {},
-
-	// executeEthenaSusde
-	dexValueObject.ExchangeEthenaSusde: {},
-
-	// executeMakersDAI
-	dexValueObject.ExchangeMakerSavingsDai: {},
-
-	// executeHashflow
-	dexValueObject.ExchangeHashflowV3: {},
-
-	// executeNative
-	dexValueObject.ExchangeNativeV1: {},
-
-	// executeRenzo
-	dexValueObject.ExchangeRenzoEZETH: {},
-
-	// executePufferFinance
-	dexValueObject.ExchangePufferPufETH: {},
-
-	// executeAmbient
-	dexValueObject.ExchangeAmbient: {},
-
-	// executeEtherVista
-	dexValueObject.ExchangeEtherVista: {},
-
-	// executeLitePSM
-	dexValueObject.ExchangeLitePSM: {},
-
-	// executeMkrSky
-	dexValueObject.ExchangeMkrSky: {},
-
-	// executeDaiUsds
-	dexValueObject.ExchangeDaiUsds: {},
-
-	// executeFluidVaultT1
-	dexValueObject.ExchangeFluidVaultT1: {},
-
-	// executeFluidDex
-	dexValueObject.ExchangeFluidDexT1: {},
-
-	// executeUsd0PP
-	dexValueObject.ExchangeUsd0PP: {},
-
-	// executeRingSwap
-	dexValueObject.ExchangeRingSwap: {},
-
-	// executeBebop
-	dexValueObject.ExchangeBebop: {},
-
-	// executeWBETH
-	dexValueObject.ExchangeWBETH: {},
-
-	// executeUniswapV1
-	dexValueObject.ExchangeUniSwapV1: {},
-
-	// executeMantleUSD
-	dexValueObject.ExchangeOndoUSDY: {},
-
-	// executeDexalot
-	dexValueObject.ExchangeDexalot: {},
-
-	// executeSfrxETHConvertor
-	dexValueObject.ExchangeSfrxETHConvertor: {},
-
-	// executeEETHOrWeETH
-	dexValueObject.ExchangeEtherfiVampire: {},
-
-	// executeOneInchV6Rfq
-	dexValueObject.ExchangeMxTrading: {},
-
-	// executeBatchOneInchV6
-	dexValueObject.ExchangeLO1inch: {},
-
-	// executeVirtualBC
-	dexValueObject.ExchangeVirtualFun: {},
-
-	// executeBeetsSS
-	dexValueObject.ExchangeBeetsSS: {},
-
-	// executeEtherFieBtc
-	dexValueObject.ExchangeEtherFieBTC: {},
-
-	// executeUniV4
-	dexValueObject.ExchangeUniswapV4: {},
+	l2executor.FunctionSelectorLimitOrderDS.RawName:      {},
+	l2executor.FunctionSelectorStableSwap.RawName:        {},
+	l2executor.FunctionSelectorCurveSwap.RawName:         {},
+	l2executor.FunctionSelectorPancakeStableSwap.RawName: {},
+	l2executor.FunctionSelectorBalancerV2.RawName:        {},
+	l2executor.FunctionSelectorBalancerV3.RawName:        {},
+	l2executor.FunctionSelectorDODO.RawName:              {},
+	l2executor.FunctionSelectorWombat.RawName:            {},
+	l2executor.FunctionSelectorSmardex.RawName:           {},
+	l2executor.FunctionSelectorIntegral.RawName:          {},
+	l2executor.FunctionSelectorEtherVista.RawName:        {},
+	l2executor.FunctionSelectorSwaapV2.RawName:           {},
+	l2executor.FunctionSelectorHashflow.RawName:          {},
+	l2executor.FunctionSelectorNative.RawName:            {},
+	l2executor.FunctionSelectorFluidDex.RawName:          {},
+	l2executor.FunctionSelectorBebop.RawName:             {},
+	l2executor.FunctionSelectorRingSwap.RawName:          {},
+	l2executor.FunctionSelectorDexalot.RawName:           {},
+	l2executor.FunctionSelectorLO1inch.RawName:           {},
+	l2executor.FunctionSelectorVirtualFun.RawName:        {},
+	l2executor.FunctionSelectorEtherFieBTC.RawName:       {},
+	l2executor.FunctionSelectorUniswapV4.RawName:         {},
 }
 
+// IsApproveMaxExchange returns true if we should track if executor `approveMax` for the provided exchange,
+// to support optimizing gas cost when encoding (not all pools need to call `approveMax` when swap with executor).
+// This data can be added by check the SC code, if the function of each pool type reads the SHOULD_APPROVE_MAX flag.
 func IsApproveMaxExchange(exchange Exchange) bool {
-	_, ok := useApproveMaxExchangeSet[exchange]
+	l1Selector, _ := l1executor.GetFunctionSelector(exchange)
+	_, ok := useApproveMaxFunctionSet[l1Selector.RawName]
+	if ok {
+		return true
+	}
+
+	l2Selector, _ := l2executor.GetFunctionSelector(exchange)
+	_, ok = useApproveMaxFunctionSet[l2Selector.RawName]
 	return ok
 }
