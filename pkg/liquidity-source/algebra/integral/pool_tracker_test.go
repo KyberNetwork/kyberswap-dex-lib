363f1898accf076af9b0cf3b610dff01f8ac03d9
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-02-13 04:30:09 +0700

    fix: test

--- pkg/liquidity-source/algebra/integral/pool_tracker_test.go
+++ pkg/liquidity-source/algebra/integral/pool_tracker_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/algebra"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
 	graphqlpkg "github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/graphql"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
@@ -20,13 +21,15 @@ func TestPoolTracker_GetNewPoolState(t *testing.T) {
 	// t.Skip()
 
 	d := &PoolTracker{
+		PoolTracker: algebra.PoolTracker[Timepoint, TimepointRPC]{
+			EthrpcClient: ethrpc.NewWithClient(lo.Must(ethclient.Dial("https://bsc.kyberengineering.io"))).
+				SetMulticallContract(common.HexToAddress("0xcA11bde05977b3631167028862bE2a173976CA11")),
+		},
 		config: &Config{
 			DexID:              string(valueobject.ExchangeThenaFusionV3),
 			AllowSubgraphError: true,
 			UseBasePluginV2:    true,
 		},
-		ethrpcClient: ethrpc.NewWithClient(lo.Must(ethclient.Dial("https://bsc.kyberengineering.io"))).
-			SetMulticallContract(common.HexToAddress("0xcA11bde05977b3631167028862bE2a173976CA11")),
 		graphqlClient: graphqlpkg.NewClient("https://thegraph.com/explorer/api/playground/QmWSzHwZY9ZMNYMVbQLyL276V1toR3iZsnYMfQut166yit"),
 	}
 	got, err := d.GetNewPoolState(context.Background(), thenaEp, pool.GetNewPoolStateParams{})
