d7666bcc3238593bed46b49d4dcb081ae11ea5dc
SunSpirit <48086732+sunspirit99@users.noreply.github.com>
2025-02-20 19:49:25 +0700

    Fix overnight formula

--- pkg/liquidity-source/overnight-usdp/pool_simulator_test.go
+++ pkg/liquidity-source/overnight-usdp/pool_simulator_test.go
@@ -18,7 +18,37 @@ func TestPoolSimulator_CalcAmountOut(t *testing.T) {
 		expectedError     error
 	}{
 		{
-			name: "Mint USD+ from USDC",
+			name: "BSC : Mint USD+ from USDC",
+			poolSimulator: &PoolSimulator{
+				Pool: poolpkg.Pool{
+					Info: poolpkg.PoolInfo{
+						Tokens: []string{
+							"0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+							"0xe80772eaf6e2e18b651f160bc9158b2a5cafca65",
+						},
+						Reserves: []*big.Int{
+							bignumber.NewBig(defaultReserves),
+							bignumber.NewBig(defaultReserves),
+						},
+					},
+				},
+				isPaused:        false,
+				buyFee:          bignumber.Ten,
+				redeemFee:       bignumber.Ten,
+				usdPlusDecimals: 6,
+				assetDecimals:   18,
+			},
+			param: poolpkg.CalcAmountOutParams{
+				TokenAmountIn: poolpkg.TokenAmount{
+					Amount: bignumber.NewBig("1000000000000000000"),
+					Token:  "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+				},
+				TokenOut: "0xe80772eaf6e2e18b651f160bc9158b2a5cafca65",
+			},
+			expectedAmountOut: bignumber.NewBig("999900"),
+		},
+		{
+			name: "Base : Mint USD+ from USDC",
 			poolSimulator: &PoolSimulator{
 				Pool: poolpkg.Pool{
 					Info: poolpkg.PoolInfo{
@@ -36,16 +66,16 @@ func TestPoolSimulator_CalcAmountOut(t *testing.T) {
 				buyFee:          bignumber.Ten,
 				redeemFee:       bignumber.Ten,
 				usdPlusDecimals: 6,
-				assetDecimals:   18,
+				assetDecimals:   6,
 			},
 			param: poolpkg.CalcAmountOutParams{
 				TokenAmountIn: poolpkg.TokenAmount{
-					Amount: bignumber.NewBig("1000000000"),
+					Amount: bignumber.NewBig("1000000"),
 					Token:  "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 				},
 				TokenOut: "0xB79DD08EA68A908A97220C76d19A6aA9cBDE4376",
 			},
-			expectedAmountOut: bignumber.NewBig("999900000000000000000"),
+			expectedAmountOut: bignumber.NewBig("999900"),
 		},
 		{
 			name: "Redeem USDC from USD+",
