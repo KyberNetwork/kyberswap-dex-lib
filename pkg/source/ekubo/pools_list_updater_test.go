a261c0248deb58fa656232b1cb6d7e51af9cced8
die-herdplatte <173669014+die-herdplatte@users.noreply.github.com>
2025-03-04 09:57:26 +0100

    Add test to check existence of specific pool

--- pkg/source/ekubo/pools_list_updater_test.go
+++ pkg/source/ekubo/pools_list_updater_test.go
@@ -2,9 +2,14 @@ package ekubo
 
 import (
 	"context"
+	"encoding/json"
+	"slices"
 	"testing"
 
 	"github.com/KyberNetwork/ethrpc"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/ekubo/quoting"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,6 +22,26 @@ func TestPoolListUpdater(t *testing.T) {
 	newPools, _, err := plUpdater.GetNewPools(context.Background(), nil)
 	require.NoError(t, err)
 	require.Greater(t, len(newPools), 0)
+
+	testPk := quoting.NewPoolKey(
+		common.Address{},
+		common.HexToAddress("0x618c25b11a5e9b5ad60b04bb64fcbdfbad7621d1"),
+		quoting.Config{
+			Fee:         0,
+			TickSpacing: 0,
+			Extension:   common.HexToAddress(SepoliaConfig.Oracle),
+		},
+	)
+
+	require.True(t, slices.ContainsFunc(newPools, func(el entity.Pool) bool {
+		var staticExtra StaticExtra
+		err := json.Unmarshal([]byte(el.StaticExtra), &staticExtra)
+		require.NoError(t, err)
+
+		pk := staticExtra.PoolKey
+
+		return pk.Token0.Cmp(testPk.Token0) == 0 && pk.Token1.Cmp(testPk.Token1) == 0 && slices.Equal(pk.Config.Compressed(), testPk.Config.Compressed())
+	}))
 
 	newPools, _, err = plUpdater.GetNewPools(context.Background(), nil)
 	require.NoError(t, err)
