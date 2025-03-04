2dba09debfe075dbd1c433b90e441245e7e64bf7
Hải Nam <nam.le@kyber.network>
2025-03-04 16:26:39 +0700

    chore: update gas

--- pkg/liquidity-source/pandafun/constant.go
+++ pkg/liquidity-source/pandafun/constant.go
@@ -2,7 +2,7 @@ package pandafun
 
 import "github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
 
-const defaultGas = 200000 // TODO: Update this
+const defaultGas = 130000
 const DexType = "panda-fun"
 
 var oneGwei = bignumber.TenPowInt(9)
