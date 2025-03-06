c8b32132980872efae8f6bd4810d95628fa9ef38
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-03-06 18:17:40 +0700

    refactor: sky-psm to use config instead of embedded data (#748)

--- pkg/liquidity-source/maker/sky-psm/config.go
+++ pkg/liquidity-source/maker/sky-psm/config.go
@@ -1,6 +1,7 @@
 package skypsm
 
 type Config struct {
-	DexID    string `json:"dexID"`
-	PoolPath string `json:"poolPath"`
+	DexID      string   `json:"dexID"`
+	PsmAddress string   `json:"psmAddress"`
+	Tokens     []string `json:"tokens"`
 }
