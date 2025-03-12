c1fc3a0fe00f5c4f5170e3f8e07f39d58eb5ae0d
die-herdplatte <173669014+die-herdplatte@users.noreply.github.com>
2025-03-12 12:01:46 +0100

    Implement UpdateBalance

--- pkg/source/ekubo/quoting/quote.go
+++ pkg/source/ekubo/quoting/quote.go
@@ -4,10 +4,21 @@ import (
 	"math/big"
 )
 
+type StateAfter struct {
+	SqrtRatio       *big.Int
+	Liquidity       *big.Int
+	ActiveTickIndex int
+}
+
+type SwapInfo struct {
+	StateAfter StateAfter
+	SkipAhead  uint32
+}
+
 type Quote struct {
 	ConsumedAmount   *big.Int
 	CalculatedAmount *big.Int
 	FeesPaid         *big.Int
 	Gas              int64
-	SkipAhead        uint32
+	SwapInfo         SwapInfo
 }
