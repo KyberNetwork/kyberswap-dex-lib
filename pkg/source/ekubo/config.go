15c44470d1f822693dfff931e85bef0e0eabf86a
die-herdplatte <173669014+die-herdplatte@users.noreply.github.com>
2025-03-17 09:53:26 +0100

    Add new Oracle address

--- pkg/source/ekubo/config.go
+++ pkg/source/ekubo/config.go
@@ -18,12 +18,12 @@ type Config struct {
 
 var MainnetConfig = Config{
 	Core:        "0xe0e0e08A6A4b9Dc7bD67BCB7aadE5cF48157d444",
-	Oracle:      "0x514d5DE68852628Af2F1236F780866989660aDA6",
+	Oracle:      "0x51d02a5948496a67827242eabc5725531342527c",
 	DataFetcher: "0x91cB8a896cAF5e60b1F7C4818730543f849B408c",
 	Router:      "0x9995855C00494d039aB6792f18e368e530DFf931",
 
 	Extensions: map[common.Address]pool.Extension{
-		common.HexToAddress("0x514d5DE68852628Af2F1236F780866989660aDA6"): pool.Oracle,
+		common.HexToAddress("0x51d02a5948496a67827242eabc5725531342527c"): pool.Oracle,
 	},
 
 	ApiUrl: "https://eth-mainnet-api.ekubo.org",
