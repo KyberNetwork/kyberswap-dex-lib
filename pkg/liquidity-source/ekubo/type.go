cd336ba3f8d5bc7e13647a18c2f521e978712381
Minh Nhat Hoang <nhathm.vn@gmail.com>
2025-04-13 16:53:19 +0700

    Remove wrappers

--- pkg/liquidity-source/ekubo/type.go
+++ pkg/liquidity-source/ekubo/type.go
@@ -1,70 +1,30 @@
 package ekubo
 
 import (
-	"errors"
-	"fmt"
-	"math/big"
-	"slices"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-
-	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/ekubo/math"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/ekubo/quoting"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/ekubo/quoting/pool"
 )
 
-type Extra struct {
-	State quoting.PoolState `json:"state"`
-}
-
-type StaticExtra struct {
-	PoolKey   quoting.PoolKey    `json:"poolKey"`
-	Extension pool.ExtensionType `json:"extensionType"`
-}
-
-type addressWrapper struct {
-	common.Address
-}
-
-func (b *addressWrapper) UnmarshalJSON(input []byte) error {
-	if len(input) <= 4 {
-		return errors.New("expected non-empty prefixed hex string")
+type (
+	PoolData struct {
+		CoreAddress string `json:"core_address"`
+		Token0      string `json:"token0"`
+		Token1      string `json:"token1"`
+		Fee         string `json:"fee"`
+		TickSpacing uint32 `json:"tick_spacing"`
+		Extension   string `json:"extension"`
 	}
 
-	hexString := input[1 : len(input)-1]
-	if len(hexString)%2 != 0 {
-		hexString = slices.Insert(hexString, 3, []byte("0")[0])
-	}
-	bytes, err := hexutil.Decode(string(hexString))
-	if err != nil {
-		return fmt.Errorf("decoding hex string: %w", err)
-	}
+	GetAllPoolsResult = []PoolData
+)
 
-	b.Address = common.BytesToAddress(bytes)
-
-	return nil
-}
-
-type uint64Wrapper struct {
-	uint64
-}
-
-func (b *uint64Wrapper) UnmarshalJSON(input []byte) error {
-	if len(input) <= 4 {
-		return errors.New("expected non-empty prefixed hex string")
+type (
+	Extra struct {
+		quoting.PoolState
 	}
 
-	bi := new(big.Int)
-	if err := bi.UnmarshalJSON(input[1 : len(input)-1]); err != nil {
-		return fmt.Errorf("parsing big int: %w", err)
+	StaticExtra struct {
+		ExtensionType pool.ExtensionType `json:"extensionType"`
+		PoolKey       *quoting.PoolKey   `json:"poolKey"`
 	}
-
-	if bi.Cmp(math.TwoPow64) != -1 {
-		return errors.New("fee expected to fit into uint64")
-	}
-
-	b.uint64 = bi.Uint64()
-
-	return nil
-}
+)
