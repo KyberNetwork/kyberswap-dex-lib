160bec1ed752f2eab63b4cd2af3e8f2ccaff9e4b
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-05-15 17:19:40 +0700

    chore: tune native-v2 gas (#834)

--- pkg/liquidity-source/native-v1/type.go
+++ pkg/liquidity-source/native-v1/type.go
@@ -100,10 +100,6 @@ type (
 		ExpirySecs       uint   `json:"exp,omitempty" mapstructure:"exp"`
 	}
 
-	Gas struct {
-		Quote int64
-	}
-
 	MetaInfo struct {
 		Timestamp int64 `json:"timestamp"`
 	}
