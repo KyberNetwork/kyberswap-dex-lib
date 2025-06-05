5021a6dcb403a29b8d45287e38d7a433afda5da7
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-06-05 10:34:17 +0700

    ft: babydogeswap specifically can receive token before swap (#1655)

--- internal/pkg/valueobject/can_receive_token_before_swap.go
+++ internal/pkg/valueobject/can_receive_token_before_swap.go
@@ -3,34 +3,46 @@ package valueobject
 import (
 	l1executor "github.com/KyberNetwork/aggregator-encoding/pkg/encode/l1encode/executor"
 	l2executor "github.com/KyberNetwork/aggregator-encoding/pkg/encode/l2encode/executor"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 )
 
-// `canReceiveTokenBeforeSwapFunctionSet` defines set of functions for pools that can receive token before calling swap.
-var canReceiveTokenBeforeSwapFunctionSet = map[string]struct{}{
-	l1executor.FunctionSelectorUniswap.RawName:     {},
-	l1executor.FunctionSelectorCamelotSwap.RawName: {},
-	l1executor.FunctionSelectorKSClassic.RawName:   {},
-	l1executor.FunctionSelectorVelodrome.RawName:   {},
-	l1executor.FunctionSelectorMuteSwitch.RawName:  {},
-	l1executor.FunctionSelectorKTX.RawName:         {},
-	l1executor.FunctionSelectorBrownfi.RawName:     {},
+var (
+	// `canReceiveTokenBeforeSwapFunctionSet` defines functions for pools that can receive token before calling swap
+	canReceiveTokenBeforeSwapFunctionSet = map[string]struct{}{
+		l1executor.FunctionSelectorUniswap.RawName:     {},
+		l1executor.FunctionSelectorCamelotSwap.RawName: {},
+		l1executor.FunctionSelectorKSClassic.RawName:   {},
+		l1executor.FunctionSelectorVelodrome.RawName:   {},
+		l1executor.FunctionSelectorMuteSwitch.RawName:  {},
+		l1executor.FunctionSelectorKTX.RawName:         {},
+		l1executor.FunctionSelectorBrownfi.RawName:     {},
 
-	l2executor.FunctionSelectorUniswap.RawName:     {},
-	l2executor.FunctionSelectorKSClassic.RawName:   {},
-	l2executor.FunctionSelectorCamelotSwap.RawName: {},
-	l2executor.FunctionSelectorVelodrome.RawName:   {},
-	l2executor.FunctionSelectorKTX.RawName:         {},
+		l2executor.FunctionSelectorUniswap.RawName:     {},
+		l2executor.FunctionSelectorKSClassic.RawName:   {},
+		l2executor.FunctionSelectorCamelotSwap.RawName: {},
+		l2executor.FunctionSelectorVelodrome.RawName:   {},
+		l2executor.FunctionSelectorKTX.RawName:         {},
 
-	// GMX and GMX-like exchanges are also able to receive token before calling swap.
-	// However, they validate balance before swapping, so it's not possible to execute two gmx swaps consecutively
-	// without transferring token back to executor.
-	// We disable gmx exchanges here to reduce ad-hoc logic on back end side (do not allow two consecutive gmx swap)
-	// l1executor.FunctionSelectorGMX.RawName:        {},
-	// l2executor.FunctionSelectorGMX.RawName:        {},
-}
+		// GMX and GMX-like exchanges are also able to receive token before calling swap.
+		// However, they validate balance before swapping, so it's not possible to execute two gmx swaps consecutively
+		// without transferring token back to executor.
+		// We disable gmx exchanges here to reduce ad-hoc logic on back end side (do not allow two consecutive gmx swap)
+		// l1executor.FunctionSelectorGMX.RawName:        {},
+		// l2executor.FunctionSelectorGMX.RawName:        {},
+	}
+
+	// `canReceiveTokenBeforeSwapExchangeSet` defines exchanges that can receive token before calling swap.
+	canReceiveTokenBeforeSwapExchangeSet = map[Exchange]struct{}{
+		valueobject.ExchangeBabyDogeSwap: {},
+	}
+)
 
 // CanReceiveTokenBeforeSwap returns true for exchanges that can receive token before calling swap.
 func CanReceiveTokenBeforeSwap(exchange Exchange) bool {
+	if _, ok := canReceiveTokenBeforeSwapExchangeSet[exchange]; ok {
+		return true
+	}
+
 	l1Selector, _ := l1executor.GetFunctionSelector(exchange, false)
 	if _, ok := canReceiveTokenBeforeSwapFunctionSet[l1Selector.RawName]; ok {
 		return true
