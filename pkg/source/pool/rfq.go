7c941d6e0571b5290a4bc6502070d6cd1b558e48
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-06-13 00:49:45 +0700

    ft: support loading poolSim in fairflow/kem RFQ

--- pkg/source/pool/rfq.go
+++ pkg/source/pool/rfq.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/samber/lo"
+
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 )
 
@@ -11,6 +14,7 @@ import (
 type RFQParams struct {
 	NetworkID    valueobject.ChainID // blockchain network id
 	RequestID    string              // request id from getRoute
+	PoolID       string              // pool id
 	Sender       string              // swap tx origin
 	Recipient    string              // fund recipient of swap tx
 	RFQSender    string              // RFQ caller (executor)
@@ -35,11 +39,11 @@ type RFQResult struct {
 // RFQHandler is the default no-op RFQ handler
 type RFQHandler struct{}
 
-func (h *RFQHandler) RFQ(_ context.Context, _ RFQParams) (*RFQResult, error) {
+func (h *RFQHandler) RFQ(context.Context, []RFQParams) (*RFQResult, error) {
 	return nil, nil
 }
 
-func (h *RFQHandler) BatchRFQ(_ context.Context, _ []RFQParams) ([]*RFQResult, error) {
+func (h *RFQHandler) BatchRFQ(context.Context, []RFQParams) ([]*RFQResult, error) {
 	return nil, nil
 }
 
@@ -47,11 +51,12 @@ func (h *RFQHandler) SupportBatch() bool {
 	return false
 }
 
-type SequentialBatchRFQHandler struct {
+// RFQSequentialBatcher knows how to batch RFQs sequentially
+type RFQSequentialBatcher struct {
 	IPoolSingleRFQ
 }
 
-func (h *SequentialBatchRFQHandler) BatchRFQ(ctx context.Context, paramsSlice []RFQParams) (results []*RFQResult,
+func (h *RFQSequentialBatcher) BatchRFQ(ctx context.Context, paramsSlice []RFQParams) (results []*RFQResult,
 	err error) {
 	results = make([]*RFQResult, len(paramsSlice))
 	for i, params := range paramsSlice {
@@ -62,6 +67,60 @@ func (h *SequentialBatchRFQHandler) BatchRFQ(ctx context.Context, paramsSlice []
 	return results, nil
 }
 
-func (h *SequentialBatchRFQHandler) SupportBatch() bool {
+func (h *RFQSequentialBatcher) SupportBatch() bool {
 	return true
+}
+
+// RFQWithPoolState knows how to load pool state for simulations before RFQ or BatchRFQ call
+type RFQWithPoolState struct {
+	IPoolRFQ
+	IPoolManager
+	ICustomFuncs
+	DexId string
+}
+
+func (h *RFQWithPoolState) RFQ(ctx context.Context, params RFQParams) (*RFQResult, error) {
+	ctx = h.GetAndClonePoolState(ctx, params.PoolID)
+	return h.IPoolRFQ.RFQ(ctx, params)
+}
+
+func (h *RFQWithPoolState) BatchRFQ(ctx context.Context, paramsSlice []RFQParams) (results []*RFQResult, err error) {
+	ctx = h.GetAndClonePoolState(ctx, lo.Map(paramsSlice, func(p RFQParams, _ int) string { return p.PoolID })...)
+	return h.IPoolRFQ.BatchRFQ(ctx, paramsSlice)
+}
+
+func (h *RFQWithPoolState) GetAndClonePoolState(ctx context.Context, poolAddrs ...string) context.Context {
+	poolState, _ := h.GetStateByPoolAddresses(ctx, poolAddrs, []string{h.DexId}, PoolManagerExtraData{})
+	if poolState == nil {
+		poolState = &FindRouteState{}
+	}
+	for addr, poolSim := range poolState.Pools {
+		poolState.Pools[addr] = h.ClonePool(ctx, poolSim)
+	}
+	for dex, limit := range poolState.SwapLimit {
+		poolState.SwapLimit[dex] = h.CloneSwapLimit(ctx, limit)
+	}
+	return context.WithValue(ctx, h, poolState)
+}
+
+func (h *RFQWithPoolState) PoolState(ctx context.Context) *FindRouteState {
+	poolState, _ := ctx.Value(h).(*FindRouteState)
+	return poolState
+}
+
+type IPoolManager interface {
+	// GetStateByPoolAddresses return a map of address - pools and a map of dexType- swapLimit for
+	GetStateByPoolAddresses(ctx context.Context, poolAddresses, dex []string,
+		extraData PoolManagerExtraData) (*FindRouteState, error)
+}
+
+type PoolManagerExtraData struct {
+	KyberLimitOrderAllowedSenders string
+}
+
+type FindRouteState struct {
+	Pools                   map[string]IPoolSimulator // map PoolAddress-IPoolSimulator implementation
+	SwapLimit               map[string]SwapLimit      // map dexType-SwapLimit
+	StateRoot               common.Hash               // aevm state root
+	PublishedPoolsStorageID string                    // last published pools
 }
