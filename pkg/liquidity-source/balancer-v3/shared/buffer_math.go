558ec1b5202fdf9bf366a8b1836691199afdf03a
Vu Tien <tien7668@gmail.com>
2025-07-10 15:23:39 +0700

    check rate missing for transition

--- pkg/liquidity-source/balancer-v3/shared/buffer_math.go
+++ pkg/liquidity-source/balancer-v3/shared/buffer_math.go
@@ -1,20 +1,29 @@
 package shared
 
 import (
+	"errors"
+
 	"github.com/holiman/uint256"
 )
 
 var (
-	WAD = uint256.NewInt(1e18) // 10**18
+	WAD            = uint256.NewInt(1e18) // 10**18
+	ErrInvalidRate = errors.New("invalid rate")
 )
 
-func (b *ExtraBuffer) ConvertToShares(assets *uint256.Int) *uint256.Int {
-	assets.MulDivOverflow(assets, WAD, b.Rate)
+func (b *ExtraBuffer) ConvertToShares(assets *uint256.Int) (*uint256.Int, error) {
+	if b.Rate == nil || b.Rate.IsZero() {
+		return nil, ErrInvalidRate
+	}
 
-	return assets
+	assets.MulDivOverflow(assets, WAD, b.Rate)
+	return assets, nil
 }
 
-func (b *ExtraBuffer) ConvertToAssets(shares *uint256.Int) *uint256.Int {
+func (b *ExtraBuffer) ConvertToAssets(shares *uint256.Int) (*uint256.Int, error) {
+	if b.Rate == nil || b.Rate.IsZero() {
+		return nil, ErrInvalidRate
+	}
 	shares.MulDivOverflow(shares, b.Rate, WAD)
-	return shares
+	return shares, nil
 }
