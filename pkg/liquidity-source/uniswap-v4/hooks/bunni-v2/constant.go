9477947dda80e02b6c397a714098138df70dd499
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-07-23 14:49:51 +0700

    fix: add nil check for bunni hook constructor

--- pkg/liquidity-source/uniswap-v4/hooks/bunni-v2/constant.go
+++ pkg/liquidity-source/uniswap-v4/hooks/bunni-v2/constant.go
@@ -1,12 +1,18 @@
 package bunniv2
 
 import (
+	"errors"
+
 	"github.com/ethereum/go-ethereum/common"
 )
 
-var HookAddresses = []common.Address{
-	common.HexToAddress("0x0010d0d5db05933fa0d9f7038d365e1541a41888"),
-	common.HexToAddress("0x0000fe59823933ac763611a69c88f91d45f81888"),
-}
+var (
+	HookAddresses = []common.Address{
+		common.HexToAddress("0x0010d0d5db05933fa0d9f7038d365e1541a41888"),
+		common.HexToAddress("0x0000fe59823933ac763611a69c88f91d45f81888"),
+	}
 
-var HubAddress = common.HexToAddress("0x000000dceb71f3107909b1b748424349bfde5493")
+	HubAddress = common.HexToAddress("0x000000dceb71f3107909b1b748424349bfde5493")
+
+	ErrNilRpcClient = errors.New("rpc client is nil")
+)
