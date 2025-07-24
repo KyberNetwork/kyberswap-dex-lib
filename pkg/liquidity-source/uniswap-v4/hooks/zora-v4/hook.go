16ced933624629ca8a16eaaae1b5fc11dd076fc3
Hải Nam <nam.le@kyber.network>
2025-07-24 11:12:41 +0700

    chore: remove unnecessary function

--- pkg/liquidity-source/uniswap-v4/hooks/zora-v4/hook.go
+++ pkg/liquidity-source/uniswap-v4/hooks/zora-v4/hook.go
@@ -1,12 +1,10 @@
 package contentcoin
 
 import (
-	"context"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	uniswapv4 "github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/uniswap-v4"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 )
@@ -24,19 +22,6 @@ var _ = uniswapv4.RegisterHooksFactory(func(param *uniswapv4.HookParam) uniswapv
 
 	return hook
 }, HookAddresses...)
-
-func (h *Hook) GetReserves(ctx context.Context, param *uniswapv4.HookParam) (entity.PoolReserves, error) {
-	return nil, nil
-}
-
-func (h *Hook) Track(ctx context.Context, param *uniswapv4.HookParam) (string, error) {
-	return "", nil
-}
-
-func (h *Hook) BeforeSwap() (hookFeeAmt *big.Int, swapFee uniswapv4.FeeAmount) {
-	// No beforeSwap logic for this hook
-	return
-}
 
 func (h *Hook) AfterSwap() (hookFeeAmt *big.Int) {
 	// The main logic is to convert remaining fees to payout currency
