b881ba76dc877d49ed1bbfa5e0165aa529098400
SunSpirit <48086732+sunspirit99@users.noreply.github.com>
2025-07-28 20:34:05 +0700

    Fix(uni-v4): unsafe modification of amountIn

--- pkg/liquidity-source/uniswap-v4/hooks/clanker/hook_test.go
+++ pkg/liquidity-source/uniswap-v4/hooks/clanker/hook_test.go
@@ -3,14 +3,18 @@ package clanker
 import (
 	"context"
 	"encoding/json"
+	"math/big"
 	"os"
 	"testing"
 
 	"github.com/KyberNetwork/ethrpc"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	uniswapv4 "github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/uniswap-v4"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/testutil"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -72,4 +76,34 @@ func TestDynamicFeeHook_Track(t *testing.T) {
 		Pool:      &pool,
 	})
 	require.NoError(t, err)
+}
+
+func Test_CalcAmountOut(t *testing.T) {
+	var p entity.Pool
+	poolData := `{"address":"0xabb949ef8d1e86c37e2620de318da773273460a843a623c3c05d2b85dc893e77","swapFee":8388608,"exchange":"uniswap-v4","type":"uniswap-v4","timestamp":1753702228,"reserves":["83499061346031704444327158001","4265567673400257806"],"tokens":[{"address":"0x109ddc73b46b2f8141880a5573d2b0d2acf10b07","symbol":"neged2.0","decimals":18,"swappable":true},{"address":"0x4200000000000000000000000000000000000006","symbol":"WETH","decimals":18,"swappable":true}],"extra":"{\"liquidity\":596800550298755887846132,\"sqrtPriceX96\":566274760763156353397804,\"tickSpacing\":200,\"tick\":-236988,\"ticks\":[{\"index\":-237400,\"liquidityGross\":596800550298755887846132,\"liquidityNet\":596800550298755887846132},{\"index\":-120000,\"liquidityGross\":596800550298755887846132,\"liquidityNet\":-596800550298755887846132}],\"hX\":\"{\\\"ProtocolFee\\\":6000,\\\"ClankerFee\\\":30000,\\\"PairedFee\\\":30000,\\\"ClankerIsToken0\\\":true,\\\"ClankerTracked\\\":true}\"}","staticExtra":"{\"0x0\":[false,false],\"fee\":8388608,\"tS\":200,\"hooks\":\"0xdd5eeaff7bd481ad55db083062b13a3cdf0a68cc\",\"uR\":\"0x6ff5693b99212da76ad316178a184ab56d299b43\",\"pm2\":\"0x000000000022d473030f116ddee9f6b43ac78ba3\",\"mc3\":\"0xca11bde05977b3631167028862be2a173976ca11\"}","blockNumber":33456440}`
+	assert.NoError(t, json.Unmarshal([]byte(poolData), &p))
+
+	pSim, err := uniswapv4.NewPoolSimulator(p, valueobject.ChainIDBase)
+	assert.NoError(t, err)
+
+	got, err := pSim.CalcAmountOut(pool.CalcAmountOutParams{
+		TokenAmountIn: pool.TokenAmount{
+			Token:  "0x4200000000000000000000000000000000000006",
+			Amount: big.NewInt(1000000000000000),
+		},
+		TokenOut: "0x109ddc73b46b2f8141880a5573d2b0d2acf10b07",
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "18870367192095562543977004", got.TokenAmountOut.Amount.String())
+}
+
+func Test_CalcAmountIn(t *testing.T) {
+	var p entity.Pool
+	poolData := `{"address":"0xabb949ef8d1e86c37e2620de318da773273460a843a623c3c05d2b85dc893e77","swapFee":8388608,"exchange":"uniswap-v4","type":"uniswap-v4","timestamp":1753702228,"reserves":["83499061346031704444327158001","4265567673400257806"],"tokens":[{"address":"0x109ddc73b46b2f8141880a5573d2b0d2acf10b07","symbol":"neged2.0","decimals":18,"swappable":true},{"address":"0x4200000000000000000000000000000000000006","symbol":"WETH","decimals":18,"swappable":true}],"extra":"{\"liquidity\":596800550298755887846132,\"sqrtPriceX96\":566274760763156353397804,\"tickSpacing\":200,\"tick\":-236988,\"ticks\":[{\"index\":-237400,\"liquidityGross\":596800550298755887846132,\"liquidityNet\":596800550298755887846132},{\"index\":-120000,\"liquidityGross\":596800550298755887846132,\"liquidityNet\":-596800550298755887846132}],\"hX\":\"{\\\"ProtocolFee\\\":6000,\\\"ClankerFee\\\":30000,\\\"PairedFee\\\":30000,\\\"ClankerIsToken0\\\":true,\\\"ClankerTracked\\\":true}\"}","staticExtra":"{\"0x0\":[false,false],\"fee\":8388608,\"tS\":200,\"hooks\":\"0xdd5eeaff7bd481ad55db083062b13a3cdf0a68cc\",\"uR\":\"0x6ff5693b99212da76ad316178a184ab56d299b43\",\"pm2\":\"0x000000000022d473030f116ddee9f6b43ac78ba3\",\"mc3\":\"0xca11bde05977b3631167028862be2a173976ca11\"}","blockNumber":33456440}`
+	assert.NoError(t, json.Unmarshal([]byte(poolData), &p))
+
+	pSim, err := uniswapv4.NewPoolSimulator(p, valueobject.ChainIDBase)
+	assert.NoError(t, err)
+
+	testutil.TestCalcAmountIn(t, pSim)
 }
