d66bf050409c023a9fd6272a2b7150db3824b53d
Hải Nam <nam.le@kyber.network>
2025-08-01 13:18:44 +0700

    feat: remove revert zora hook

--- pkg/liquidity-source/uniswap-v4/hooks/zora/constant.go
+++ pkg/liquidity-source/uniswap-v4/hooks/zora/constant.go
@@ -4,7 +4,7 @@ import "github.com/ethereum/go-ethereum/common"
 
 var HookAddresses = []common.Address{
 	common.HexToAddress("0xa1ebdd5ca6470bbd67114331387f2dda7bfad040"),
-	common.HexToAddress("0xfff800b76768da8ab6aab527021e4a6a91219040"),
+	// common.HexToAddress("0xfff800b76768da8ab6aab527021e4a6a91219040"),
 	common.HexToAddress("0x5e5d19d22c85a4aef7c1fdf25fb22a5a38f71040"),
 	common.HexToAddress("0x5bf219b3cc11e3f6dd8dc8fc89d7d1deb0431040"),
 	common.HexToAddress("0xd61a675f8a0c67a73dc3b54fb7318b4d91409040"),
