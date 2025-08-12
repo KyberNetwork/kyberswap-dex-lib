ac86c5160bb71a33d6e783c73c93ae014a473370
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-08-13 02:34:23 +0700

    ft: adjust gas for brownfi-v2, euler-swap, gmx, uniswapv3

--- pkg/source/gmx/constants.go
+++ pkg/source/gmx/constants.go
@@ -6,12 +6,15 @@ import (
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
 )
 
-const DexTypeGmx = "gmx"
+const (
+	DexTypeGmx = "gmx"
 
-const FlagArbitrumSeqOffline = "0xa438451d6458044c3c8cd2f6f31c91ac882a6d91"
+	FlagArbitrumSeqOffline = "0xa438451d6458044c3c8cd2f6f31c91ac882a6d91"
+
+	defaultGas = 286524
+)
 
 var (
-	DefaultGas         = Gas{Swap: 165000}
 	BasisPointsDivisor = bignumber.BasisPoint
 	PricePrecision     = bignumber.TenPowInt(30)
 	USDGDecimals       = big.NewInt(18)
