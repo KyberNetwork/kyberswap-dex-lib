2289aa8ae7ad83e943dc6651a3ad1fc47a6b4d7c
Minh Nhat Hoang <nhathm.vn@gmail.com>
2025-08-15 09:00:12 +0700

    should deprecate source/uniswap

--- pkg/source/uniswap/config.go
+++ pkg/source/uniswap/config.go
@@ -1,8 +1,18 @@
 package uniswap
 
+import (
+	"github.com/KyberNetwork/blockchain-toolkit/time/durationjson"
+)
+
 type Config struct {
-	DexID          string  `json:"dexID"`
-	SwapFee        float64 `json:"swapFee"`
-	FactoryAddress string  `json:"factoryAddress"`
-	NewPoolLimit   int     `json:"newPoolLimit"`
+	DexID              string                   `json:"dexID"`
+	SwapFee            float64                  `json:"swapFee"`
+	FactoryAddress     string                   `json:"factoryAddress"`
+	NewPoolLimit       int                      `json:"newPoolLimit"`
+	TrackInactivePools TrackInactivePoolsConfig `json:"trackInactivePools"`
+}
+
+type TrackInactivePoolsConfig struct {
+	Enabled       bool                  `json:"enabled"`
+	TimeThreshold durationjson.Duration `json:"timeThreshold"`
 }
--- pkg/source/uniswap/pool_tracker.go
+++ pkg/source/uniswap/pool_tracker.go
@@ -2,11 +2,12 @@ package uniswap
 
 import (
 	"context"
-	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/KyberNetwork/ethrpc"
 	"github.com/KyberNetwork/logger"
+	"github.com/samber/lo"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
@@ -14,15 +15,18 @@ import (
 )
 
 type PoolTracker struct {
+	config       *Config
 	ethrpcClient *ethrpc.Client
 }
 
-var _ = pooltrack.RegisterFactoryE(DexTypeUniswap, NewPoolTracker)
+var _ = pooltrack.RegisterFactoryCE(DexTypeUniswap, NewPoolTracker)
 
 func NewPoolTracker(
+	config *Config,
 	ethrpcClient *ethrpc.Client,
 ) (*PoolTracker, error) {
 	return &PoolTracker{
+		config:       config,
 		ethrpcClient: ethrpcClient,
 	}, nil
 }
@@ -78,6 +82,7 @@ func (d *PoolTracker) GetNewPoolState(
 		reserveString(reserves.Reserve0),
 		reserveString(reserves.Reserve1),
 	}
+	p.IsInactive = d.IsInactive(&p, time.Now().Unix())
 
 	logger.Infof("[Uniswap V2] Finish getting new state of pool: %v", p.Address)
 
@@ -102,8 +107,6 @@ func (d *PoolTracker) fetchReservesFromNode(ctx context.Context, poolAddress str
 		return Reserves{}, err
 	}
 
-	fmt.Println("reserves", reserves.BlockTimestampLast)
-
 	return reserves, nil
 }
 
@@ -112,4 +115,29 @@ func reserveString(reserve *big.Int) string {
 		return reserveZero
 	}
 	return reserve.String()
+}
+
+func (d *PoolTracker) IsInactive(p *entity.Pool, currentTimestamp int64) bool {
+	if !d.config.TrackInactivePools.Enabled {
+		return false
+	}
+
+	inactiveTimeThresholdInSecond := int64(d.config.TrackInactivePools.TimeThreshold.Seconds())
+	if inactiveTimeThresholdInSecond <= 0 {
+		return false
+	}
+
+	return currentTimestamp-p.Timestamp > inactiveTimeThresholdInSecond
+}
+
+func (d *PoolTracker) GetInactivePools(_ context.Context, currentTimestamp int64, pools ...entity.Pool) ([]string, error) {
+	if len(pools) == 0 {
+		return nil, nil
+	}
+
+	inactivePools := lo.Filter(pools, func(p entity.Pool, _ int) bool {
+		return d.IsInactive(&p, currentTimestamp)
+	})
+
+	return lo.Map(inactivePools, func(p entity.Pool, _ int) string { return p.Address }), nil
 }
