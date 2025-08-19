00dc526422211ccdc3c6dc633d615302cc8eba8b
sunspirit99 <phamnhatlinh19999@gmail.com>
2025-08-19 11:56:03 +0700

    update

--- pkg/liquidity-source/uniswap/v4/hooks/bunni-v2/legacy_hook.go
+++ pkg/liquidity-source/uniswap/v4/hooks/bunni-v2/legacy_hook.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	// need to implement its formula first before uncommenting (disable due to poor volume)
+	// consider implementing exact formula or completely deprecating this version in the future
 	LegacyHookAddresses = []common.Address{
 		// common.HexToAddress("0x0010d0d5db05933fa0d9f7038d365e1541a41888"),
 		// common.HexToAddress("0x0000fe59823933ac763611a69c88f91d45f81888"),
