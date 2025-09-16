ee5cd2362982036ae7993cc221f9abbccb02dfe5
johngrantuk <johngrantuk@googlemail.com>
2025-09-16 11:16:50 +0100

    Add RECLAMM hook type. Note - reCLAMM pools are also their own hook. Hooks only affects add/remove function, not swap.

--- pkg/liquidity-source/balancer/v3/shared/hook.go
+++ pkg/liquidity-source/balancer/v3/shared/hook.go
@@ -7,6 +7,9 @@ const (
 	FeeTakingHookType        HookType = "FEE_TAKING"
 	StableSurgeHookType      HookType = "STABLE_SURGE"
 	VeBALFeeDiscountHookType HookType = "VEBAL_DISCOUNT"
+	// reCLAMM pools are also their own hook. The hook only affects add/remove function, not swap.
+	// https://docs.balancer.fi/concepts/explore-available-balancer-pools/reclamm-pool/reclamm-pool-math.html#on-add-remove-liquidity
+	ReClammHookType HookType = "RECLAMM"
 )
 
 // Define a map of supported hooks
@@ -15,6 +18,7 @@ var hooksMap = map[HookType]bool{
 	FeeTakingHookType:        true,
 	StableSurgeHookType:      true,
 	VeBALFeeDiscountHookType: true,
+	ReClammHookType:          true,
 }
 
 func IsHookSupported(hook HookType) bool {
