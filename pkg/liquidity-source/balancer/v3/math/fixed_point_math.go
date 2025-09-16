8a5beb3441a1146e56d30c32f8c20f897ec5649c
johngrantuk <johngrantuk@googlemail.com>
2025-09-16 11:02:10 +0100

    Add constants and fixed point maths required for reCLAMM.

--- pkg/liquidity-source/balancer/v3/math/fixed_point_math.go
+++ pkg/liquidity-source/balancer/v3/math/fixed_point_math.go
@@ -93,6 +93,47 @@ func (f *fixPoint) PowUp(x, y *uint256.Int) (*uint256.Int, error) {
 	return f.Add(raw, maxError)
 }
 
+func (f *fixPoint) PowDown(x, y *uint256.Int) (*uint256.Int, error) {
+	if y.Eq(U1e18) {
+		return x, nil
+	}
+
+	if y.Eq(U2e18) {
+		return f.MulUp(x, x)
+	}
+
+	if y.Eq(U4e18) {
+		square, err := f.MulUp(x, x)
+		if err != nil {
+			return nil, err
+		}
+
+		return f.MulUp(square, square)
+	}
+
+	raw, err := Pow(x, y)
+	if err != nil {
+		return nil, err
+	}
+
+	var maxError *uint256.Int
+	maxError, err = f.MulUp(raw, UMaxPowRelativeError)
+	if err != nil {
+		return nil, err
+	}
+
+	maxError, err = f.Add(maxError, U1)
+	if err != nil {
+		return nil, err
+	}
+
+	if raw.Lt(maxError) {
+		return U0, nil
+	}
+
+	return f.Sub(raw, maxError)
+}
+
 func (f *fixPoint) Complement(x *uint256.Int) *uint256.Int {
 	// result = (x < ONE) ? (ONE - x) : 0
 	result := new(uint256.Int)
@@ -125,4 +166,26 @@ func (f *fixPoint) Mul(a *uint256.Int, b *uint256.Int) (*uint256.Int, error) {
 		return nil, ErrMulOverflow
 	}
 	return c, nil
+}
+
+func (f *fixPoint) Div(a *uint256.Int, b *uint256.Int) (*uint256.Int, error) {
+	if b.IsZero() {
+		return nil, ErrZeroDivision
+	}
+	c := new(uint256.Int).Div(a, b)
+	return c, nil
+}
+
+func (f *fixPoint) Max(a, b *uint256.Int) (*uint256.Int, error) {
+	if a.Cmp(b) >= 0 {
+		return a, nil
+	}
+	return b, nil
+}
+
+func (f *fixPoint) Min(a, b *uint256.Int) (*uint256.Int, error) {
+	if a.Cmp(b) < 0 {
+		return a, nil
+	}
+	return b, nil
 }
