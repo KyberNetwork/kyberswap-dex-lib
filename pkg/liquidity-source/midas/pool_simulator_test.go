82fcf7e8bada34e0a74cf084753e0ff600a65f35
Minh Nhat Hoang <nhathm.vn@gmail.com>
2025-09-25 14:36:31 +0700

    chore: update tests

--- pkg/liquidity-source/midas/pool_simulator_test.go
+++ pkg/liquidity-source/midas/pool_simulator_test.go
@@ -146,7 +146,7 @@ func (ts *PoolSimulatorTestSuite) TestCalcAmountOut() {
 			expectedAmountOut: "103054757",
 		},
 		{
-			name:              "mBTC redeem WBTC",
+			name:              "msyrupUSD redeem USDC",
 			pool:              "rv-swapper-ustb-msyrupUSD-USDC",
 			tokenIn:           "0x20226607b4fa64228abf3072ce561d6257683464",
 			tokenOut:          "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
