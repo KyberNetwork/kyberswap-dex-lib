8116a62a6e138820bb069cddf4b58e54454178b2
FabienC <fabiencoutant.ctt@gmail.com>
2025-09-29 10:43:36 +0300

    feat: add needed abis

---
+++ pkg/liquidity-source/parallel-protocol/abis.go
@@ -0,0 +1,32 @@
+package parallelprotocol
+
+import (
+	"bytes"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+var (
+	parallelizerABI abi.ABI
+	chainlinkABI  abi.ABI
+	morphoABI     abi.ABI
+)
+
+func init() {
+	builder := []struct {
+		ABI  *abi.ABI
+		data []byte
+	}{
+		{&parallelizerABI, ParallelizerJson},
+		{&chainlinkABI, ChainlinkJson},
+		{&morphoABI, MorphoJson},
+	}
+
+	for _, b := range builder {
+		var err error
+		*b.ABI, err = abi.JSON(bytes.NewReader(b.data))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
