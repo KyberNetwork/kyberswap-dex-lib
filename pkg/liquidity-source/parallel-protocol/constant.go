2e5fcbf21b3093d051fdbcfcf9b6370c407934fb
FabienC <fabiencoutant.ctt@gmail.com>
2025-09-29 10:44:22 +0300

    feat: add constants

---
+++ pkg/liquidity-source/parallel-protocol/constant.go
@@ -0,0 +1,49 @@
+package parallelprotocol
+
+import (
+	"errors"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+const (
+	DexType        = "parallel-protocol"
+	defaultReserve = "100000000000000000000000000"
+)
+
+var (
+	ErrInvalidToken              = errors.New("invalid token")
+	ErrInvalidAmountIn           = errors.New("invalid amount in")
+	ErrInsufficientInputAmount   = errors.New("INSUFFICIENT_INPUT_AMOUNT")
+	ErrUnsupportedSwap           = errors.New("unsupported swap")
+	ErrInvalidOracle             = errors.New("invalid oracle compared to oracle type")
+	ErrUnimplemented             = errors.New("unimplemented")
+	ErrInvalidChainlinkRate      = errors.New("InvalidChainlinkRate")
+	ErrERC4626DepositMoreThanMax = errors.New("ERC4626: deposit more than max")
+	ErrERC4626RedeemMoreThanMax  = errors.New("ERC4626: redeem more than max")
+	ErrInvalidSwap               = errors.New("invalid swap")
+	ErrMulOverflow               = errors.New("MUL_OVERFLOW")
+)
+
+
+var ChainlinkArgument = abi.Arguments{
+	{Name: "circuitChainlink", Type: AddressArr},
+	{Name: "stalePeriods", Type: Uint32Arr},
+	{Name: "circuitChainIsMultiplied", Type: Uint8Arr},
+	{Name: "chainlinkDecimals", Type: Uint8Arr},
+	{Name: "quoteType", Type: Uint8},
+}
+
+var HyperparametersArgument = abi.Arguments{
+	{Name: "userDeviation", Type: Int128},
+	{Name: "burnRatioDeviation", Type: Int128},
+}
+
+var MaxArgument = abi.Arguments{
+	{Name: "maxValue", Type: Uint256},
+}
+
+var MorphoArgument = abi.Arguments{
+	{Name: "oracle", Type: Address},
+	{Name: "normalizationFactor", Type: Uint256},
+}
---
+++ pkg/liquidity-source/parallel-protocol/embed.go
@@ -0,0 +1,14 @@
+package parallelprotocol
+
+import (
+	_ "embed"
+)
+
+//go:embed abis/parallelizer.json
+var ParallelizerJson []byte
+
+//go:embed abis/chainlink.json
+var ChainlinkJson []byte
+
+//go:embed abis/morpho.json
+var MorphoJson []byte
