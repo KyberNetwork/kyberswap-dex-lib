817eb4694f55d0161b2ca445ec38b58c5082b878
SunSpirit <48086732+sunspirit99@users.noreply.github.com>
2025-10-03 12:34:45 +0700

    update(euler-swap): Increase buffer swap limit to 90% (#1088)

--- pkg/liquidity-source/euler-swap/constant.go
+++ pkg/liquidity-source/euler-swap/constant.go
@@ -54,5 +54,5 @@ var (
 	ErrDivisionByZero    = errors.New("division by zero")
 	ErrSwapLimitExceeded = errors.New("swap limit exceed")
 
-	bufferSwapLimit = uint256.NewInt(85) // 85%
+	bufferSwapLimit = uint256.NewInt(90) // 90%
 )
