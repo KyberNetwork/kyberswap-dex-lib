40418505e7f8e422ff2b14d25b9e916a16207b27
Hải Nam <nam.le@kyber.network>
2025-10-22 10:37:10 +0700

    feat: calculate before & after swap fee for angstrom

--- pkg/liquidity-source/uniswap/v4/hooks/angstrom/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/angstrom/constant.go
@@ -1,10 +1,23 @@
 package angstrom
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
 
 const Handler = "uniswap-v4-angstrom"
 
 const GET_ATTESTATIONS_PATH = "/getAttestations"
+
+// UnlockedFee/ProtocolUnlockedFee used in before/after swap hooks.
+// It difficult to track these fields, since contract doesn't expose them.
+// Only returns value from events, which can be seen from Angstrom Controller:
+// https://etherscan.io/address/0x1746484EA5e11C75e009252c102C8C33e0315fD4#events
+const UnlockedFee = 338
+
+var ProtocolUnlockedFee = big.NewInt(112)
+var ONE_E6 = big.NewInt(1_000_000)
 
 var Adapter = common.HexToAddress("0xb535aeb27335b91e1b5bccbd64888ba7574efbf8")
 
--- pkg/liquidity-source/uniswap/v4/hooks/angstrom/hook.go
+++ pkg/liquidity-source/uniswap/v4/hooks/angstrom/hook.go
@@ -1,8 +1,12 @@
 package angstrom
 
 import (
+	"math/big"
+
 	uniswapv4 "github.com/KyberNetwork/kyberswap-dex-lib/pkg/liquidity-source/uniswap/v4"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
+	"github.com/samber/lo"
 )
 
 type Hook struct {
@@ -16,3 +20,39 @@ var _ = uniswapv4.RegisterHooksFactory(func(param *uniswapv4.HookParam) uniswapv
 
 	return hook
 }, HookAddresses...)
+
+func (h *Hook) BeforeSwap(swapHookParams *uniswapv4.BeforeSwapParams) (*uniswapv4.BeforeSwapResult, error) {
+	return &uniswapv4.BeforeSwapResult{
+		DeltaSpecific:   bignumber.ZeroBI,
+		DeltaUnSpecific: bignumber.ZeroBI,
+		SwapFee:         uniswapv4.FeeAmount(UnlockedFee),
+	}, nil
+}
+
+func (h *Hook) AfterSwap(swapHookParams *uniswapv4.AfterSwapParams) (*uniswapv4.AfterSwapResult, error) {
+	exactIn := swapHookParams.ExactIn
+	targetAmount := swapHookParams.AmountOut
+
+	var tmp big.Int
+
+	fee := lo.Ternary(
+		exactIn,
+
+		new(big.Int).Div(
+			tmp.Mul(targetAmount, ProtocolUnlockedFee),
+			ONE_E6,
+		),
+
+		new(big.Int).Sub(
+			tmp.Div(
+				tmp.Mul(targetAmount, ONE_E6),
+				tmp.Sub(ONE_E6, ProtocolUnlockedFee),
+			),
+			targetAmount,
+		),
+	)
+
+	return &uniswapv4.AfterSwapResult{
+		HookFee: fee,
+	}, nil
+}
