204877c35d1c9905105cebd72f80ef16f5a02147
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-10-27 23:11:30 +0700

    chore: add all zora hook addresses

--- pkg/liquidity-source/uniswap/v4/hooks/zora/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/zora/constant.go
@@ -5,24 +5,29 @@ import "github.com/ethereum/go-ethereum/common"
 var (
 	HookAddresses = []common.Address{
 		// CreatorCoinHook
-		common.HexToAddress("0xa1ebdd5ca6470bbd67114331387f2dda7bfad040"),
-		common.HexToAddress("0x5e5d19d22c85a4aef7c1fdf25fb22a5a38f71040"),
-		common.HexToAddress("0xd61a675f8a0c67a73dc3b54fb7318b4d91409040"),
+		common.HexToAddress("0xffF800B76768dA8AB6aab527021e4a6A91219040"),
 		common.HexToAddress("0x9278f6e55ce58519c79dc1ab0ad3b29ea7821040"),
-		common.HexToAddress("0x8218FA8d7922e22aED3556a09D5A715F16Ad5040"),
-		common.HexToAddress("0x1258e5f3C71ca9dCE95Ce734Ba5759532E46D040"),
+		common.HexToAddress("0xfBcE3D80C659C765bc6C55e29e87d839c7609040"), // v1
+		common.HexToAddress("0x854F820475b229B7805a386F758Cfb285023d040"), // v1.0.0.1
+		common.HexToAddress("0x9301690BE9aC901DE52c5EbFf883862BBfC99040"), // v1.1.1
+		common.HexToAddress("0x5e5D19d22c85A4aef7C1FdF25fB22A5a38f71040"), // v1.1.1.1
+		common.HexToAddress("0xd61A675F8a0c67A73DC3B54FB7318B4D91409040"), // v1.1.2
+		common.HexToAddress("0x8218FA8d7922e22aED3556a09D5A715F16Ad5040"), // v2.2
+		common.HexToAddress("0x1258e5f3C71ca9dCE95Ce734Ba5759532E46D040"), // v2.2.1
+		common.HexToAddress("0x3dCDef28F358b0e89D630F699fBE083db89d5040"),
 
 		// ContentCoinHook
-		// common.HexToAddress("0xfff800b76768da8ab6aab527021e4a6a91219040"), // disabled for now: large number of pools
-		common.HexToAddress("0x5bf219b3cc11e3f6dd8dc8fc89d7d1deb0431040"),
-		common.HexToAddress("0x9ea932730a7787000042e34390b8e435dd839040"),
-		common.HexToAddress("0x2b15a16B3Ef024005bA899Bb51764FCd58Cf9040"),
-		common.HexToAddress("0xC8d077444625eB300A427a6dfB2b1DBf9b159040"),
+		common.HexToAddress("0xd3D133469ADC85e01A4887404D8AC12d630e9040"),
+		common.HexToAddress("0x9b47e436e216b2cb54d6f24154f86d946098d040"),
+		common.HexToAddress("0xA1eBdD5cA6470Bbd67114331387f2dDa7bfad040"), // v1
+		common.HexToAddress("0xb030Fd8c2f8576f8Ab05cFBbe659285e7D7a1040"), // v1.0.0.1
+		common.HexToAddress("0xe61bDF0c9e665F02DF20FEdE6DcEF379Cb751040"), // v1.0.0.2
+		common.HexToAddress("0x81542dC43Aff247eff4a0eceFC286A2973aE1040"), // v1.1.1
+		common.HexToAddress("0x5bF219b3Cc11E3f6Dd8dc8fC89D7d1deB0431040"), // v1.1.1.1
+		common.HexToAddress("0x9ea932730A7787000042e34390B8E435dD839040"), // v1.1.2
+		common.HexToAddress("0xff74Be9D3596eA7a33BB4983DD7906fB34135040"), // v2.2
+		common.HexToAddress("0x2b15a16B3Ef024005bA899Bb51764FCd58Cf9040"), // v2.2.1
+		common.HexToAddress("0xC8d077444625eB300A427a6dfB2b1DBf9b159040"), // v2.3.0
+		common.HexToAddress("0x030149dE8f1bD128E61b635a88Ba7387D89D9040"),
 	}
-
-	ZoraAddress = common.HexToAddress("0x1111111111166b7FE7bd91427724B487980aFc69")
 )
-
-func IsZora(token common.Address) bool {
-	return token.Cmp(ZoraAddress) == 0
-}
