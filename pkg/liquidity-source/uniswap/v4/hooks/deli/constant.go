3b3a5734cea8796b5285c0d9211ff3d911525b3e
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-11-08 10:00:19 +0700

    chore: add deli hook addresses

--- pkg/liquidity-source/uniswap/v4/hooks/deli/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/deli/constant.go
@@ -8,11 +8,13 @@ import (
 )
 
 var (
-	HookAddresses = []common.Address{
-		common.HexToAddress("0xC384B99A6e5cD1a800B2d83aB71BaB7bD712b0cc"), // DeliHook
+	HookAddresses = []common.Address{ // DeliHook
+		common.HexToAddress("0xC384B99A6e5cD1a800B2d83aB71BaB7bD712b0cc"),
+		common.HexToAddress("0x570A48F96035C2874de1c0F13c5075A05683b0cc"),
 	}
-	ConstantProductAddresses = []common.Address{
-		common.HexToAddress("0x00C9DA9AbC5303219ead3Cf0307b5A8A7644BaC8"), // DeliHookConstantProduct
+	ConstantProductAddresses = []common.Address{ // DeliHookConstantProduct
+		common.HexToAddress("0x00C9DA9AbC5303219ead3Cf0307b5A8A7644BaC8"),
+		common.HexToAddress("0x95AFBC0FCcF974B41380f24e562f15b6DD90faC8"),
 	}
 )
 
