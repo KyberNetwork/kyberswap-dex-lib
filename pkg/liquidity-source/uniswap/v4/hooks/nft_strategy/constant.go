b4088187e3834f4ce690380dfed66faa3ba9f2ad
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-11-12 00:25:15 +0700

    ft: add more nftstrat hook addresses (#1161)

--- pkg/liquidity-source/uniswap/v4/hooks/nft_strategy/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/nft_strategy/constant.go
@@ -6,10 +6,12 @@ import (
 
 var HookAddresses = []common.Address{
 	common.HexToAddress("0xfAaad5B731F52cDc9746F2414c823eca9B06E844"),
-	common.HexToAddress("0xBD15e4D324F8D02479A5FF53b52eF4048a79e444"),
-	common.HexToAddress("0xD6A45DF0C82C9A686Ab1e58FB28d8Fc0cF106444"),
 	common.HexToAddress("0xe3C63A9813Ac03BE0e8618B627cb8170cfA468c4"),
 	common.HexToAddress("0x5d8A61FA2Ced43EEaBffC00c85f705E3e08c28c4"),
+	common.HexToAddress("0xD6A45DF0C82C9A686Ab1e58FB28d8Fc0cF106444"),
+	common.HexToAddress("0xBD15e4D324F8D02479A5FF53b52eF4048a79e444"),
+	common.HexToAddress("0xDAdaAa9591d6F4d68748898FbAcc99dc69012444"),
+	common.HexToAddress("0xfdFA1DFAB150fE6B2FDFdd609F37B2459C7b6444"),
 }
 
 var PunkHookAddress = common.HexToAddress("0xfAaad5B731F52cDc9746F2414c823eca9B06E844")
