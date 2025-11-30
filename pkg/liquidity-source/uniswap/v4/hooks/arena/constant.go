33aa454f099d9b774f7e5aafbbdf1d9c014ec852
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-11-30 22:37:37 +0700

    chore: add arena hook addresses (#1189)

--- pkg/liquidity-source/uniswap/v4/hooks/arena/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/arena/constant.go
@@ -6,4 +6,5 @@ import (
 
 var HookAddresses = []common.Address{
 	common.HexToAddress("0x32B717Da13fdF36081Cee825414a305C50F9A044"),
+	common.HexToAddress("0xE32A5d788c568FC5A671255d17B618e70552E044"),
 }
