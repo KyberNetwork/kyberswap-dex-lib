70cac037482ccb2c637f3154ea185bd18d7cb0b2
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2025-12-05 17:56:01 +0700

    ft: adjust lgl-clob gas estimate and add safety buffer (#1200)

--- pkg/liquidity-source/lgl-clob/constant.go
+++ pkg/liquidity-source/lgl-clob/constant.go
@@ -8,13 +8,16 @@ import (
 const (
 	DexType = "lgl-clob"
 
-	maxPriceLevels = 50
+	maxPriceLevels = 48
+
+	safetyBuffer = 0.69420
 )
 
 var (
 	bMaxPriceLevels = big.NewInt(maxPriceLevels)
 
-	ErrInvalidToken  = errors.New("invalid token")
-	ErrInvalidAmount = errors.New("invalid amount")
-	ErrEmptyOrders   = errors.New("empty orders")
+	ErrInvalidToken         = errors.New("invalid token")
+	ErrInvalidAmount        = errors.New("invalid amount")
+	ErrEmptyOrders          = errors.New("empty orders")
+	ErrExceededSafetyBuffer = errors.New("exceed safety buffer")
 )
