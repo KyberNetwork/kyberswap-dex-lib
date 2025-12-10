854c4a57f913820cfe30a044fa704559b8938a5c
tien7668 <33758175+tien7668@users.noreply.github.com>
2025-12-10 21:22:50 +0700

    getSupportedTokens missed for some chains (#1207)

--- pkg/liquidity-source/kelp/rseth-l2/config.go
+++ pkg/liquidity-source/kelp/rseth-l2/config.go
@@ -3,8 +3,9 @@ package rsethl2
 import "github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 
 type Config struct {
-	DexID          string              `json:"dexID"`
-	LRTDepositPool string              `json:"lrtDepositPool"`
-	ChainId        valueobject.ChainID `json:"chainId"`
-	CheckNative    bool                `json:"checkNative"`
+	DexID             string              `json:"dexID"`
+	LRTDepositPool    string              `json:"lrtDepositPool"`
+	ChainId           valueobject.ChainID `json:"chainId"`
+	CheckNative       bool                `json:"checkNative"`
+	NoSupportedTokens bool                `json:"noSupportedTokens"`
 }
