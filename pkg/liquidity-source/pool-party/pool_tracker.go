577ed95f9938a3fa1d445541af33c7714083351a
Hải Nam <nam.le@kyber.network>
2025-12-18 10:56:43 +0700

    chore: update pool party to use rpc call instead of subgraph

--- pkg/liquidity-source/pool-party/pool_tracker.go
+++ pkg/liquidity-source/pool-party/pool_tracker.go
@@ -9,13 +9,11 @@ import (
 	"github.com/KyberNetwork/logger"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/goccy/go-json"
-	"github.com/samber/lo"
-	"github.com/sourcegraph/conc/pool"
 
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
 	sourcePool "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
 	pooltrack "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool/tracker"
-	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/bignumber"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/abi"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/graphql"
 )
 
@@ -49,95 +47,64 @@ func (d *PoolTracker) GetNewPoolState(
 		"dexID":       d.config.DexID,
 	})
 
-	var rateToETH *big.Int
-	var blockNumber big.Int
-	var subgraphPool SubgraphPool
+	var rateToETH, balanceOf *big.Int
 
-	g := pool.New().WithContext(ctx)
-	g.Go(func(context.Context) error {
-		req := d.ethrpcClient.NewRequest().SetContext(ctx)
+	req := d.ethrpcClient.NewRequest().SetContext(ctx)
 
-		req.AddCall(&ethrpc.Call{
-			ABI:    oneInchOracle,
-			Target: d.config.Oracle,
-			Method: "getRateToEth",
-			Params: []any{
-				common.HexToAddress(p.Tokens[1].Address), // srcToken (address)
-				false,                                    // useSrcWrappers (bool)
-			},
-		}, []any{&rateToETH})
+	req.AddCall(&ethrpc.Call{
+		ABI:    oneInchOracle,
+		Target: d.config.Oracle,
+		Method: "getRateToEth",
+		Params: []any{
+			common.HexToAddress(p.Tokens[1].Address), // srcToken (address)
+			false,                                    // useSrcWrappers (bool)
+		},
+	}, []any{&rateToETH})
 
-		res, err := req.TryAggregate()
-		if err != nil {
-			l.WithFields(logger.Fields{
-				"error": err,
-			}).Error("failed to getRateToETH")
-			return err
-		}
+	req.AddCall(&ethrpc.Call{
+		ABI:    abi.Erc20ABI,
+		Target: p.Tokens[1].Address,
+		Method: "balanceOf",
+		Params: []any{
+			common.HexToAddress(p.Address),
+		},
+	}, []any{&balanceOf})
 
-		if res.BlockNumber != nil {
-			blockNumber.Set(res.BlockNumber)
-		}
-
-		return nil
-	})
-
-	g.Go(func(ctx context.Context) error {
-		req := graphql.NewRequest(getPoolState(p.Address))
-
-		var res struct {
-			Pool SubgraphPool `json:"pool"`
-		}
-
-		if err := d.graphqlClient.Run(ctx, req, &res); err != nil {
-			l.WithFields(logger.Fields{
-				"error": err,
-			}).Error("failed to query subgraph for pool state")
-			return err
-		}
-
-		subgraphPool = res.Pool
-
-		return nil
-	})
-
-	if err := g.Wait(); err != nil {
+	res, err := req.TryAggregate()
+	if err != nil {
 		l.WithFields(logger.Fields{
 			"error": err,
-		}).Error("failed to fetch pool state")
-		return entity.Pool{}, err
-	}
-
-	var prevExtra Extra
-	if err := json.Unmarshal([]byte(p.Extra), &prevExtra); err != nil {
+		}).Error("failed to track pool state from PoolParty")
 		return p, err
 	}
 
-	newPublicAmountAvailable := lo.Ternary(
-		subgraphPool.PublicAmountAvailable != "",
-		bignumber.NewBig10(subgraphPool.PublicAmountAvailable),
-		prevExtra.PublicAmountAvailable,
-	)
+	poolStatus := poolStatusActive
+	isVisible := true
+	if balanceOf.Sign() == 0 {
+		poolStatus = poolStatusCanceled
+		isVisible = false
+	}
 
 	extra := Extra{
-		PoolStatus:            subgraphPool.PoolStatus,
-		IsVisible:             subgraphPool.IsVisible,
+		PoolStatus:            poolStatus,
+		IsVisible:             isVisible,
 		BoostPriceBps:         d.config.BoostPriceBps,
 		RateToETH:             rateToETH,
-		PublicAmountAvailable: newPublicAmountAvailable,
+		PublicAmountAvailable: balanceOf,
 		Exchange:              d.config.Exchange,
 	}
 	extraBytes, _ := json.Marshal(extra)
 
-	if extra.PoolStatus != poolStatusActive || !extra.IsVisible {
-		p.Reserves[0] = "0"
-	} else {
-		p.Reserves[1] = subgraphPool.PublicAmountAvailable
-	}
+	p.Reserves[1] = balanceOf.String()
 
 	p.Extra = string(extraBytes)
 	p.Timestamp = time.Now().Unix()
-	p.BlockNumber = blockNumber.Uint64()
+
+	if res.BlockNumber != nil {
+		p.BlockNumber = res.BlockNumber.Uint64()
+	}
+
+	l.Info("successfully get new pool state")
 
 	return p, nil
 }
