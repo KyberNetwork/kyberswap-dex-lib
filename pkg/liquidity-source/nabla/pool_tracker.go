d0d4804fcfeb95d25a1135239d380fab8b0aee40
Minh Nhat Hoang <nhat.hm@outlook.com>
2025-12-26 20:15:43 +0700

    feat: update full state from rpc for nabla (#1236)

--- pkg/liquidity-source/nabla/pool_tracker.go
+++ pkg/liquidity-source/nabla/pool_tracker.go
@@ -20,6 +20,7 @@ import (
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
 	pooltrack "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool/tracker"
 	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/util/eth"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
 )
 
 var _ = pooltrack.RegisterFactoryCE0(DexType, NewPoolTracker)
@@ -72,7 +73,6 @@ func (t *PoolTracker) GetNewPoolState(ctx context.Context, p entity.Pool,
 		logger.Infof("starting refresh of pool %v due to asset changes", p.Address)
 
 		poolByAssets := make([]common.Address, len(assets))
-
 		req := t.ethrpcClient.R().SetContext(ctx)
 		for i, asset := range assets {
 			req.AddCall(&ethrpc.Call{
@@ -82,65 +82,36 @@ func (t *PoolTracker) GetNewPoolState(ctx context.Context, p entity.Pool,
 				Params: []any{asset},
 			}, []any{&poolByAssets[i]})
 		}
+		_, err := req.Aggregate()
+		if err != nil {
+			logger.Errorf("failed to aggregate pool by asset")
+			return p, err
+		}
+
+		curves := make([]common.Address, len(assets))
+		req = t.ethrpcClient.R().SetContext(ctx)
+		for i, sp := range poolByAssets {
+			req.AddCall(&ethrpc.Call{
+				ABI:    swapPoolABI,
+				Target: sp.String(),
+				Method: "slippageCurve",
+			}, []any{&curves[i]})
+		}
 		resp, err := req.Aggregate()
 		if err != nil {
-			logger.Errorf("failed to aggregate pool by asset and price feed id")
 			return p, err
 		}
 
-		n := len(poolByAssets)
-		var (
-			reserves             = make([]*big.Int, n)
-			reservesWithSlippage = make([]*big.Int, n)
-			totalLiabilities     = make([]*big.Int, n)
-			swapFees             = make([]SwapFees, n)
-			curveAddresses       = make([]common.Address, n)
-			betaCParams          = make([]Params, n)
-		)
-
+		betaCParams := make([]Params, len(assets))
 		req = t.ethrpcClient.R().SetContext(ctx).SetBlockNumber(resp.BlockNumber)
-		for i, pAddress := range poolByAssets {
-			req.AddCall(&ethrpc.Call{
-				ABI:    swapPoolABI,
-				Target: pAddress.String(),
-				Method: "reserve",
-			}, []any{&reserves[i]}).AddCall(&ethrpc.Call{
-				ABI:    swapPoolABI,
-				Target: pAddress.String(),
-				Method: "reserveWithSlippage",
-			}, []any{&reservesWithSlippage[i]}).AddCall(&ethrpc.Call{
-				ABI:    swapPoolABI,
-				Target: pAddress.String(),
-				Method: "totalLiabilities",
-			}, []any{&totalLiabilities[i]}).AddCall(&ethrpc.Call{
-				ABI:    swapPoolABI,
-				Target: pAddress.String(),
-				Method: "swapFees",
-			}, []any{&swapFees[i]}).AddCall(&ethrpc.Call{
-				ABI:    swapPoolABI,
-				Target: pAddress.String(),
-				Method: "slippageCurve",
-			}, []any{&curveAddresses[i]})
-		}
-		resp, err = req.Aggregate()
-		if err != nil {
-			return p, err
-		}
-
-		assetPrices, err := t.getAssetPrices(ctx, assets, resp.BlockNumber)
-		if err != nil {
-			return p, err
-		}
-
-		req = t.ethrpcClient.R().SetContext(ctx).SetBlockNumber(resp.BlockNumber)
-		for i := 0; i < n; i++ {
+		for i := 0; i < len(assets); i++ {
 			req.AddCall(&ethrpc.Call{
 				ABI:    curveABI,
-				Target: curveAddresses[i].String(),
+				Target: curves[i].String(),
 				Method: "params",
 			}, []any{&betaCParams[i]})
 		}
-		resp, err = req.Aggregate()
+		_, err = req.Aggregate()
 		if err != nil {
 			return p, err
 		}
@@ -155,32 +126,27 @@ func (t *PoolTracker) GetNewPoolState(ctx context.Context, p entity.Pool,
 		extra.Pools = lo.Map(poolByAssets, func(poolByAsset common.Address, i int) NablaPool {
 			return NablaPool{
 				Address: poolByAsset,
-				Curve:   curveAddresses[i],
+				Curve:   curves[i],
 				Meta: NablaPoolMeta{
-					CurveBeta:   int256.MustFromBig(betaCParams[i].Beta),
-					CurveC:      int256.MustFromBig(betaCParams[i].C),
-					BackstopFee: int256.MustFromBig(swapFees[i].BackstopFee),
-					ProtocolFee: int256.MustFromBig(swapFees[i].ProtocolFee),
-					LpFee:       int256.MustFromBig(swapFees[i].LpFee),
-				},
-				State: NablaPoolState{
-					Reserve:             int256.MustFromBig(reserves[i]),
-					ReserveWithSlippage: int256.MustFromBig(reservesWithSlippage[i]),
-					TotalLiabilities:    int256.MustFromBig(totalLiabilities[i]),
-					Price:               int256.MustFromBig(assetPrices[i]),
+					CurveBeta: int256.MustFromBig(betaCParams[i].Beta),
+					CurveC:    int256.MustFromBig(betaCParams[i].C),
 				},
 			}
 		})
 
 		extra.DependenciesStored = false
 
-		p.BlockNumber = resp.BlockNumber.Uint64()
+		if err = t.getRPCState(ctx, &p, &extra); err != nil {
+			return p, nil
+		}
 
 		logger.Infof("finished refreshing pool %v after asset changes", p.Address)
 	}
 
 	if len(params.Logs) > 0 {
 		t.handleEvents(ctx, &p, &extra, params.Logs)
+	} else if err := t.getRPCState(ctx, &p, &extra); err != nil {
+		return p, err
 	}
 
 	extraBytes, err := json.Marshal(extra)
@@ -196,7 +162,64 @@ func (t *PoolTracker) GetNewPoolState(ctx context.Context, p entity.Pool,
 	return p, nil
 }
 
-func (t *PoolTracker) getAssetPrices(ctx context.Context, assets []common.Address, blockNumber *big.Int) ([]*big.Int, error) {
+func (t *PoolTracker) getRPCState(ctx context.Context, p *entity.Pool, extra *Extra) error {
+	n := len(p.Tokens)
+	var (
+		reserves             = make([]*big.Int, n)
+		reservesWithSlippage = make([]*big.Int, n)
+		totalLiabilities     = make([]*big.Int, n)
+		swapFees             = make([]SwapFees, n)
+	)
+	req := t.ethrpcClient.R().SetContext(ctx)
+	for i, sp := range extra.Pools {
+		req.AddCall(&ethrpc.Call{
+			ABI:    swapPoolABI,
+			Target: sp.Address.String(),
+			Method: "reserve",
+		}, []any{&reserves[i]}).AddCall(&ethrpc.Call{
+			ABI:    swapPoolABI,
+			Target: sp.Address.String(),
+			Method: "reserveWithSlippage",
+		}, []any{&reservesWithSlippage[i]}).AddCall(&ethrpc.Call{
+			ABI:    swapPoolABI,
+			Target: sp.Address.String(),
+			Method: "totalLiabilities",
+		}, []any{&totalLiabilities[i]}).AddCall(&ethrpc.Call{
+			ABI:    swapPoolABI,
+			Target: sp.Address.String(),
+			Method: "swapFees",
+		}, []any{&swapFees[i]})
+	}
+	resp, err := req.Aggregate()
+	if err != nil {
+		return err
+	}
+
+	assets := lo.Map(p.Tokens, func(t *entity.PoolToken, _ int) string { return t.Address })
+	prices, err := t.getAssetPrices(ctx, assets, resp.BlockNumber)
+	if err != nil {
+		return err
+	}
+
+	for i := range n {
+		extra.Pools[i].Meta.BackstopFee = int256.MustFromBig(swapFees[i].BackstopFee)
+		extra.Pools[i].Meta.ProtocolFee = int256.MustFromBig(swapFees[i].ProtocolFee)
+		extra.Pools[i].Meta.LpFee = int256.MustFromBig(swapFees[i].LpFee)
+
+		extra.Pools[i].State.Reserve = int256.MustFromBig(reserves[i])
+		extra.Pools[i].State.ReserveWithSlippage = int256.MustFromBig(reservesWithSlippage[i])
+		extra.Pools[i].State.TotalLiabilities = int256.MustFromBig(totalLiabilities[i])
+		extra.Pools[i].State.Price = int256.MustFromBig(prices[i])
+	}
+
+	p.BlockNumber = resp.BlockNumber.Uint64()
+
+	logger.Infof("finished getting state from RPC for %v", p.Address)
+
+	return nil
+}
+
+func (t *PoolTracker) getAssetPrices(ctx context.Context, assets []string, blockNumber *big.Int) ([]*big.Int, error) {
 	if len(assets) == 0 {
 		return nil, nil
 	}
@@ -205,7 +228,7 @@ func (t *PoolTracker) getAssetPrices(ctx context.Context, assets []common.Addres
 	results := make([]hexutil.Bytes, len(assets))
 
 	for i, asset := range assets {
-		callData, err := oracleABI.Pack("getAssetPrice", asset)
+		callData, err := oracleABI.Pack("getAssetPrice", common.HexToAddress(asset))
 		if err != nil {
 			logger.Errorf("failed to pack get asset price")
 			return nil, err
@@ -215,7 +238,7 @@ func (t *PoolTracker) getAssetPrices(ctx context.Context, assets []common.Addres
 			Method: "eth_call",
 			Args: []any{
 				map[string]any{
-					"from": t.config.Whitelisted,
+					"from": lo.Ternary(len(t.config.Whitelisted) > 0, t.config.Whitelisted, valueobject.ZeroAddress),
 					"to":   t.config.Oracle,
 					"data": hexutil.Encode(callData),
 				},
@@ -306,9 +329,7 @@ func (t *PoolTracker) handleEvents(ctx context.Context, p *entity.Pool, extra *E
 	}
 
 	if shouldGetAssetPrices {
-		assets := lo.Map(p.Tokens, func(token *entity.PoolToken, index int) common.Address {
-			return common.HexToAddress(token.Address)
-		})
+		assets := lo.Map(p.Tokens, func(token *entity.PoolToken, index int) string { return token.Address })
 		prices, err := t.getAssetPrices(ctx, assets, big.NewInt(int64(p.BlockNumber)))
 		if err != nil {
 			logger.Errorf("failed to get asset prices: %v", err)
@@ -316,9 +337,7 @@ func (t *PoolTracker) handleEvents(ctx context.Context, p *entity.Pool, extra *E
 		}
 
 		for i := range extra.Pools {
-			swapPool := extra.Pools[i]
-			swapPool.State.Price = int256.MustFromBig(prices[i])
-			extra.Pools[i] = swapPool
+			extra.Pools[i].State.Price = int256.MustFromBig(prices[i])
 		}
 	}
 }
