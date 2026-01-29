7b89af7ea87855dce2e96d2904c78819087601ae
Minh Nhat Hoang <nhat.hm@outlook.com>
2026-01-29 18:06:59 +0700

    chore: tune gas for ekubo-v3

--- pkg/liquidity-source/ekubo/v3/pools/stableswap.go
+++ pkg/liquidity-source/ekubo/v3/pools/stableswap.go
@@ -115,7 +115,7 @@ func (p *StableswapPool) Quote(amount *uint256.Int, isToken1 bool) (*quoting.Quo
 		ConsumedAmount:   amountRemaining.Sub(amount, amountRemaining),
 		CalculatedAmount: &calculatedAmount,
 		FeesPaid:         &feesPaid,
-		Gas:              quoting.BaseGasStableswapSwap + int64(initializedTicksCrossed)*quoting.GasInitializedTickCrossed, // TODO Lower costs than a CL pool tick crossing
+		Gas:              quoting.BaseGasStableswapSwap + int64(initializedTicksCrossed)*quoting.GasInitializedTickCrossed,
 		SwapInfo: quoting.SwapInfo{
 			SkipAhead:  0,
 			IsToken1:   isToken1,
