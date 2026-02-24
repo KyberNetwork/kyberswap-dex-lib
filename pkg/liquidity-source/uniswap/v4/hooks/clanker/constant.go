20383b7fbe2d04189f2d0f99824af3ef95a88ecd
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2026-02-25 00:10:56 +0700

    fix: clanker on bsc is static (#1309)

--- pkg/liquidity-source/uniswap/v4/hooks/clanker/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/clanker/constant.go
@@ -24,6 +24,7 @@ var (
 		valueobject.ChainIDArbitrumOne: "0xEb9D2A726Edffc887a574dC7f46b3a3638E8E44f",
 		valueobject.ChainIDEthereum:    "0x6C8599779B03B00AAaE63C6378830919Abb75473",
 		valueobject.ChainIDMonad:       "0xF9a0C289Eab6B571c6247094a853810987E5B26D",
+		valueobject.ChainIDBSC:         "0xea30438E0B5f99096cb05A8Da63be55A6A298F6a",
 	}
 
 	DynamicFeeHookAddresses = []common.Address{
@@ -32,7 +33,6 @@ var (
 		common.HexToAddress("0xd60D6B218116cFd801E28F78d011a203D2b068Cc"), // base v2 v4.1.0 permission-ed
 		common.HexToAddress("0x9b37A43422D7bBD4C8B231be11E50AD1acE828CC"), // unichain v4.0.0
 		common.HexToAddress("0xFd213BE7883db36e1049dC42f5BD6A0ec66B68cC"), // arbitrum v4.0.0
-		common.HexToAddress("0xC5d309026BCAb6630888d51CE21154AD2f4828cC"), // bsc v4.0.0
 	}
 
 	StaticFeeHookAddresses = []common.Address{
@@ -43,5 +43,6 @@ var (
 		common.HexToAddress("0xf7aC669593d2D9D01026Fa5B756DD5B4f7aAa8Cc"), // arbitrum v4.0.0
 		common.HexToAddress("0x6C24D0bCC264EF6A740754A11cA579b9d225e8Cc"), // ethereum v4.0.0
 		common.HexToAddress("0x94F802a9EFE4dd542FdBd77a25D9e69A6dC828Cc"), // monad v2 v4.1.0
+		common.HexToAddress("0xC5d309026BCAb6630888d51CE21154AD2f4828cC"), // bsc v4.0.0
 	}
 )
