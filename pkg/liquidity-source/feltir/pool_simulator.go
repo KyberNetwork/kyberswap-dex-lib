58964bc5471f6cc4d92929c332273413057e9c5e
SunSpirit <48086732+sunspirit99@users.noreply.github.com>
2026-03-27 22:21:59 +0700

    fix(feltir): track remainingIn capacity to prevent cumulative swap limit bypass

--- pkg/liquidity-source/feltir/pool_simulator.go
+++ pkg/liquidity-source/feltir/pool_simulator.go
@@ -19,6 +19,7 @@ type PoolSimulator struct {
 	pool.Pool
 	Extra
 	StaticExtra
+	remainingIn [2]*big.Int
 }
 
 var (
@@ -40,7 +41,7 @@ func NewPoolSimulator(p entity.Pool) (*PoolSimulator, error) {
 	tokens := lo.Map(p.Tokens, func(e *entity.PoolToken, _ int) string { return strings.ToLower(e.Address) })
 	reserves := lo.Map(p.Reserves, func(e string, _ int) *big.Int { return bignumber.NewBig(e) })
 
-	return &PoolSimulator{
+	sim := &PoolSimulator{
 		Pool: pool.Pool{Info: pool.PoolInfo{
 			Address:     p.Address,
 			Exchange:    p.Exchange,
@@ -51,7 +52,15 @@ func NewPoolSimulator(p entity.Pool) (*PoolSimulator, error) {
 		}},
 		Extra:       extra,
 		StaticExtra: staticExtra,
-	}, nil
+	}
+
+	for dir := range 2 {
+		if dir < len(sim.Samples) && len(sim.Samples[dir]) > 0 {
+			sim.remainingIn[dir] = new(big.Int).Set(sim.Samples[dir][len(sim.Samples[dir])-1][0])
+		}
+	}
+
+	return sim, nil
 }
 
 func (s *PoolSimulator) CalcAmountOut(params pool.CalcAmountOutParams) (*pool.CalcAmountOutResult, error) {
@@ -71,8 +80,10 @@ func (s *PoolSimulator) CalcAmountOut(params pool.CalcAmountOutParams) (*pool.Ca
 	amountIn := params.TokenAmountIn.Amount
 	samples := s.Samples[indexIn]
 
-	if amountIn.Cmp(samples[len(samples)-1][0]) > 0 {
-		return nil, ErrInsufficientLiquidity
+	if rem := s.remainingIn[indexIn]; rem != nil {
+		if amountIn.Cmp(rem) > 0 {
+			return nil, ErrInsufficientLiquidity
+		}
 	}
 
 	idx := sort.Search(len(samples), func(i int) bool {
@@ -109,6 +120,13 @@ func (s *PoolSimulator) UpdateBalance(params pool.UpdateBalanceParams) {
 	s.Info.Reserves[indexIn] = new(big.Int).Add(s.Info.Reserves[indexIn], params.TokenAmountIn.Amount)
 	s.Info.Reserves[indexOut] = new(big.Int).Sub(s.Info.Reserves[indexOut], params.TokenAmountOut.Amount)
 
+	if s.remainingIn[indexIn] != nil {
+		s.remainingIn[indexIn] = new(big.Int).Sub(s.remainingIn[indexIn], params.TokenAmountIn.Amount)
+		if s.remainingIn[indexIn].Sign() < 0 {
+			s.remainingIn[indexIn] = new(big.Int)
+		}
+	}
+
 	if limit := params.SwapLimit; limit != nil {
 		_, _, _ = limit.UpdateLimit(
 			params.TokenAmountOut.Token,
@@ -131,6 +149,11 @@ func (s *PoolSimulator) CalculateLimit() map[string]*big.Int {
 func (s *PoolSimulator) CloneState() pool.IPoolSimulator {
 	cloned := *s
 	cloned.Info.Reserves = slices.Clone(s.Info.Reserves)
+	for i := range s.remainingIn {
+		if s.remainingIn[i] != nil {
+			cloned.remainingIn[i] = new(big.Int).Set(s.remainingIn[i])
+		}
+	}
 	return &cloned
 }
 
