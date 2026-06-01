8bdbe5563874ca5cc54f0966b4cb05ff4fdf21b9
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2026-06-01 18:08:17 +0700

    ft: update aegis integration for new version

--- pkg/liquidity-source/uniswap/v4/hooks/aegis/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/aegis/constant.go
@@ -5,5 +5,6 @@ import (
 )
 
 var HookAddresses = []common.Address{
-	common.HexToAddress("0xA0b0D2d00fD544D8E0887F1a3cEDd6e24Baf10cc\n"),
+	common.HexToAddress("0xA0b0D2d00fD544D8E0887F1a3cEDd6e24Baf10cc"),
+	common.HexToAddress("0x8f29BD5c8429730fA4C46e6295c4e679eDEdd0cC"),
 }
