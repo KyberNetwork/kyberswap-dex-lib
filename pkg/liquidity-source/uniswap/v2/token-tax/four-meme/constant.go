b1da7cf9585a12abc784ca92ceff1597f19beaa8
SunSpirit <48086732+sunspirit99@users.noreply.github.com>
2026-06-22 10:49:52 +0700

    ft(uniswap-v2): support four.meme tax pools paired with USDT , USD1 & UUSD

--- pkg/liquidity-source/uniswap/v2/token-tax/four-meme/constant.go
+++ pkg/liquidity-source/uniswap/v2/token-tax/four-meme/constant.go
@@ -13,5 +13,8 @@ var factories = map[string]struct{}{
 }
 
 var baseTokens = map[string]struct{}{
-	"0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c": {}, // BSC
+	"0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c": {}, // WBNB
+	"0x55d398326f99059ff775485246999027b3197955": {}, // USDT
+	"0x61a10e8556bed032ea176330e7f17d6a12a10000": {}, // UUSD
+	"0x8d0d000ee44948fc98c9b98a4fa4921476f08b0d": {}, // USD1
 }
