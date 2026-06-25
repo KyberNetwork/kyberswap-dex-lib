b088197becff1fa7709795279e258eea4680c3ab
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2026-06-25 10:53:43 +0700

    chore: add arrakis hook

--- pkg/liquidity-source/uniswap/v4/hooks/arrakis/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/arrakis/constant.go
@@ -7,5 +7,6 @@ import (
 var (
 	HookAddresses = []common.Address{
 		common.HexToAddress("0xF9527fB5a34aC6FBC579e4fbc3bF292Ed57d4880"),
+		common.HexToAddress("0x192499a79caDB2E9ba221a252a3e3b7adc714880"),
 	}
 )
