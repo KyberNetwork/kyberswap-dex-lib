a2eb0715f44ad239f5835c8b05202c778666df04
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2026-08-07 12:39:29 +0700

    chore: add stable hook

--- pkg/liquidity-source/uniswap/v4/hooks/stable-stable/constant.go
+++ pkg/liquidity-source/uniswap/v4/hooks/stable-stable/constant.go
@@ -5,9 +5,10 @@ import (
 )
 
 const (
-	gasBeforeSwap int64 = 11695
+	gasBeforeSwap int64 = 13445
 )
 
 var HookAddresses = []common.Address{
 	common.HexToAddress("0x4509b7Eb3F9641226804Fea4976963435d1c6080"),
+	common.HexToAddress("0x0000113dCf4ADd69999Fad8F20F2b63F979bfcC0"),
 }
